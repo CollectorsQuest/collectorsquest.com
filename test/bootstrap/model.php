281c26a7d0cb62ce15942b82ee0053e7fbd6e21c
IvanTanev <vankata.t@gmail.com>
2012-02-14 16:36:19 +0200

    Allow application to be set in functional tests
    Add $app = 'application'; before including the boostrap in your tests
    
    Conflicts:
    
    	test/bootstrap/model.php
    	test/bootstrap/routing.php

--- test/bootstrap/model.php
+++ test/bootstrap/model.php
@@ -2,6 +2,6 @@
 
 include __DIR__ .'/unit.php';
 
-/** @var $sf_configuration sfProjectConfiguration  */
-$sf_application = $sf_configuration->getApplicationConfiguration('frontend', 'test', isset($debug) ? $debug : true);
+/** @var $sf_configuration sfProjectConfiguration */
+$sf_application = $sf_configuration->getApplicationConfiguration(isset($app) ? $app : 'frontend', 'test', isset($debug) ? $debug : true);
 $sf_context = sfContext::createInstance($sf_application);
