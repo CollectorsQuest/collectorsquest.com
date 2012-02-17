22a258fdc6b15ef19995da950704bdf8b65c6b1e
Teomarino <teo.marinov@gmail.com>
2012-02-17 16:02:33 +0200

    Implemented todo item #124928865 http://goo.gl/JcD6F Drag and drop for Collectibles

---
+++ apps/legacy/modules/collection/templates/_carousel_view_collection.php
@@ -0,0 +1,42 @@
+<?php use_javascript('jquery/bt.js'); ?>
+<li>
+<div id="grid_view_collection_<?php echo $collection->getId(); ?>"
+     class="span-5 grid_view_collection <?php echo (isset($class)) ? $class : null; ?>"
+     style="<?= (isset($style)) ? $style : null; ?>">
+
+  <div class="stack">
+    <?php echo link_to_collection($collection, 'image'); ?>
+  </div>
+  <p class="caption">
+    <?php echo link_to_collection($collection, 'text'); ?>&nbsp;<font style="color:#ccc;">(<?php echo (int) $collection->countCollectibles(); ?>)</font>&nbsp;<?php if ($collection->countCollectiblesSince('7 day ago') > 0) echo image_tag('icons/new.png'); ?>
+    <br><small>by</small>
+    <?php echo link_to_collector($collection->getCollector(), 'text', array('style' => 'color: #000;')); ?>
+  </p>
+</div>
+</li>
+<?php cq_javascript_tag(); ?>
+<script type="text/javascript">
+$(document).ready(function()
+{
+  $('#grid_view_collection_<?php echo $collection->getId(); ?> .stack').bt(
+  {
+    ajaxPath: '<?= url_for('@ajax_collection?section=component&page=snapshot&id='. $collection->getId() .'&collectibles=3'); ?>',
+    padding: 10,
+    width: 250,
+    spikeLength: 30,
+    spikeGirth: 30,
+    cornerRadius: 10,
+    fill: 'rgb(244, 247, 220)',
+    // fill: 'rgba(0, 0, 0, .9)',
+    strokeWidth: 3,
+    strokeStyle: '#BADC70',
+    positions: ['top', 'bottom'],
+    hoverIntentOpts:
+    {
+      interval: 800,
+      timeout: 1500
+    }
+  });
+});
+</script>
+<?php cq_end_javascript_tag(); ?>
--- apps/legacy/modules/collection/templates/_collectiblesMove.php
+++ apps/legacy/modules/collection/templates/_collectiblesMove.php
@@ -4,40 +4,48 @@
  * @var  Collectible[]  $collectibles
  * @var  Collection     $collection
  */
-?>
 
+?>
 <br class="clear" />
-<?php
-  foreach ($collections as $i => $c)
-  {
-    // Show the collection (in grid, list or hybrid view)
-    include_partial(
-      'collections/grid_view_collection',
-      array(
-        'collection' => $c,
-        'culture' => $sf_user->getCulture(),
-        'i' => $i
-      )
-    );
-  }
-?>
 
-<hr/>
-
-<br class="clear" />
-<div id="movable-collectibles" style="margin-left: 40px; margin-right: -10px;">
-<?php
-  foreach ($collectibles as $i => $collectible)
-  {
-    echo '<div id="collectible-', $collectible->getId(), '" class="span-3 append-bottom">',
-         image_tag_collectible($collectible, '75x75', array('style' => 'cursor: move;')),
-         '</div>';
-  }
-?>
+<div class="slider-wrapper cf ">
+  <a href="#" class="prevPage browse left scrollable-big-buttons"></a>
+<div class="scrollable-big">
+  <ul>
+    <?php
+      foreach ($collections as $i => $c)
+      {
+        // Show the collection (in grid, list or hybrid view)
+        include_partial(
+          'collection/carousel_view_collection',
+          array(
+            'collection' => $c,
+            'culture' => $sf_user->getCulture(),
+            'i' => $i
+          )
+        );
+      }
+    ?>
+  </ul>
+</div>
+  <a href="#" class="nextPage browse right scrollable-big-buttons"></a>
 </div>
 
+
+<div class="movable-collectibles-wrapper">
+  <div id="movable-collectibles" class="movable-collectibles-container cf">
+    <?php
+        foreach ($collectibles as $i => $collectible)
+        {
+          echo '<div id="collectible-', $collectible->getId(), '" class="span-2 append-bottom thumb-space">',
+               image_tag_collectible($collectible, '75x75', array('style' => 'cursor: move;')),
+               '</div>';
+        }
+    ?>
+  </div>
+</div>
 <br class="clear">
-<div style="width: 200px; margin: auto;">
+<div style="width: 200px; margin:20px auto;">
   <?php
     echo cq_button(
       __('Done moving collectibles...'),
@@ -46,6 +54,7 @@
     );
   ?>
 </div>
+
 
 <script type="text/javascript">
 $(document).ready(function()
@@ -69,5 +78,14 @@ $(document).ready(function()
       );
     }
   });
+
+  $('div.scrollable-big .loading').hide();
+  $('div.scrollable-big ul').show();
+  $("div.scrollable-big").jCarouselLite(
+  {
+    btnNext: "button.nextPage", btnPrev: "button.prevPage",
+    mouseWheel: false, visible: 3, scroll: 2, circular: false, start: 0
+  });
+
 });
 </script>
