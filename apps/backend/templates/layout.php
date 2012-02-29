aaa774b60331849293ae5229736c7f6d18439ad3
Kiril Angov <kiril.angov@gmail.com>
2012-02-29 17:37:59 +0200

    Fixing the backend application as well as combine.php in terms of static assets caching

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -8,27 +8,14 @@
     <link rel="shortcut icon" href="/images/favicon.ico" />
     <?php include_partial('iceBackendModule/head'); ?>
 
-    <?php
-      sfConfig::set('symfony.asset.stylesheets_included', true);
-      $css = @implode(',', array_keys($sf_response->getStylesheets()));
-
-      if (!empty($css))
-      {
-        echo '<link rel="stylesheet" type="text/css" media="screen" href="/combine.php?type=css&files='. $css .'&revision='. SVN_REVISION .'" />';
-      }
-    ?>
+    <?php cq_include_stylesheets(); ?>
   </head>
   <body>
     <?php include_component('iceBackendModule', 'body', array('content' => $sf_content)); ?>
 
     <?php
-      sfConfig::set('symfony.asset.javascripts_included', true);
-      $js = @implode(',', array_keys($sf_response->getJavascripts()));
-
-      if (!empty($js))
-      {
-        echo '<script type="text/javascript" src="/combine.php?type=javascript&files='. $js .'&revision='. SVN_REVISION .'"></script>';
-      }
+      // Include all external javascript files
+      cq_include_javascripts();
 
       // Echo all the javascript for the page
       cq_echo_javascripts();
