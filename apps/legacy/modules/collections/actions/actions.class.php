d5d6e61b6c5098ad60b6f71ff4f7c456a60e8b52
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-03-01 12:49:56 +0200

    Added infinite scroll only when 'show all' selected
    Implemented infinitescroll for collections
    
    Removed wrong placed infinite scroll
    
    Fixed pager

--- apps/legacy/modules/collections/actions/actions.class.php
+++ apps/legacy/modules/collections/actions/actions.class.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 +<?php
 
 /**
@@ -99,3 +100,107 @@ class collectionsActions extends cqActions
     return sfView::SUCCESS;
   }
 }
+=======
+<?php
+
+/**
+ * collections actions.
+ *
+ * @package    CollectorsQuest
+ * @subpackage collections
+ * @author     Kiril Angov
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class collectionsActions extends cqActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->addBreadcrumb($this->__('Community'), '@community');
+    $this->addBreadcrumb($this->__('Collections'), '@collections');
+
+    $this->prependTitle($this->__('Collections'));
+
+    $c = new Criteria();
+    $c->setDistinct();
+    $c->add(CollectorCollectionPeer::NUM_ITEMS, 0, Criteria::GREATER_THAN);
+    $c->addJoin(CollectorCollectionPeer::COLLECTOR_ID, CollectorPeer::ID, Criteria::LEFT_JOIN);
+
+    if ($filter = $request->getParameter('filter'))
+    {
+      switch($filter)
+      {
+        case 'most-popular':
+          $this->addBreadcrumb($this->__('Most Popular'));
+          $this->prependTitle($this->__('Most Popular'));
+
+          $c->add(CollectorCollectionPeer::NUM_VIEWS, 100, Criteria::GREATER_EQUAL);
+          $c->addDescendingOrderByColumn(CollectorCollectionPeer::NUM_VIEWS);
+          break;
+        case 'most-talked-about':
+          $this->addBreadcrumb($this->__('Most Talked-About'));
+          $this->prependTitle($this->__('Most Talked-About'));
+
+          $c->add(CollectorCollectionPeer::NUM_COMMENTS, 0, Criteria::GREATER_THAN);
+          $c->addDescendingOrderByColumn(CollectorCollectionPeer::NUM_COMMENTS);
+          break;
+        case 'most-recent':
+        default:
+          $this->addBreadcrumb($this->__('Most Recent'));
+          $this->prependTitle($this->__('Most Recent'));
+
+          $c->addDescendingOrderByColumn(CollectorCollectionPeer::CREATED_AT);
+          break;
+      }
+
+      $this->filter = $filter;
+    }
+    else if ($collector_slug = $request->getParameter('collector'))
+    {
+      if ($collector = CollectorPeer::retrieveBySlug($collector_slug))
+      {
+        $c->add(CollectorPeer::SLUG, $collector_slug);
+        $c->addDescendingOrderByColumn(CollectorCollectionPeer::CREATED_AT);
+
+        $this->addBreadcrumb(sprintf($this->__('Collections of %s'), $collector->getDisplayName()), '@collections_by_collector='. $collector_slug);
+        $this->prependTitle(sprintf($this->__('Collections of %s'), $collector->getDisplayName()));
+      }
+    }
+    else if ($tag = $request->getParameter('tag'))
+    {
+      $c->add(iceModelTagPeer::NAME, $tag);
+      $c->addJoin(CollectorCollectionPeer::ID, iceModelTaggingPeer::TAGGABLE_ID, Criteria::LEFT_JOIN);
+      $c->addJoin(iceModelTaggingPeer::TAG_ID, iceModelTagPeer::ID, Criteria::LEFT_JOIN);
+
+      $this->addBreadcrumb(ucwords(strtolower($tag)), '@collections_by_tag='. $tag);
+      $this->prependTitle(ucwords(strtolower($tag)));
+    }
+    else
+    {
+      $c->addDescendingOrderByColumn(CollectorCollectionPeer::UPDATED_AT);
+    }
+
+    $per_page = sfConfig::get('app_pager_list_collections_max', 15);
+//    if (true || $this->getUser()->isAuthenticated()) $per_page += 1;
+
+    $pager = new sfPropelPager('CollectorCollection', $per_page);
+    $pager->setCriteria($c);
+    $pager->setMaxPerPage($per_page);
+
+    // Added By Prakash Panchal On 31-Mar-2011.
+    $snPage = ($this->getRequestParameter('jpage')) ? $this->getRequestParameter('jpage', 1) : $this->getRequestParameter('page', 1);
+
+    $pager->setPage($snPage);
+    $pager->init();
+
+    $this->pager = $pager;
+    $this->display = $this->getUser()->getAttribute('display', 'grid', 'collections');
+
+    return sfView::SUCCESS;
+  }
+}
+>>>>>>> Implemented infinitescroll for collections
--- apps/legacy/modules/collections/templates/indexSuccess.php
+++ apps/legacy/modules/collections/templates/indexSuccess.php
@@ -1,56 +1,37 @@
-<br clear="all" /><br>
+<br clear="all" /><br />
 
 <?php
-  $offset = 0;
-  $collections = $pager->getResults();
-  foreach ($collections as $i => $collection)
-  {
-    // Show the collection (in grid, list or hybrid view)
-    include_partial(
-      'collections/'. $display .'_view_collection',
-      array(
-        'collection' => $collection,
-        'culture' => $sf_user->getCulture(),
-        'i' => $i
-      )
-    );
-
-    if (false && !$sf_user->isAuthenticated())
-    {
-      // Serving ads or showing of a related blog post
-      if ('list' == $display && ($i + $offset) == 1)
-      {
-        !empty($blog_post) ?
-          include_partial('collections/list_view_blog_post', array('blog_post' => $blog_post)) :
-          include_partial('collections/list_view_ad');
-
-        $offset++;
-      }
-      else if ('grid' == $display && ($i + $offset) == 4)
-      {
-        !empty($blog_post) ?
-          include_partial('collections/grid_view_blog_post', array('blog_post' => $blog_post)) :
-          include_partial('collections/grid_view_ad');
-
-        $offset++;
-      }
-      else if ('hybrid' == $display && ($i + $offset) == 4)
-      {
-        !empty($blog_post) ?
-          include_partial('collections/hybrid_view_blog_post', array('blog_post' => $blog_post)) :
-          include_partial('collections/hybrid_view_ad');
-
-        $offset++;
-      }
-    }
-
-    echo (($i+$offset+1)%3==0)?'<br clear="all">':'';
-  }
+/* @var $pager sfPropelPager */
+/* @var $sf_user cqUser */
+$offset = 0;
+$collections = $pager->getResults();
 ?>
+<div id="collections">
+  <?php foreach ($collections as $i => $collection): ?>
+  <?php
+  // Show the collection (in grid, list or hybrid view)
+  include_partial(
+    'collections/' . $display . '_view_collection',
+    array(
+      'collection' => $collection,
+      'culture'    => $sf_user->getCulture(),
+      'i'          => $i
+    )
+  ); ?>
 
-<br clear="all">
-<div class="span-19 last" style="margin-bottom: 25px">
-  <?php include_partial('global/pager', array('pager' => $pager)); ?>
+  <?php if (0 == ($i + $offset + 1) % 3): ?>
+    <br clear="all" />
+    <?php endif; ?>
+  <?php endforeach; ?>
+
+  <br clear="all" />
+
+  <div class="span-19 last" style="margin-bottom: 25px">
+    <?php include_partial('global/pager', array(
+    'pager'  => $pager,
+    'options'=> array('url'   => '@collections_by_filter?filter=' . $filter)
+  )); ?>
+  </div>
 </div>
 
 <?php if (!$sf_user->isAuthenticated()): ?>
@@ -58,3 +39,4 @@
   <?php cq_ad_slot('collectorsquest_com_-_After_Listing_728x90', '728', '90'); ?>
 </div>
 <?php endif; ?>
+
