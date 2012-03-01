b9f2ebd02d9fa3eaaacac0ca0ba7265b5f200ffd
Teomarino <teo.marinov@gmail.com>
2012-03-01 14:59:50 +0200

    Fix spaminess styles in backend

--- apps/backend/modules/collectors/templates/_list_spaminess.php
+++ apps/backend/modules/collectors/templates/_list_spaminess.php
@@ -1,4 +1,8 @@
 <?php
+  /**
+   * @var $Collector Collector
+   */
+
   $spam_score = (float) $Collector->getSpamScore();
 
   if ($spam_score == 0)
@@ -7,14 +11,14 @@
   }
   else if ($spam_score <= 30)
   {
-    echo '<center><span class="label success">', $spam_score, '%</span></center>';
+    echo '<center><span class="label label-success">', $spam_score, '%</span></center>';
   }
   else if ($spam_score <= 60)
   {
-    echo '<center><span class="label warning">', $spam_score, '%</span></center>';
+    echo '<center><span class="label label-warning">', $spam_score, '%</span></center>';
   }
   else
   {
-    echo '<center><span class="label important">', $spam_score, '%</span></center>';
+    echo '<center><span class="label label-important">', $spam_score, '%</span></center>';
   }
 ?>
