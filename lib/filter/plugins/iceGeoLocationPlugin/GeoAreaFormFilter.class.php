20d187dc51ca325cde87ffb3b100f9b74f74a4f2
Kiril Angov <kiril.angov@gmail.com>
2012-03-02 17:13:49 +0200

    Enabling iceGeoLocationPlugin and rebuilding Propel classes

---
+++ lib/filter/plugins/iceGeoLocationPlugin/GeoAreaFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * GeoArea filter form.
+ *
+ * @package    CollectorsQuest
+ * @subpackage filter
+ * @author     Collectors
+ */
+class GeoAreaFormFilter extends BaseGeoAreaFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/plugins/iceGeoLocationPlugin/GeoCountryForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * GeoCountry form.
+ *
+ * @package    CollectorsQuest
+ * @subpackage form
+ * @author     Collectors
+ */
+class GeoCountryForm extends BaseGeoCountryForm
+{
+  public function configure()
+  {
+  }
+}
