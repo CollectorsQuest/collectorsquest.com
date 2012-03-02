5c3030a62df7ea54c5bdecd8f9dc2545551a15be
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-03-02 10:51:38 +0200

    Fixed styles of spaminess column

--- apps/backend/modules/collectors/templates/_list_spaminess.php
+++ apps/backend/modules/collectors/templates/_list_spaminess.php
@@ -1,24 +1,28 @@
 <?php
-  /**
-   * @var $Collector Collector
-   */
+/**
+ * @var $Collector Collector
+ */
 
-  $spam_score = (float) $Collector->getSpamScore();
+$spam_score = (float)$Collector->getSpamScore();
 
-  if ($spam_score == 0)
-  {
-    echo '<center><span class="label">', $spam_score, '</span></center>';
-  }
-  else if ($spam_score <= 30)
-  {
-    echo '<center><span class="label label-success">', $spam_score, '%</span></center>';
-  }
-  else if ($spam_score <= 60)
-  {
-    echo '<center><span class="label label-warning">', $spam_score, '%</span></center>';
-  }
-  else
-  {
-    echo '<center><span class="label label-important">', $spam_score, '%</span></center>';
-  }
+switch ($spam_score)
+{
+  case 0:
+    $class = '';
+    break;
+
+  case $spam_score <= 30:
+    $class = 'label-success';
+    break;
+
+  case $spam_score <= 60:
+    $class = 'label-warning';
+    break;
+
+  case $spam_score > 60:
+    $class = 'label-important';
+    break;
+}
+
 ?>
+<span class="label <?php echo $class ?>"><?php echo $spam_score; ?>%</span>
