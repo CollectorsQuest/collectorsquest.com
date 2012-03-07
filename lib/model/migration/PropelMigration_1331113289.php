320089d6234d7bcce21ce6861a16cba53257bfd4
Kiril Angov <kiril.angov@gmail.com>
2012-03-07 13:07:36 +0200

    Adding empty migration placeholder for the next database changes

---
+++ lib/model/migration/PropelMigration_1331113289.php
@@ -0,0 +1,61 @@
+<?php
+
+class PropelMigration_1331113289
+{
+	public function preUp($manager)
+	{
+
+	}
+
+	public function postUp($manager)
+	{
+		// add the post-migration code here
+	}
+
+	public function preDown($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postDown($manager)
+	{
+
+	}
+
+	/**
+	 * Get the SQL statements for the Up migration
+	 *
+	 * @return array list of the SQL strings to execute for the Up migration
+	 *               the keys being the datasources
+	 */
+	public function getUpSQL()
+	{
+		return array (
+      'propel' => "
+        SET FOREIGN_KEY_CHECKS = 0;
+
+
+
+        SET FOREIGN_KEY_CHECKS = 1;
+      ",
+    );
+	}
+
+	/**
+	 * Get the SQL statements for the Down migration
+	 *
+	 * @return array list of the SQL strings to execute for the Down migration
+	 *               the keys being the datasources
+	 */
+	public function getDownSQL()
+	{
+		return array (
+      'propel' => "
+        SET FOREIGN_KEY_CHECKS = 0;
+
+        SET FOREIGN_KEY_CHECKS = 1;
+      ",
+    );
+	}
+
+}
