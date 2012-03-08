d0ac2de3207e7172c991d2a86e6ca91c2d0dd2b7
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-03-08 17:25:00 +0200

    Removed old commented code

--- apps/legacy/modules/seller/templates/packagesSuccess.php
+++ apps/legacy/modules/seller/templates/packagesSuccess.php
@@ -43,17 +43,7 @@
     </td>
     <td width="50%" valign="top">
       <form action="" name="frmpackage" id="frmpackage" method="post">
-        <?php
-        $packagesForm->renderHiddenFields();
-// Seller Details
-//        echo input_hidden_tag('user_type', 'Seller');
-//        echo input_hidden_tag('items_allowed');
-//        echo input_hidden_tag('package_price');
-//        echo input_hidden_tag('package_name');
-//        echo input_hidden_tag('free_subscription', $freeSubscription);
-//        echo input_hidden_tag('commit');
-//        echo input_hidden_tag('type', $sf_request->getParameter('type'));
-        ?>
+        <?php $packagesForm->renderHiddenFields(); ?>
         <table width="100%" border="0" cellspacing="0" cellpadding="0" class="offerright">
           <tr>
             <td>
@@ -62,72 +52,6 @@
                 <table width="100%" border="0" cellspacing="0" cellpadding="0">
                   <?php echo $packagesForm['package_id']->renderError(); ?>
                   <?php echo $packagesForm['package_id']->render(); ?>
-                  <?php /*if ($sf_user->hasFlash('msg_package')): ?>
-                  <tr>
-                    <td colspan="2">
-                      <ul class="error_list"><li><?php echo $sf_user->getFlash('msg_package') ?></li></ul>
-                    </td>
-                  </tr>
-                <?php endif; ?>
-                <tr>
-                  <td colspan="2"><h5>Casual Plan - Choose One</h5></td>
-                </tr>
-                <?php
-                if ($omPackages):
-                  $bFlag = 1;
-                  foreach ($omPackages as $asValues):
-                    if ($asValues['PLAN_TYPE'] == "Casual"):
-                      ?>
-                      <tr>
-                        <td colspan="2">
-                          <table width="100%" cellpadding="5" cellspacing="5">
-                            <tr>
-                              <td>
-                                <?php
-                                echo radiobutton_tag(
-                                  'package_id', $asValues['ID'], $sf_request->getParameter('package_id') == $asValues['ID'], array('id' => 'package_id_' . $asValues['ID'], 'onclick' => 'setPackageInformation(' . $asValues['MAX_ITEMS_FOR_SALE'] . ', this.value,' . $asValues['PACKAGE_PRICE'] . ',"' . $asValues['PACKAGE_NAME'] . '");'));
-                                ?>
-                              </td>
-                              <td width="5px"></td>
-                              <td>
-                                <?php echo money_format('%.2n', $asValues['PACKAGE_PRICE']); ?> -
-                                <?php echo $asValues['PACKAGE_NAME']; ?>
-                              </td>
-                            </tr>
-                          </table>
-                        </td>
-                      </tr>
-                    <?php else:
-                      if ($bFlag == 1): ?>
-                        <tr>
-                          <td colspan="2"><br /><h5>Power Plan - Annual Fee</h5></td>
-                        </tr>
-                        <?php $bFlag = 0;
-                      endif; ?>
-                      <tr>
-                        <td colspan="2">
-                          <table width="100%" cellpadding="5" cellspacing="5">
-                            <tr>
-                              <td>
-                                <?php
-                                echo radiobutton_tag(
-                                  'package_id', $asValues['ID'], $sf_request->getParameter('package_id') == $asValues['ID'], array('id' => 'package_id_' . $asValues['ID'], 'onclick' => 'setPackageInformation(' . $asValues['MAX_ITEMS_FOR_SALE'] . ', this.value, ' . $asValues['PACKAGE_PRICE'] . ',"' . $asValues['PACKAGE_NAME'] . '");'
-                                ));
-                                ?>
-                              </td>
-                              <td width="5px"></td>
-                              <td>
-                                <?php echo money_format('%.2n', $asValues['PACKAGE_PRICE']); ?> -
-                                <?php echo $asValues['PACKAGE_NAME']; ?>
-                              </td>
-                            </tr>
-                          </table>
-                        </td>
-                      </tr>
-                    <?php
-                    endif;
-                  endforeach;
-                endif; */
                   ?>
                   <tr>
                     <td><h5 class="title_h5">Promotion Code: </h5></td>
@@ -145,183 +69,27 @@
                 <?php if (!$freeSubscription): ?>
                 <?php /* @var $paymentType sfFormField */ ?>
                 <?php echo $packagesForm['payment_type']->render(array('style'=> 'height: 24px;')); ?>
-                <?php /* ?>
-  <tr>
-    <td colspan="2">
-      <input type="radio" name="payment_type" id="payment_paypal" value="paypal" />
-      <label for="payment_paypal">
-        <?php echo image_tag('legacy/payment/paypal.gif', array('style' => 'height: 24px;')) ?>
-      </label>
-    </td>
-  </tr>
-  <tr>
-    <td colspan="2" style="vertical-align: middle!important;">
-      <input type="radio" name="payment_type" id="payment_cc" value="package" checked="checked" />
-      <label for="payment_cc">
-        <?php echo image_tag('legacy/payment/cc.gif', array('style' => 'height: 24px;')) ?>
-      </label>
-    </td>
-  </tr>
- <?php */
                 ?>
                 <table id="fieldset_cc">
                   <?php echo $packagesForm['card_type']->renderRow() ?>
-                  <?php echo $packagesForm['cc_number']->renderRow() ?>
-                  <?php echo $packagesForm['expiry_date']->renderRow() ?>
-                  <?php echo $packagesForm['cvv_number']->renderRow(); ?>
-                  <?php $ccFields = array(
+                <?php echo $packagesForm['cc_number']->renderRow() ?>
+                <?php echo $packagesForm['expiry_date']->renderRow() ?>
+                <?php echo $packagesForm['cvv_number']->renderRow(); ?>
+                <?php $ccFields = array(
                   'first_name', 'last_name', 'street', 'city', 'state', 'zip', 'country',
                 ); ?>
-                  <?php foreach ($ccFields as $field): ?>
+                <?php foreach ($ccFields as $field): ?>
                   <?php echo $packagesForm[$field]->renderRow(); ?>
                   <?php endforeach; ?>
-                  <tr>
-                    <td colspan="2">
-                      <?php if ($packagesForm['term_condition']->hasError()): ?>
-                      <?php echo $packagesForm['term_condition']->renderError() ?>
-                      <?php endif; ?>
-                      <?php echo $packagesForm['term_condition']->render() ?>&nbsp;<label for="<?php echo $packagesForm['term_condition']->renderId() ?>">I accept the
-                      <a href="/blog/terms-and-conditions/" title="terms and conditions" target="_blank">terms and conditions</a> set forth by this site.</label>
-                    </td>
-                  </tr>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valign">
-        Card Type:
-      </div>
-    </td>
-    <td>
-      <?php
-      $amCardType = array(
-        'Visa'             => 'Visa',
-        'MasterCard'       => 'MasterCard',
-        'Discover'         => 'Discover',
-        'American Express' => 'American Express'
-      );
-      echo select_tag('card_type', options_for_select($amCardType, $sf_request->getParameter('card_type')), array('style' => "width:150px;"));
-      ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">Credit Card Number:</div>
-    </td>
-    <td>
-      <?php echo input_tag('credit_card_number', $sf_request->getParameter('credit_card_number'), array('style' => "width:142px;")); ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valign">Expiration Date:</div>
-    </td>
-    <td>
-      <?php
-      $anMonths = $anYears = array();
-      for ($snI = 1; $snI <= 12; $snI++)
-        $anMonths[$snI] = $snI;
-      for ($snI = date('Y'); $snI < date('Y') + 10; $snI++)
-        $anYears[$snI] = $snI;
-
-      echo select_tag('expiry_date_month', options_for_select($anMonths, $sf_request->getParameter('expiry_date_month')), array('style' => 'width:50px;')) . '&nbsp;&nbsp;';
-      echo select_tag('expiry_date_year', options_for_select($anYears, $sf_request->getParameter('expiry_date_year')), array('style' => 'width:92px;'));
-      ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">Card Verification Number:</div>
-    </td>
-    <td>
-      <?php echo input_tag('cvv_number', $sf_request->getParameter('cvv_number'), array(
-      'maxlength' => 3,
-      'style'     => "width:142px;"
-    )); ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">First name:</div>
-    </td>
-    <td>
-      <?php echo input_tag('first_name', $sf_request->getParameter('first_name'), array('style' => "width:142px;")) ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">Last name:</div>
-    </td>
-    <td>
-      <?php echo input_tag('last_name', $sf_request->getParameter('last_name'), array('style' => "width:142px;")) ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">Street:</div>
-    </td>
-    <td>
-      <?php echo input_tag('street', $sf_request->getParameter('street'), array('style' => "width:142px;")) ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">City:</div>
-    </td>
-    <td>
-      <?php echo input_tag('city', $sf_request->getParameter('city'), array('style' => "width:142px;")) ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">State:</div>
-    </td>
-    <td>
-      <?php echo input_tag('state', $sf_request->getParameter('state'), array('style' => "width:142px;")) ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">Zip:</div>
-    </td>
-    <td>
-      <?php echo input_tag('zip', $sf_request->getParameter('zip'), array('style' => "width:142px;")) ?>
-    </td>
-  </tr>
- */ ?>
-                  <?php /*
-  <tr>
-    <td>
-      <div class="text_valignnew">Country:</div>
-    </td>
-    <td>
-      <?php
-      echo $packagesForm['country']->render(array('style'=> 'width: 142px;'));
-      //                        $country = new sfWidgetFormI18nChoiceCountry(array('choices' => $countries, 'add_empty' => true), array('style' => 'width: 142px;')) ;
-      //                        $country->setDefault($sf_request->getParameter('country'));
-      //                        echo $country->render('country');
-      ?>
-    </td>
-  </tr>
- */ ?>
-                </table>
+                <tr>
+                  <td colspan="2">
+                    <?php if ($packagesForm['term_condition']->hasError()): ?>
+                    <?php echo $packagesForm['term_condition']->renderError() ?>
+                    <?php endif; ?>
+                    <?php echo $packagesForm['term_condition']->render() ?>&nbsp;<label for="<?php echo $packagesForm['term_condition']->renderId() ?>">I accept the
+                    <a href="/blog/terms-and-conditions/" title="terms and conditions" target="_blank">terms and conditions</a> set forth by this site.</label>
+                  </td>
+                </tr>
                 <?php if ($sf_user->hasFlash('msg_payment')): ?>
                   <table>
                     <tr>
@@ -333,19 +101,6 @@
                     </tr>
                   </table>
                   <?php endif; ?>
-                <?php /*
-                    <tr>
-                    <td>
-                    <br /><h5>OR Pay Via PayPal</h5>
-                    </td>
-                    </tr>
-                    <tr>
-                    <td>
-                    <div style="float: left;"><?php echo image_tag('legacy/btn_xpressCheckout.gif', array('onclick' => "getpayment('paypal');return false;", 'style' => 'cursor:pointer;float:right;')); ?></div>
-                    </td>
-                    </tr>
-                   */
-                ?>
                 <?php endif; ?>
                 <h5 style="margin-top: 10px;">* To avoid interruption of service, annual subscriptions
                   automatically renew at the end of the subscription period</h5>
@@ -353,23 +108,6 @@
               <button type="submit" class="submit" style="margin-left: 190px;">
                 <span><span>Sign up</span></span>
               </button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <?php /*if ($freeSubscription): ?>
-    <button type="submit" value="Sign up" class="submit" style="margin-left:190px;" onclick="freeSubscription('package');return false;">
-      <span><span>Sign up</span></span>
-    </button>
-    <?php else: ?>
-    <button type="submit" value="Sign up" class="submit" style="margin-left:190px;" onclick="getpayment($('input[name*=payment_type]:checked').val()); return false;">
-      <span><span>Sign up</span></span>
-    </button>
-    <?php endif; ?>
-    <noscript style="text-align:center;">
-      <span style="color:#FF0000; font-weight:bold; text-align:center;">Your browser detect disable javascript! Please enable it!</span>&nbsp;
-    </noscript>
-    */ ?>
             </td>
           </tr>
         </table>
