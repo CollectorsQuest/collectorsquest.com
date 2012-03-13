88d583f20176de47604ac97a4f0c643ddd63cde8
Kiril Angov <kiril.angov@gmail.com>
2012-03-13 15:30:39 +0200

    Adding a copy and paste menu for now but with the right navigation items

--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -1,17 +1,25 @@
-<div class="navbar">
-  <div><!--class="navbar-inner"-->
-    <div class="container-fluid container-fluid-header-fix">
+<a class="brand" href="#"><!-- Logo -->
+  <img src="/images/frontend/dev/cq-logo.png" alt="Collectors' Quest">
+</a>
+
+<div class="navbar" style="width: 450px;">
+  <div class="navbar-inner">
+    <div class="container">
       <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </a>
-      <a class="brand" href="#"><!-- Logo -->
-        <img src="/images/frontend/dev/cq-logo.png" alt="Collectors' Quest" >
-      </a>
-      <div class="nav-collapse">
-        <!-- nav -->
-      </div><!--/.nav-collapse -->
+
+      <div class="nav-collapse collapse" style="height: 0px; ">
+        <ul class="nav">
+          <li class="active"><a href="#">HOME</a></li>
+          <li><a href="#">COLLECTIONS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">VIDEO</a></li>
+          <li><a href="#">MARKET</a></li>
+        </ul>
+      </div><!-- /.nav-collapse -->
     </div>
-  </div>
+  </div><!-- /navbar-inner -->
 </div>
