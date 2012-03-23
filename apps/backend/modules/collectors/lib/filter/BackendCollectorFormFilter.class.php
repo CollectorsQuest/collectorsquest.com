746e0d2993b212d23c2fe3ec29b7f50d21c6c1e0
Kiril Angov <kiril.angov@gmail.com>
2012-03-23 12:12:27 +0200

    Making use of the setUpXXXField() semantics

--- apps/backend/modules/collectors/lib/filter/BackendCollectorFormFilter.class.php
+++ apps/backend/modules/collectors/lib/filter/BackendCollectorFormFilter.class.php
@@ -7,10 +7,19 @@ class BackendCollectorFormFilter extends CollectorFormFilter
   {
     parent::configure();
 
+    $this->setupUsernameField();
+    $this->setupDisplayNameField();
+  }
+
+  public function setupUsernameField()
+  {
     $this->widgetSchema['username'] = new BackendWidgetFormModelTypeAhead(array(
       'field' => CollectorPeer::USERNAME
     ));
+  }
 
+  public function setupDisplayNameField()
+  {
     $this->widgetSchema['display_name'] = new BackendWidgetFormModelTypeAhead(array(
       'field' => CollectorPeer::DISPLAY_NAME
     ));
