463024cc68eb2372168ee44a8fbd27fdf86bb37f
Kiril Angov <kiril.angov@gmail.com>
2012-03-26 14:39:40 +0300

    Loading google fonts with JS and creating special class .FugazOne for the custom font we will be using Some reorganization in main.less

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -2,7 +2,7 @@
   <div class="footer-inner">
     <div class="row-fluid">
       <div class="span4">
-        <h2>About Collectors’ Quest</h2>
+        <h2 class="FugazOne">About Collectors’ Quest</h2>
         <p>
           Collectors’ Quest is an interactive community and marketplace for the passionate collector. Collectors can meet others who share their interests, organize and catalog their collections, as well as buy, sell or trade with others...
           <a href="#">Learn more &raquo;</a>
@@ -14,7 +14,7 @@
         <p>Follow us on Twitter</p>
       </div><!--/span-->
       <div class="span4">
-        <h2>Sign Up</h2>
+        <h2 class="FugazOne">Sign Up</h2>
 
         <form class="form-horizontal">
           <fieldset>
@@ -53,15 +53,15 @@
       <div class="span4">
         <ul class="footer-info-box">
           <li class="show-off">
-            <h2>Show Off</h2>
+            <h2 class="FugazOne">Show Off</h2>
             <p>Share your passion with a world of interested people by organizing your collections with our easy to use tools.</p>
           </li>
           <li class="piggy_bank">
-            <h2>Get Paid</h2>
+            <h2 class="FugazOne">Get Paid</h2>
             <p>It’s easy to sell an item once you’re a member. Just choose “I’m a seller” during the sign up process.</p>
           </li>
           <li class="faq">
-            <h2>Help/FAQ</h2>
+            <h2 class="FugazOne">Help/FAQ</h2>
             <p>Want to know how to get more out of your membership? <a href="#">Watch</a> our helpful videos today!</p>
           </li>
         </ul>
