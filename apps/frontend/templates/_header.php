14920dd9bf5b3571a942ccca1b6c5bd5136b5e5f
Teomarino <teo.marinov@gmail.com>
2012-03-26 17:36:38 +0300

    Update the navbar. [TODO: http://goo.gl/PVFtG]

--- apps/frontend/templates/_header.php
+++ apps/frontend/templates/_header.php
@@ -18,9 +18,39 @@
         </div>
       </div>
   </div><!-- /navbar-inner -->
+
+  <div class="navbar">
+    <div class="navbar-inner">
+      <div class="container">
+        <a href="/" alt="Collectors Quest" title="Home page" class="cq-logo logo ir"></a>
+        <ul class="nav">
+          <li>
+            <a href="<?= url_for('@homepage'); ?>" style="padding: 9px 10px;">
+              <i class="icon-home icon-white"></i>
+            </a>
+          </li>
+          <li><?= link_to('Collections', 'collections/index'); ?></li>
+          <li><?= link_to('News', 'news/index'); ?></li>
+          <li><?= link_to('Video', 'video/index'); ?></li>
+          <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">Dropdown <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Action</a></li>
+              <li><a href="#">Another action</a></li>
+              <li><a href="#">Something else here</a></li>
+              <li class="divider"></li>
+              <li class="nav-header">Nav header</li>
+              <li><a href="#">Separated link</a></li>
+              <li><a href="#">One more separated link</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </header>
 
-<!-- old menu -->
+<?php /*
 <nav id="nav" role="navigation">
   <a href="/" alt="Collectors Quest" title="Home page" class="cq-logo logo ir"></a>
   <div class="menu-wrapper cf">
@@ -37,7 +67,8 @@
     </ul>
   </div>
 </nav>
-<!-- /old menu -->
+*/?>
+
 <script>
   $(document).ready(function()
   {
