b06d9dc93c552835bccc2929944854367f1c40e7
Kiril Angov <kiril.angov@gmail.com>
2012-03-28 15:20:04 +0300

    cqPatternRoutingTest.php is now working correctly

--- test/unit/cq/cqPatternRoutingTest.php
+++ test/unit/cq/cqPatternRoutingTest.php
@@ -15,16 +15,16 @@ $t = new lime_test(5, array('output' => new lime_output_color(), 'error_reportin
   $t->is(substr($url, 0, 7), '/ex/v1;', 'Testing the passing of GET parameters with encryption');
 
   $url = url_for('@collection_by_slug?id=1&slug=slideonover&encrypt=1', true);
-  $t->is(substr($url, 0, 7), 'http://', 'Testing the passing of GET parameters with encryption and absolute');
+  $t->is(substr($url, 0, 29), 'http://www.example.com/ex/v1;', 'Testing the passing of GET parameters with encryption and absolute');
 
   $url = url_for('@collection_by_slug?id=1&slug=slideonover&encrypt=1', false);
-  $t->is($routing->decryptUrl($url), 'test/unit/cq/collection/1/slideonover', 'Testing the decryption');
+  $t->is($routing->decryptUrl($url), '/collection/1/slideonover', 'Testing the decryption');
 
   $url = url_for('@collection_by_slug?id=1&slug=slideonover&encrypt=1', false);
   $_SERVER['QUERY_STRING'] = 'token=EC-22W34314DU597432P&PayerID=84BQLBU43QAB8';
   $t->is(
     $routing->decryptUrl($url),
-    'test/unit/cq/collection/1/slideonover',
+    '/collection/1/slideonover',
     'Testing the decryption with extra query string'
   );
 
@@ -32,6 +32,6 @@ $t = new lime_test(5, array('output' => new lime_output_color(), 'error_reportin
   $_SERVER['QUERY_STRING'] = 'token=EC-22W34314DU597432P&PayerID=84BQLBU43QAB8';
   $t->is(
     $routing->decryptUrl($url),
-    'test/unit/cq/collection/1/slideonover',
+    '/collection/1/slideonover',
     'Testing the decryption with extra query string'
   );
