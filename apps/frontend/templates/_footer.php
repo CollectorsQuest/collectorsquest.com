7d39822b1f243c3dc031447c4718a30fac1240c9
Teomarino <teo.marinov@gmail.com>
2012-03-31 23:44:13 +0300

    Update footer

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,80 +1,179 @@
-<footer id="footer" style="position: relative;">
+<footer id="footer">
   <div class="footer-inner">
     <div class="row-fluid">
       <div class="span4">
-        <h2 class="FugazOne">About Collectors’ Quest</h2>
-        <p>
-          Collectors’ Quest is an interactive community and marketplace for the passionate collector. Collectors can meet others who share their interests, organize and catalog their collections, as well as buy, sell or trade with others...
-          <a href="#">Learn more &raquo;</a>
-        </p>
-        <div class="form-actions">
-          <button class="btn btn-primary" type="submit">Contact us</button>
+        <div class="aboutus-footer-inner">
+          <h2 class="FugazOne">About Collectors’ Quest</h2>
+          <p>
+            Collectors’ Quest is an interactive community and marketplace for the passionate collector. Collectors can meet others who share their interests, organize and catalog their collections, as well as buy, sell or trade with others...
+            <a href="#">Learn more</a>
+          </p>
+          <div class="row-spacing-footer">
+            <button class="btn btn-primary blue pull-left" type="submit">Contact us</button>
+          </div>
+          <p><a href="#"><i class="s-16-icon-facebook ico-16px-alignment"></i> Follow us on Facebook</a></p>
+          <p><a href="#" ><i class="s-16-icon-twitter ico-16px-alignment"></i> Follow us on Twitter</a></p>
         </div>
-        <p>Follow us on Facebook</p>
-        <p>Follow us on Twitter</p>
       </div><!--/span-->
 
       <div class="span4">
-      <?php if (!$sf_user->isAuthenticated()): ?>
 
-        <div id="footer-form-signup">
-          <h2 class="FugazOne">Sign Up</h2>
-
-          <?= form_tag('@collector_signup', array('class' => 'form-horizontal')); ?>
-            <fieldset>
-              <?= new CollectorSignupStep1Form(); ?>
-              <div class="form-actions">
-                <input type="submit" class="btn btn-primary" value="Sign Up" />
-              </div>
-            </fieldset>
-          </form>
-
-          <div id="footer-control-login">
-            Already have an account? <?= link_to('Log In', '@login', array('id' => 'footer-control-login-button')); ?>
+<?php /* footer-form-signup
+        <h2 class="FugazOne">Sign Up</h2>
+        <form class="form-horizontal">
+        <div class="row-fluid row-spacing">
+          <div class="span4 v-center-container-label">
+            <span class="v-center"><label for="focusedInput" class="control-label">Full&nbsp;<br>Name</label></span>
           </div>
-        </div><!-- #footer-form-signup -->
-
-        <div id="footer-form-login" style="display: none">
-          <h2 class="FugazOne">Log In</h2>
-          <?= form_tag('@login', array('class' => 'form-horizontal')) ?>
-            <fieldset>
-              <?= new CollectorLoginForm() ?>
-              <div class="form-actions">
-                <input type="submit" class="btn btn-primary" value="Log In" />
-              </div>
-            </fieldset>
-          </form>
-
-          <div id="footer-control-signup" style="display: none">
-            Don't have an account yet? <?= link_to('Sign up', '@collector_signup', array('id' => 'footer-control-signup-button')); ?>
+          <div class="span8"><input type="text" placeholder="Full name" id="focusedInput" class="input-footer focused"></div>
+        </div>
+        <div class="row-fluid row-spacing">
+          <div class="span4 v-center-container-label">
+            <span class="v-center"><label for="focusedInput1" class="control-label">Email</label></span>
           </div>
-        </div> <!-- #footer-form-login -->
+          <div class="span8"><input type="text" placeholder="Email" id="focusedInput1" class="input-footer focused"></div>
+        </div>
+        <div class="row-fluid row-spacing">
+          <div class="span4 v-center-container-label">
+            <span class="v-center"><label for="focusedInput2" class="control-label">Password</label></span>
+          </div>
+          <div class="span8"><input type="text" placeholder="Password" id="focusedInput2" class="input-footer focused"></div>
+        </div>
+        <div class="row-fluid row-spacing">
+          <div class="span4 v-center-container-label">
+            <span class="v-center"><label for="focusedInput3" class="control-label">Confirm Password</label></span>
+          </div>
+          <div class="span8"><input type="text" placeholder="Password again…" id="focusedInput3" class="input-footer focused"></div>
+        </div>
 
-      <?php else: ?>
-        <!-- nothing here yet -->
-        <br />
-      <?php endif; ?>
+        <div class="row-fluid row-spacing">
+          <div class="span9 top-padding-10">
+            Sign up using&nbsp;
+            <a href="#" rel="tooltip" title="Sign up using facebook" class="s-16-icon-facebook">
+              <i class="hide-text">Sign up using facebook</i>
+            </a>
+            <a href="#" title="Sign up using twitter" class="s-16-icon-twitter">
+              <i class="hide-text">Sign up using twitter</i>
+            </a>
+            <a href="#" title="Sign up using google" class="s-16-icon-google">
+              <i class="hide-text">Sign up using google</i>
+            </a>
+          </div>
+          <div class="span3">
+            <button class="btn btn-primary blue pull-right" type="submit">Submit</button>
+          </div>
+        </div>
+        </form>
+        <div class="row-spacing pull-right">
+          Already have an account? <a href="#"><strong>Log in</strong></a>
+        </div>
+*/ ?>
+
+<?php /* footer-control-login
+
+        <h2 class="FugazOne">Sign In</h2>
+        <form class="form-horizontal">
+        <div class="row-fluid row-spacing">
+          <div class="span4 v-center-container-label">
+            <span class="v-center"><label for="focusedInput1" class="control-label">Email</label></span>
+          </div>
+          <div class="span8"><input type="text" placeholder="Email" id="focusedInput1" class="input-footer focused"></div>
+        </div>
+        <div class="row-fluid row-spacing">
+          <div class="span4 v-center-container-label">
+            <span class="v-center"><label for="focusedInput2" class="control-label">Password</label></span>
+          </div>
+          <div class="span8"><input type="text" placeholder="Password" id="focusedInput2" class="input-footer focused"></div>
+        </div>
+
+        <div class="row-fluid row-spacing">
+          <div class="span9 top-padding-10">
+            Sign in using&nbsp;
+            <a href="#" rel="tooltip" title="Sign up using facebook" class="s-16-icon-facebook">
+              <i class="hide-text">Sign in using facebook</i>
+            </a>
+            <a href="#" title="Sign up using twitter" class="s-16-icon-twitter">
+              <i class="hide-text">Sign in using twitter</i>
+            </a>
+            <a href="#" title="Sign up using google" class="s-16-icon-google">
+              <i class="hide-text">Sign in using google</i>
+            </a>
+          </div>
+          <div class="span3">
+            <button class="btn btn-primary blue pull-right" type="submit">Submit</button>
+          </div>
+        </div>
+        </form>
+        <div class="row-fluid">
+          <div class="span12">
+          <a href="#" title="Forgot your password?" class="pull-right">Forgot your password?</a>
+          </div>
+        </div>
+        <div class="row-fluid">
+          <div class="span12 top-padding-double">
+          <p class="pull-right">Not a member yet? <a href="#"><strong>Log up</strong></a><p>
+          </div>
+        </div>
+        <!-- //footer-control-login -->
+*/ ?>
+        <h2 class="FugazOne">Welcome back, Robotbacon!</h2>
+        <ul class="footer-info-box">
+          <li>
+            <i class="icon_big_email"></i>
+            <div class="info-box-text">
+              <p>You have <a href="#">1 new message</a> in your inbox</p>
+            </div>
+          </li>
+          <li>
+            <i class="icon_big_battery"></i>
+            <div class="info-box-text">
+              <p>Your profile is 50% complete. <a href=""><strong>Add a collection</strong></a> in minutes. (+10%)</p>
+            </div>
+          </li>
+          <li>
+            <div class="row-fluid">
+              <div class="span6">
+                <i class="icon_big_add"></i>
+                <div class="info-box-text">
+                  <p><a href="#"><strong>Upload<br> a photo</strong></a></p>
+                </div>
+              </div>
+              <div class="span6">
+                <i class="icon_big_organize"></i>
+                <div class="info-box-text">
+                  <p><a href="#"><strong>Organize your<br> collection</strong></a></p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div class="row-fluid top-padding-10">
+          <div class="span12">
+            <button class="btn btn-primary blue" type="submit">My Profile</button>
+            <a href="#" title="Forgot your password?">Log Out?</a>
+          </div>
+        </div>
 
       </div><!-- .span4 -->
 
       <div class="span4">
         <ul class="footer-info-box">
           <li>
-            <span class="icon_big_box"></span>
+            <i class="icon_big_box"></i>
             <div class="info-box-text">
               <h2 class="FugazOne">Show Off</h2>
               <p>Share your passion with a world of interested people by organizing your collections with our easy to use tools.</p>
             </div>
           </li>
           <li>
-            <span class="icon_big_piggy_bank"></span>
+            <i class="icon_big_piggy_bank"></i>
             <div class="info-box-text">
               <h2 class="FugazOne">Get Paid</h2>
               <p>It’s easy to sell an item once you’re a member. Just choose “I’m a seller” during the sign up process.</p>
             </div>
           </li>
           <li>
-            <span class="icon_big_question"></span>
+            <i class="icon_big_question"></i>
             <div class="info-box-text">
               <h2 class="FugazOne">Help/FAQ</h2>
               <p>Want to know how to get more out of your membership? <a href="#">Watch</a> our helpful videos today!</p>
@@ -88,3 +187,46 @@
     <i class="icon-arrow-up"></i> Scroll<br/> to Top
   </a>
 </footer>
+
+
+<?php /*
+<?php if (!$sf_user->isAuthenticated()): ?>
+
+<div id="footer-form-signup">
+  <h2 class="FugazOne">Sign Up</h2>
+
+  <?= form_tag('@collector_signup', array('class' => 'form-horizontal')); ?>
+  <fieldset>
+    <?= new CollectorSignupStep1Form(); ?>
+    <div class="form-actions">
+      <input type="submit" class="btn btn-primary" value="Sign Up" />
+    </div>
+  </fieldset>
+  </form>
+
+  <div id="footer-control-login">
+    Already have an account? <?= link_to('Log In', '@login', array('id' => 'footer-control-login-button')); ?>
+  </div>
+</div><!-- #footer-form-signup -->
+
+<div id="footer-form-login" style="display: none">
+  <h2 class="FugazOne">Log In</h2>
+  <?= form_tag('@login', array('class' => 'form-horizontal')) ?>
+  <fieldset>
+    <?= new CollectorLoginForm() ?>
+    <div class="form-actions">
+      <input type="submit" class="btn btn-primary" value="Log In" />
+    </div>
+  </fieldset>
+  </form>
+
+  <div id="footer-control-signup" style="display: none">
+    Don't have an account yet? <?= link_to('Sign up', '@collector_signup', array('id' => 'footer-control-signup-button')); ?>
+  </div>
+</div> <!-- #footer-form-login -->
+
+<?php else: ?>
+<!-- nothing here yet -->
+<br />
+<?php endif; ?>
+*/?>
