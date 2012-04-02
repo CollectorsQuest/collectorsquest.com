a98cf2a5de1551ac947122c10c9129b6f1635bd9
Teomarino <teo.marinov@gmail.com>
2012-04-02 15:53:09 +0300

    Add layout for the Masonry script

--- apps/frontend/modules/general/templates/indexSuccess.php
+++ apps/frontend/modules/general/templates/indexSuccess.php
@@ -1,6 +1,57 @@
 <h2 class="FugazOne" style="font-size: 20px; color: white; width: 100%; background: #0982C2; margin-bottom: 0;">
   &nbsp;&nbsp;Showcase
 </h2>
-<div class="row-fluid">
+<div id="masonry" class="row">
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+  </div>
+  <div class="span3">
+    <a href="#">
+    <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
+  <div class="span6">
+    <a href="#">
+    <?= ice_image_tag_flickholdr('291x291'); ?>
+    </a>
+  </div>
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
+  <div class="span3">
+    <a href="#">
+      <?= ice_image_tag_flickholdr('138x138'); ?>
+    </a>
+  </div>
 
 </div>
+
+
