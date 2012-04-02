c49dcec5b24f10361e7c972ede801b502f4f37d8
Kiril Angov <kiril.angov@gmail.com>
2012-04-02 16:19:47 +0300

    Moving homepage css rules to homepage.less and adding and setting up masonry.js on the homepage

--- apps/frontend/modules/general/templates/indexSuccess.php
+++ apps/frontend/modules/general/templates/indexSuccess.php
@@ -1,52 +1,52 @@
 <h2 class="FugazOne" style="font-size: 20px; color: white; width: 100%; background: #0982C2; margin-bottom: 0;">
   &nbsp;&nbsp;Showcase
 </h2>
-<div id="masonry" class="row">
-  <div class="span3">
+<div id="homepage" class="row">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
     <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
   </div>
-  <div class="span6">
+  <div class="span6 brick">
     <a href="#">
     <?= ice_image_tag_flickholdr('291x291'); ?>
     </a>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
   </div>
-  <div class="span3">
+  <div class="span3 brick">
     <a href="#">
       <?= ice_image_tag_flickholdr('138x138'); ?>
     </a>
@@ -55,3 +55,19 @@
 </div>
 
 
+
+<script>
+  $(document).ready(function()
+  {
+    var $container = $('#homepage');
+
+    $container.imagesLoaded(function() {
+      $container.masonry(
+        {
+          itemSelector : '.brick',
+          columnWidth : 138, gutterWidth: 15,
+          isAnimated: !Modernizr.csstransitions
+        });
+    });
+  });
+</script>
