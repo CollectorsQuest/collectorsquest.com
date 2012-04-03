7bd51942d336c464e7ff00d0b44bff4cd504a594
IvanTanev <vankata.t@gmail.com>
2012-04-03 15:02:38 +0300

    ContentCategory schema + migration

---
+++ lib/filter/ContentCategoryFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ContentCategory filter form.
+ *
+ * @package    CollectorsQuest
+ * @subpackage filter
+ * @author     Collectors
+ */
+class ContentCategoryFormFilter extends BaseContentCategoryFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/ContentCategoryForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ContentCategory form.
+ *
+ * @package    CollectorsQuest
+ * @subpackage form
+ * @author     Collectors
+ */
+class ContentCategoryForm extends BaseContentCategoryForm
+{
+  public function configure()
+  {
+  }
+}
