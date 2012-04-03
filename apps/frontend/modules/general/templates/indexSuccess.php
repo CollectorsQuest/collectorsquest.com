6190f14621fa27cf7043ed90673ae841e215b272
Teomarino <teo.marinov@gmail.com>
2012-04-03 23:46:59 +0300

    Implement masonry html

--- apps/frontend/modules/general/templates/indexSuccess.php
+++ apps/frontend/modules/general/templates/indexSuccess.php
@@ -1,89 +1,241 @@
-<h2 class="FugazOne" style="font-size: 20px; color: white; width: 100%; background: #0982C2; margin-bottom: 0;">
+<h2 class="FugazOne blue-title">
   &nbsp;&nbsp;Showcase
 </h2>
 <div class="row">
-  <div  id="homepage" class="row-content">
+  <div id="homepage" class="row-content">
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
-    </div>
-    <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-    </div>
-    <div class="span3 brick">
-      <a href="#">
-      <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
-    </div>
-    <div class="span6 brick">
-      <a href="#">
-      <?= ice_image_tag_flickholdr('290x290'); ?>
-      </a>
-    </div>
-    <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
-    </div>
-    <div class="span6 brick masonry-blogpost">
-      <div class="masonry-blogpost" style="background: url("www.collectorsquest.com/uploads/blog/2012/04/marathon-breal-silver-cup-spiros-louis-olympics.jpg") no-repeat">
-        <a href="#" class="link">adsasd
-
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 8)) ?>
         </a>
+        <div class="details">
+        <h3>Transformers</h3>
+        <p>More than meets the eye</p>
+        <p>Collin David</p>
+        </div>
       </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 3)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 1)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 2)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
+    </div>
+    <div class="span6 brick masonry-blogpost">
+      <div class="masonry-blogpost yellow-background">
+        <a href="#" class="link" title="read the article">
+          <h2 class="FugazOne">Breal’s Silver Cup</h2>
+          <p>
+            The first competitive marathon was run as part of the first modern Olympic games in 1896, and were the brainchild of French businessman and philologist Michel Bréal.
+            The first marathon race as we know it today premiered at the first modern Olympic Games in Athens, Greece in 1896. The men’s marathon race was invented by French philologist Michel Bréal as part of the Athletics at the 1896 Olympic Games. Inspired by the legend of the messenger Pheidippides, Bréal had the idea to stage a race from the city of Marathon to Athens – a distance of just over twenty-five miles.
+          </p>
+        </a>
+        <div class="masonry-blogpost-author">
+          <p>
+            <a href="#">
+              <img alt="beautiful memories of barbie" title="beautiful memories of barbie" src="/images/legacy/multimedia/CollectorCollection/50x50.png" width="35" height="35">
+            </a>
+            posted by<br>
+            <a href="#">Collin David</a>
+          </p>
+          </a>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 8)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 38)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 13)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 8)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 8)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
+    </div>
+    <div class="span6 brick masonry-blogpost">
+      <div class="masonry-blogpost blue-background">
+        <a href="#" class="link" title="read the article">
+          <h2 class="FugazOne">Low Tech Pottery: My Camera Teapot</h2>
+          <p>Our youngest grandson, Nicholas, was over to visit. He always checks out my teapot collection. His favorite is the Miss Priss (a cat) and the small frog.  I try to move them around so he’ll have to take some time to locate them, but he always succeeds.</p>
+        </a>
+        <div class="masonry-blogpost-author">
+          <p>
+            <a href="#">
+              <img alt="beautiful memories of barbie" title="beautiful memories of barbie" src="/images/legacy/multimedia/CollectorCollection/50x50.png" width="35" height="35">
+            </a>
+            posted by<br>
+            <a href="#">Collin David</a>
+          </p>
+          </a>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 18)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
     <div class="span3 brick">
-      <a href="#">
-        <?= ice_image_tag_flickholdr('138x138'); ?>
-      </a>
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 2)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
     </div>
+    <div class="span3 brick">
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 7)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
+    </div>
+    <div class="span3 brick">
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 6)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
+    </div>
+    <div class="span3 brick">
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 5)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
+    </div>
+    <div class="span3 brick">
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 38)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
+    </div>
+    <div class="span3 brick">
+      <div class="square">
+        <a href="#">
+          <?= ice_image_tag_flickholdr('138x138', array('i' => 38)) ?>
+        </a>
+        <div class="details">
+          <h3>Transformers</h3>
+          <p>More than meets the eye</p>
+          <p>Collin David</p>
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </div>
 
