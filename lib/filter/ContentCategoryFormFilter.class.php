dfb730ce11c79a64906e3d1517c2414d829b5a6a
IvanTanev <vankata.t@gmail.com>
2012-04-04 14:58:42 +0300

    Add ContentCategory to the backend (General/Categories)
    Also implement cqWidgetFormPropelChoiceByParentId, use in ContentCategory
    and CollectionCategory

--- lib/filter/ContentCategoryFormFilter.class.php
+++ lib/filter/ContentCategoryFormFilter.class.php
@@ -11,5 +11,20 @@ class ContentCategoryFormFilter extends BaseContentCategoryFormFilter
 {
   public function configure()
   {
+    $this->widgetSchema['collection_category_id'] = new cqWidgetFormPropelChoiceByParentId(array(
+        'model' => 'CollectionCategory',
+        'order_by' => array('Name', 'asc'),
+        'add_empty' => true,
+        'id_to_make_first' => 0,
+    ));
+
+    $this->unsetFields();
+  }
+
+  protected function unsetFields()
+  {
+    unset ($this['tree_left']);
+    unset ($this['tree_right']);
+    unset ($this['tree_level']);
   }
 }
