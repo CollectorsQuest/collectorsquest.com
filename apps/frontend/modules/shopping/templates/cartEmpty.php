10012e327e98b2e95fb07ff8be2e296096ea8899
Teomarino <teo.marinov@gmail.com>
2012-04-09 10:28:07 +0300

    [Implement] cartEmpty To do: http://goo.gl/5iVNN

--- apps/frontend/modules/shopping/templates/cartEmpty.php
+++ apps/frontend/modules/shopping/templates/cartEmpty.php
@@ -1,7 +1,14 @@
-<br/><br/>
-<div class="empty_cart_message">
-  <p>Your cart is empty.</p>
-  <div class="button-center">
-    <?= link_to('See the Marketplace', '@marketplace', array('class' => 'btn btn-large')); ?>
+<div class="row-fluid">
+  <div class="row-fluid">
+    <div class="span6">
+      <div class="empty_cart_message">
+        <p>Your cart is <span>empty</span></p>
+      </div>
+    </div>
+    <div class="span6">
+      <div class="button-center">
+        <?= link_to('See the Marketplace', '@marketplace', array('class' => 'btn btn-primary blue')); ?>
+      </div>
+    </div>
   </div>
 </div>
