6cc8ecebd292c13a6ddf07a95217d65e57226fda
Kiril Angov <kiril.angov@gmail.com>
2012-04-12 11:42:20 +0300

    Adjusted the footer links to fit all resolutions

--- apps/frontend/templates/_footer_links.php
+++ apps/frontend/templates/_footer_links.php
@@ -1,7 +1,7 @@
 <div id="footer-links">
   <div class="footer-links-inner">
     <div class="row-fluid">
-      <div class="span6">
+      <div class="span7">
         <ul role="footer-links">
           <li><?= link_to('About Us', '@page?slug=about'); ?></li>
           <li><?= link_to('Contact', '@page?slug=contact-us'); ?></li>
@@ -11,9 +11,9 @@
           <li><?= link_to('Report an Error', '@feedback', array('target' => '_blank', 'style' => 'color: red;')); ?></li>
         </ul>
       </div>
-      <div class="span6 text-right">
-        <?= link_to('CollectorsQuest.com', '@homepage', array('title' => 'Interactive community and marketplace for the collectible community', 'style' => 'text-decoration: none;')); ?>
-        © <?= date('Y'); ?> All rights reserved &nbsp; • &nbsp; <a href="http://nytm.org/made" title="Made in NY">Made by hand in NY</a>
+      <div class="span5 text-right">
+        © <?= date('Y'); ?> Collectors' Quest, Inc. &nbsp; • &nbsp;
+        <a href="http://nytm.org/made" title="Made in NY">Made by hand in NY</a>
       </div>
     </div>
   </div>
