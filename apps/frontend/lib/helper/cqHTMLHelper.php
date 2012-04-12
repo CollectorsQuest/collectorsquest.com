f02565ad8d5c75528dc2359ad3c4d979bb8ec362
Kiril Angov <kiril.angov@gmail.com>
2012-04-12 21:04:10 +0300

    Adding classes for Chivo web font for page title <h1>

--- apps/frontend/lib/helper/cqHTMLHelper.php
+++ apps/frontend/lib/helper/cqHTMLHelper.php
@@ -3,7 +3,7 @@
 function cq_page_title($h1, $small = null, $options = array())
 {
   $small = (is_string($small) && !empty($small)) ? content_tag('small', (string) $small) : null;
-  $h1 = content_tag('h1', (string) $h1 . ($small ? '&nbsp;' . $small : ''));
+  $h1 = content_tag('h1', (string) $h1 . ($small ? '&nbsp;' . $small : ''), array('class' => 'Chivo webfont'));
   $options = array_merge(array('class' => 'page-title'), $options);
 
   echo content_tag('div', $h1, $options);
