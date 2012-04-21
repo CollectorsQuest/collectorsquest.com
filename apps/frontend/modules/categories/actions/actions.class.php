287ec0916d0bfa82951f29fd6856c56ff8135833
Kiril Angov <kiril.angov@gmail.com>
2012-04-21 23:04:51 +0300

    Implemented large portion of the categories/category page

--- apps/frontend/modules/categories/actions/actions.class.php
+++ apps/frontend/modules/categories/actions/actions.class.php
@@ -10,6 +10,41 @@ class categoriesActions extends cqFrontendActions
   public function executeCategory(sfWebRequest $request)
   {
     $this->category = $this->getRoute()->getObject();
+    $this->collectors_question = null;
+
+    $q = wpPostQuery::create()
+      ->filterByPostType('collectors_question')
+      ->filterByPostStatus('publish')
+      ->joinwpPostMeta(null, Criteria::RIGHT_JOIN)
+      ->usewpPostMetaQuery()
+        ->filterByMetaKey('cq_content_category_id')
+        ->filterByMetaValue($this->category->getId())
+      ->endUse()
+      ->orderByPostDate(Criteria::DESC);
+
+    /** @var $wp_posts wpPost[] */
+    if ($wp_posts = $q->limit(5)->find())
+    foreach ($wp_posts as $wp_post)
+    {
+      if ($thumbnail_id = $wp_post->getPostMetaValue('_thumbnail_id'))
+      {
+        $q = wpPostMetaQuery::create()
+          ->filterByPostId($thumbnail_id)
+          ->filterByMetaKey('_wp_attached_file');
+
+        /** @var $wp_post_meta wpPostMeta */
+        if ($wp_post_meta = $q->findOne())
+        {
+          $this->collectors_question = array(
+            'title' => $wp_post->getPostTitle(),
+            'content' => $wp_post->getPostContent(),
+            'image' => '/uploads/blog/' . $wp_post_meta->getMetaValue()
+          );
+
+          break;
+        }
+      }
+    }
 
     $q = CollectorCollectionQuery::create()
        ->filterByCollectionCategory($this->category);
