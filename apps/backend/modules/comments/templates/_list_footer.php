5f89e0516a10a9f11c12ca73a70743cf43c08c0c
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-04-21 13:38:50 +0300

    Added enter/exit inline edit mode for comment body

--- apps/backend/modules/comments/templates/_list_footer.php
+++ apps/backend/modules/comments/templates/_list_footer.php
@@ -1,7 +1,35 @@
 <script type="text/javascript">
   jQuery(document).ready(function () {
-    jQuery('.sf_admin_list_td_body')
-        .dotdotdot({height:36})
-    ;
+    var inlineSave = $('<button id="inline-save">Save</button>');
+    $('.sf_admin_list_td_body')
+        .dotdotdot({height: 36})
+        .unbind('click')
+        .bind('click', function(e) {
+            var control = $(this).find('.dotdotdot[contentEditable!="true"]');
+            if (control) {
+                console.log('enter edit mode');
+                console.log(control.contentEditable);
+                $(control)
+                    .html($(this).triggerHandler('originalContent'))
+                    .attr('contentEditable', true)
+                    .after(inlineSave)
+                ;
+            }
+            e.stopPropagation();
+    });
+
+    $(document)
+        .unbind('click')
+        .bind('click', function(e) {
+        $('.sf_admin_list_td_body .dotdotdot[contentEditable="true"]')
+            .attr('contentEditable', false)
+            .each(function(item) {
+                console.log('exit edit mode');
+            })
+            .trigger('update');
+        $('button#inline-save').remove();
+        e.stopPropagation();
+    });
+
   });
 </script>
