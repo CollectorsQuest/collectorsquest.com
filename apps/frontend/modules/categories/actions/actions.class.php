1213e6c62ebb2534317491c6fe86d30c2442bf9e
Kiril Angov <kiril.angov@gmail.com>
2012-04-21 23:38:33 +0300

    Tweaks of categories/category page

--- apps/frontend/modules/categories/actions/actions.class.php
+++ apps/frontend/modules/categories/actions/actions.class.php
@@ -12,36 +12,39 @@ class categoriesActions extends cqFrontendActions
     $this->category = $this->getRoute()->getObject();
     $this->collectors_question = null;
 
-    $q = wpPostQuery::create()
-      ->filterByPostType('collectors_question')
-      ->filterByPostStatus('publish')
-      ->joinwpPostMeta(null, Criteria::RIGHT_JOIN)
-      ->usewpPostMetaQuery()
-        ->filterByMetaKey('cq_content_category_id')
-        ->filterByMetaValue($this->category->getId())
-      ->endUse()
-      ->orderByPostDate(Criteria::DESC);
-
-    /** @var $wp_posts wpPost[] */
-    if ($wp_posts = $q->limit(5)->find())
-    foreach ($wp_posts as $wp_post)
+    if ($request->getParameter('page', 1) == 1)
     {
-      if ($thumbnail_id = $wp_post->getPostMetaValue('_thumbnail_id'))
+      $q = wpPostQuery::create()
+        ->filterByPostType('collectors_question')
+        ->filterByPostStatus('publish')
+        ->joinwpPostMeta(null, Criteria::RIGHT_JOIN)
+        ->usewpPostMetaQuery()
+          ->filterByMetaKey('cq_content_category_id')
+          ->filterByMetaValue($this->category->getId())
+        ->endUse()
+        ->orderByPostDate(Criteria::DESC);
+
+      /** @var $wp_posts wpPost[] */
+      if ($wp_posts = $q->limit(5)->find())
+      foreach ($wp_posts as $wp_post)
       {
-        $q = wpPostMetaQuery::create()
-          ->filterByPostId($thumbnail_id)
-          ->filterByMetaKey('_wp_attached_file');
-
-        /** @var $wp_post_meta wpPostMeta */
-        if ($wp_post_meta = $q->findOne())
+        if ($thumbnail_id = $wp_post->getPostMetaValue('_thumbnail_id'))
         {
-          $this->collectors_question = array(
-            'title' => $wp_post->getPostTitle(),
-            'content' => $wp_post->getPostContent(),
-            'image' => '/uploads/blog/' . $wp_post_meta->getMetaValue()
-          );
+          $q = wpPostMetaQuery::create()
+            ->filterByPostId($thumbnail_id)
+            ->filterByMetaKey('_wp_attached_file');
 
-          break;
+          /** @var $wp_post_meta wpPostMeta */
+          if ($wp_post_meta = $q->findOne())
+          {
+            $this->collectors_question = array(
+              'title' => $wp_post->getPostTitle(),
+              'content' => $wp_post->getPostContent(),
+              'image' => '/uploads/blog/' . $wp_post_meta->getMetaValue()
+            );
+
+            break;
+          }
         }
       }
     }
@@ -49,7 +52,7 @@ class categoriesActions extends cqFrontendActions
     $q = CollectorCollectionQuery::create()
        ->filterByCollectionCategory($this->category);
 
-    $pager = new PropelModelPager($q, 16);
+    $pager = new PropelModelPager($q, $this->collectors_question !== null ? 16 : 36);
     $pager->setPage($request->getParameter('page', 1));
     $pager->init();
 
