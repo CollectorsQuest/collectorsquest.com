110fe29bad35eafe9ff454923a1c07ec1f5d9af3
Teomarino <teo.marinov@gmail.com>
2012-04-23 19:02:05 +0300

    Update the Brand page

--- apps/frontend/modules/aent/templates/_sidebarPawnStars.php
+++ apps/frontend/modules/aent/templates/_sidebarPawnStars.php
@@ -4,13 +4,73 @@
 <br><br>
 <img src="/images/banners/040412_pawnstars_sidebar_banner_03.jpg" alt="">
 
-<?php
-$link = link_to(
-  'See all video &raquo;',
-  'http://'. sfConfig::get('app_magnify_channel', 'video.collectorsquest.com'),
-  array('class' => 'text-v-middle link-align')
-);
-cq_sidebar_title('Now Playing', $link, array('left' => 8, 'right' => 4));
-?>
+<?php include_component('_sidebar', 'widgetMagnifyVideos'); ?>
 
+
+<div class="row-fluid sidebar-title">
+  <div class="span6">
+    <h3 class="Chivo webfont">
+      When to Watch
+    </h3>
+  </div>
+  <div class="span6 text-right">
+    <a href="/collectors" class="text-v-middle link-align">
+      Go to episode guide »
+    </a>
+  </div>
+</div>
+
+<div id="programming-notes">
+  <div class="row-fluid">
+    <div class="span3">
+        <a href="#">
+          <img src="http://placehold.it/62x46" alt="">
+        </a>
+    </div>
+    <div class="span9 fix-height-text-block">
+      <div class="content_container">
+          <p>
+            Monday, March 26, 2012  8/7c
+          </p>
+          <a href="#" title="Help Wanted">
+            Help Wanted
+          </a>
+      </div>
+    </div>
+  </div>
+  <div class="row-fluid">
+    <div class="span3">
+      <a href="#">
+        <img src="http://placehold.it/62x46" alt="">
+      </a>
+    </div>
+    <div class="span9 fix-height-text-block">
+      <div class="content_container">
+        <p>
+          Monday, March 26, 2012  8/7c
+        </p>
+        <a href="#" title="Help Wanted">
+          Help Wanted
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row-fluid">
+    <div class="span3">
+      <a href="#">
+        <img src="http://placehold.it/62x46" alt="">
+      </a>
+    </div>
+    <div class="span9 fix-height-text-block">
+      <div class="content_container">
+        <p>
+          Monday, March 26, 2012  8/7c
+        </p>
+        <a href="#" title="Help Wanted">
+          Help Wanted
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
 
