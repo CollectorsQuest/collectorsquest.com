a27b92aaa1dc3d5b8a4bc360666057e8b703cc1e
Evan Stein <estein.work@gmail.com>
2012-04-23 19:57:51 -0400

    adding for ajax post loading on news landing page

--- web/blog/wp-content/themes/collectorsquest/functions.php
+++ web/blog/wp-content/themes/collectorsquest/functions.php
@@ -166,3 +166,43 @@ function map_meta_cap_editorial($caps, $cap, $user_id, $args)
   /* Return the capabilities required by the user. */
   return $caps;
 }
+
+if ($_SERVER['HTTP_HOST'] == 'www.collectorsquest.dev' || $_SERVER['HTTP_HOST'] == 'www.collectorsquest.next' || $_SERVER['HTTP_HOST'] == 'www.cqnext.com')
+{
+  /**
+   * Initialization. Add our script if needed on this page.
+   */
+ function cq_is_init() {
+   global $wp_query;
+
+   // Add code to index pages.
+ 	if( !is_singular() ) {
+     // Queue JS and CSS
+     wp_enqueue_script(
+       'cq-load-posts',
+       '/wp-content/themes/collectorsquest/js/load-posts.js',
+       array('jquery'),
+       '1.0',
+       true
+     );
+
+     // What page are we on? And what is the pages limit?
+     $max = $wp_query->max_num_pages;
+     $paged = ( get_query_var('paged') > 1 ) ? get_query_var('paged') : 1;
+
+     // Add some parameters for the JS.
+     wp_localize_script(
+       'cq-load-posts',
+       'cq',
+       array(
+         'startPage' => $paged,
+         'maxPages' => $max,
+         'nextLink' => next_posts($max, false)
+       )
+     );
+
+   }
+ }
+add_action('template_redirect', 'cq_is_init');
+
+}
