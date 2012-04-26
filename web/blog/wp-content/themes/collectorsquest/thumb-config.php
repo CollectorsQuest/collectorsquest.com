0509a14b2623a8729461a095dea5d1b3a8cba6ba
Evan Stein <estein.work@gmail.com>
2012-04-26 19:19:46 -0400

    needed for timthumb, added with obscurity

---
+++ web/blog/wp-content/themes/collectorsquest/thumb-config.php
@@ -0,0 +1,7 @@
+<?php
+
+define ('FILE_CACHE_DIRECTORY', '/www/vhosts/collectorsquest.com/current/web/uploads/blog/cache');
+define ('FILE_CACHE_SUFFIX', '.thumb.txt');			// What to put at the end of all files in the cache directory so we can identify them
+define ('FILE_CACHE_PREFIX', 'thumb');
+
+?>
