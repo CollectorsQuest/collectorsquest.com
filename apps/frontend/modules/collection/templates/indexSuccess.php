9117a2a95cfbc47bf176a09df46c7c48264d3998
Kiril Angov <kiril.angov@gmail.com>
2012-04-27 16:30:24 +0300

    Fixed the display of the sharethis widget on the Collection page

--- apps/frontend/modules/collection/templates/indexSuccess.php
+++ apps/frontend/modules/collection/templates/indexSuccess.php
@@ -15,20 +15,22 @@
     <?= $collection->getNumItems(); ?> Collectibles &nbsp;|&nbsp;
     <?= number_format($collection->getNumViews()); ?> Views
 
-    <div class="pull-right">
-      <span class='st_email'></span>
-      <span class='st_facebook'></span>
-      <span class='st_twitter'></span>
-      <span class='st_pinterest'></span>
-      <span class='st_plusone'></span>
+    <div class="pull-right" style="margin-top: -2px; margin-right: 5px;">
+      <span class='st_email_button' displayText="Mail"></span>
+      <span class='st_facebook_button'></span>
+      <span class='st_twitter_button'></span>
+      <span class='st_pinterest_button'></span>
     </div>
   </div>
 </div>
 
-<div class="cf" style="margin: 20px 0 10px 0;">
+<?php if ($pager->getPage() === 1): ?>
+<div class="cf" style="margin-top: 20px;">
   <?= cqStatic::linkify($collection->getDescription('html')); ?>
 </div>
+<?php endif; ?>
 
+<br/>
 <div class="row">
   <div id="collectibles" class="row-content">
   <?php
