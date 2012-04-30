46e95f3cc3b54aea0f7dadf40eca80e6bc1f9912
IvanTanev <vankata.t@gmail.com>
2012-04-30 11:44:39 +0300

    Move original shipping rates forms implementation to legacy app lib folder
    Duplicate to frontend

---
+++ apps/legacy/lib/form/shipping-rates/ShippingRateCollectorFormForEmbedding.class.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * ShippingRateCollectorFormForEmbedding
+ */
+class ShippingRateCollectorFormForEmbedding extends ShippingRateCollectorForm
+{
+
+  public function configure()
+  {
+    parent::configure();
+
+    unset ($this['id']);
+    unset ($this['collector_id']);
+    unset ($this['country_iso3166']);
+    unset ($this['calculation_type']);
+    unset ($this['price_range_min']);
+    unset ($this['price_range_max']);
+
+    $this->setupVisibleFieldsBasedOnCalculationType();
+
+    // we unset the shippingRatePriceRangeValidatorSchema and
+    // shippingRateAmountInCentsOrPercentValidatorSchema validators,
+    // because they need to be added in a special way by the parent form,
+    // in order to work in combination with cqCopyFieldsToEmbeddedFormValidatorSchema
+    $this->validatorSchema->setPostValidator(new sfValidatorPass());
+
+    $this->removeRequiredValidators();
+
+    $this->validatorSchema->setOption('allow_extra_fields', true);
+    $this->validatorSchema->setOption('filter_extra_fields', true);
+  }
+
+  public function getNameForEmbedding()
+  {
+    return 'shipping_rate_collector_' . $this->getObject()->getId();
+  }
+
+  protected function removeRequiredValidators()
+  {
+    foreach (array_keys($this->validatorSchema->getFields()) as $validator_name)
+    {
+      $this->validatorSchema[$validator_name]->setOption('required', false);
+    }
+  }
+
+  protected function setupVisibleFieldsBasedOnCalculationType()
+  {
+    switch ($this->getCurrentCalculationType()):
+      case (ShippingRatePeer::CALCULATION_TYPE_FLAT_RATE):
+        $this->useFields(array(
+            'amount_in_cents',
+            'amount_in_percent',
+        ));
+        break;
+
+      case (ShippingRatePeer::CALCULATION_TYPE_FREE_SHIPPING):
+      case (ShippingRatePeer::CALCULATION_TYPE_LOCAL_PICKUP):
+      case (ShippingRatePeer::CALCULATION_TYPE_NO_SHIPPING):
+        // hide all fields
+        $this->useFields(array());
+        break;
+
+      case (ShippingRatePeer::CALCULATION_TYPE_PRICE_RANGE):
+        // show all fields
+        break;
+
+      default:
+        // hide all fields
+        $this->useFields(array());
+        break;
+
+    endswitch;
+  }
+
+  protected function getCurrentCalculationType()
+  {
+    return $this->getOption('current_calculation_type',
+      $this->getObject()->getCalculationType());
+  }
+
+}
