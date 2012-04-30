f04922c16774d96f0504fa6f8be033dcab51aecb
Kiril Angov <kiril.angov@gmail.com>
2012-04-30 13:19:11 +0300

    Deleting the old Multimedia.php, MultimediaPeer.php and MultimediaQuery.php Writing a migration for the database changes to table "multimedia"

--- lib/model/plugins/iceMultimediaPlugin/Multimedia.php
+++ lib/model/plugins/iceMultimediaPlugin/Multimedia.php
@@ -1,21 +1,24 @@
 <?php
 
-
 require 'lib/model/plugins/iceMultimediaPlugin/om/BaseMultimedia.php';
 
-
-/**
- * Skeleton subclass for representing a row from the 'multimedia' table.
- *
- * 
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
- * @package    propel.generator.lib.model.plugins.iceMultimediaPlugin
- */
 class Multimedia extends BaseMultimedia
 {
+  public function setColors($colors)
+  {
+    if (is_array($colors))
+    {
+      $colors = implode(', ', $colors);
+    }
 
+    return parent::setColors($colors);
+  }
+
+  public function getColors()
+  {
+    $colors = explode(', ', parent::getColors());
+    $colors = array_filter($colors);
+
+    return $colors;
+  }
 }
