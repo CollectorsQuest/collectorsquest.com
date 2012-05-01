967d739fff0ca81276b22d844f09095b09e953be
Evan Stein <estein.work@gmail.com>
2012-05-01 13:14:54 -0400

    adding breadcrumb data to $data['breadcrumbs']

--- web/blog/wp-content/themes/collectorsquest/index.new.php
+++ web/blog/wp-content/themes/collectorsquest/index.new.php
@@ -20,6 +20,23 @@
     $data['tag'] = $wp_query->get_queried_object()->name;
   }
 
+  $home = array('name' => 'blog', 'url' => '/blog');
+  $url = $_SERVER["REQUEST_URI"];
+
+  if (is_front_page()) {$crumbs = array($home );}
+  elseif (is_tag()) {$crumbs = array($home, array('name' => 'Tag Archive: '.single_tag_title("", false), $url));}
+  elseif (is_category()) {$crumbs = array($home, array('name' => single_cat_title("", false), $url));}
+  elseif (is_single()) {$crumbs = array($home, array('name' => get_the_author_meta('display_name'), 'url' => '/blog/author/'.get_the_author_meta('nicename')),array('name' => get_the_title(), 'url' => null));}
+  elseif (is_author()) {$crumbs = array($home, array('name' => 'Archive for '.get_the_author_meta('display_name'), $url));}
+  elseif (is_day()) {$crumbs = array($home, array('name' => "Archive for ". the_time('F jS, Y'), $url));}
+  elseif (is_month()) {$crumbs = array($home, array('name' => "Archive for ". the_time('F, Y'), $url));}
+  elseif (is_year()) {$crumbs = array($home, array('name' => "Archive for ". the_time('Y'), $url));}
+  elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "Blog Archives";}
+  elseif (is_search()) {$crumbs = array($home, array('name' => "Search Results", $url));}
+
+  $data['breadcrumbs'] = $crumbs;
+
+
   ob_start();
   wp_head();
   $head = ob_get_clean();
