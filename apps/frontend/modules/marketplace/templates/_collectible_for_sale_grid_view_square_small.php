85f4801efd2afd07a3d93371bf1ba3196017dc31
Kiril Angov <kiril.angov@gmail.com>
2012-05-02 17:39:11 +0300

    Fixed the size of the thumbnail in _collectible_for_sale_grid_view_square_small.php

--- apps/frontend/modules/marketplace/templates/_collectible_for_sale_grid_view_square_small.php
+++ apps/frontend/modules/marketplace/templates/_collectible_for_sale_grid_view_square_small.php
@@ -8,7 +8,7 @@
   <?php
     echo link_to_collectible(
       $collectible_for_sale->getCollectible(), 'image',
-      array('width' => 150, 'height' => 150, 'max_width' => 131, 'max_height' => 131)
+      array('width' => 150, 'height' => 150, 'max_width' => 132, 'max_height' => 132)
     );
   ?>
   <p>
