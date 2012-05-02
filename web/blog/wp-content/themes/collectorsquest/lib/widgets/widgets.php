3e5d6af106b5ba0e48516640742a999da544f757
Evan Stein <estein.work@gmail.com>
2012-05-02 16:35:46 -0400

    adding sub-pages widget / cleanup

--- web/blog/wp-content/themes/collectorsquest/lib/widgets/widgets.php
+++ web/blog/wp-content/themes/collectorsquest/lib/widgets/widgets.php
@@ -31,3 +31,4 @@ require_once 'widget-300x250ad.php';
 require_once 'widget-tags.php';
 require_once 'widget-our-bloggers.php';
 require_once 'widget-other-news.php';
+require_once 'widget-sub-pages.php';
--- web/blog/wp-content/themes/collectorsquest/sidebar-new.php
+++ web/blog/wp-content/themes/collectorsquest/sidebar-new.php
@@ -1,35 +1,44 @@
 <?php
 
-if (is_page() && function_exists('dynamic_sidebar') && is_active_sidebar('static-page-sidebar'))
-{
-  dynamic_sidebar('static-page-sidebar');
-}
-else if (is_page())
-{
-  the_widget('WP_Pages_Widget', $args, $instance);
-}
+if (is_page()) {
 
+ if (function_exists('dynamic_sidebar') && is_active_sidebar('static-page-sidebar'))
+ {
+   dynamic_sidebar('static-page-sidebar');
+ }
+ else
+ {
+   the_widget('CQ_Sub_Pages_widget', $args, $instance);
+ }
 
-
-if (is_singular() && function_exists('dynamic_sidebar') && is_active_sidebar('singular-sidebar'))
-{
-  dynamic_sidebar('singular-sidebar');
-}
-else if (is_singular())
-{
-  the_widget('CQ_300x250ad_widget', $args, $instance);
-  the_widget('CQ_Tags_widget', $args, $instance);
-  the_widget('CQ_Other_News_widget', $args, $instance);
 }
 
 
+elseif (is_single()) {
 
-if (function_exists('dynamic_sidebar') && is_active_sidebar('non-singular-sidebar'))
-{
-  dynamic_sidebar('non-singular-sidebar');
+  if (function_exists('dynamic_sidebar') && is_active_sidebar('singular-sidebar'))
+  {
+    dynamic_sidebar('singular-sidebar');
+  }
+  else
+  {
+    the_widget('CQ_300x250ad_widget', $args, $instance);
+    the_widget('CQ_Tags_widget', $args, $instance);
+    the_widget('CQ_Other_News_widget', $args, $instance);
+  }
+
 }
-else if (!dynamic_sidebar('non-singular-sidebar'))
-{
-  the_widget('CQ_300x250ad_widget', $args, $instance);
-  the_widget('CQ_Our_Bloggers_widget', $args, $instance);
+
+else {
+
+  if (function_exists('dynamic_sidebar') && is_active_sidebar('non-singular-sidebar'))
+  {
+    dynamic_sidebar('non-singular-sidebar');
+  }
+  else
+  {
+    the_widget('CQ_300x250ad_widget', $args, $instance);
+    the_widget('CQ_Our_Bloggers_widget', $args, $instance);
+  }
+
 }
