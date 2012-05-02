34c6ce3c3f9ccd4960cdef71e67e479ebf8adda6
Evan Stein <estein.work@gmail.com>
2012-05-02 12:42:47 -0400

    switch to check if widgets have been added to sidebar vs checking if widget is active

--- web/blog/wp-content/themes/collectorsquest/sidebar-new.php
+++ web/blog/wp-content/themes/collectorsquest/sidebar-new.php
@@ -1,45 +1,35 @@
 <?php
 
-if (is_page() && function_exists('dynamic_sidebar'))
+if (is_page() && function_exists('dynamic_sidebar') && dynamic_sidebar('static-page-sidebar'))
 {
   dynamic_sidebar('static-page-sidebar');
-
-  if(!is_active_widget( '', '', 'WP_Pages_Widget')){
-    the_widget('WP_Pages_Widget', $args, $instance);
-  }
-
-
 }
-else if (is_singular() && function_exists('dynamic_sidebar'))
+else if (is_page())
+{
+  the_widget('WP_Pages_Widget', $args, $instance);
+}
+
+
+
+if (is_singular() && function_exists('dynamic_sidebar') && dynamic_sidebar('singular-sidebar'))
 {
   dynamic_sidebar('singular-sidebar');
-
-  if(!is_active_widget( '', '', 'CQ_300x250ad_widget')){
-    the_widget('CQ_300x250ad_widget', $args, $instance);
-  }
-
-  if(!is_active_widget( '', '', 'CQ_Tags_widget')){
-    the_widget('CQ_Tags_widget', $args, $instance);
-  }
-
-  if(!is_active_widget( '', '', 'CQ_Other_News_widget')){
-    the_widget('CQ_Other_News_widget', $args, $instance);
-  }
-
-
 }
-else if (function_exists('dynamic_sidebar'))
+else if (is_singular())
+{
+  the_widget('CQ_300x250ad_widget', $args, $instance);
+  the_widget('CQ_Tags_widget', $args, $instance);
+  the_widget('CQ_Other_News_widget', $args, $instance);
+}
+
+
+
+if (function_exists('dynamic_sidebar') && dynamic_sidebar('non-singular-sidebar'))
 {
   dynamic_sidebar('non-singular-sidebar');
-
-  if(!is_active_widget( '', '', 'CQ_300x250ad_widget')){
-    the_widget('CQ_300x250ad_widget', $args, $instance);
-  }
-
-  if(!is_active_widget( '', '', 'CQ_Our_Bloggers_widget')){
-    the_widget('CQ_Our_Bloggers_widget', $args, $instance);
-  }
-
 }
-
-
+else
+{
+  the_widget('CQ_300x250ad_widget', $args, $instance);
+  the_widget('CQ_Our_Bloggers_widget', $args, $instance);
+}
