ec9c8c86cb0e51ee267616ffb9ce45b6ac5eda24
Evan Stein <estein.work@gmail.com>
2012-05-02 20:11:00 -0400

    cleanup

--- web/blog/wp-content/themes/collectorsquest/sidebar-new.php
+++ web/blog/wp-content/themes/collectorsquest/sidebar-new.php
@@ -2,41 +2,27 @@
 
 if (is_page()) {
 
- if (function_exists('dynamic_sidebar') && is_active_sidebar('static-page-sidebar'))
- {
+ if (function_exists('dynamic_sidebar') && is_active_sidebar('static-page-sidebar')) {
    dynamic_sidebar('static-page-sidebar');
- }
- else
- {
+ } else {
    the_widget('CQ_Sub_Pages_widget', $args, $instance);
  }
 
-}
+} elseif (is_single()) {
 
-
-elseif (is_single()) {
-
-  if (function_exists('dynamic_sidebar') && is_active_sidebar('singular-sidebar'))
-  {
+  if (function_exists('dynamic_sidebar') && is_active_sidebar('singular-sidebar')) {
     dynamic_sidebar('singular-sidebar');
-  }
-  else
-  {
+  } else {
     the_widget('CQ_300x250ad_widget', $args, $instance);
     the_widget('CQ_Tags_widget', $args, $instance);
     the_widget('CQ_Other_News_widget', $args, $instance);
   }
 
-}
+} else {
 
-else {
-
-  if (function_exists('dynamic_sidebar') && is_active_sidebar('non-singular-sidebar'))
-  {
+  if (function_exists('dynamic_sidebar') && is_active_sidebar('non-singular-sidebar')) {
     dynamic_sidebar('non-singular-sidebar');
-  }
-  else
-  {
+  } else {
     the_widget('CQ_300x250ad_widget', $args, $instance);
     the_widget('CQ_Our_Bloggers_widget', $args, $instance);
   }
