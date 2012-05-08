5a252093bda97a02d925d73b6beb53f07d57cbea
Kiril Angov <kiril.angov@gmail.com>
2012-05-08 20:05:35 +0300

    Fixed PHP warnings in the blog sidebar

--- apps/frontend/modules/_blog/templates/_sidebar.php
+++ apps/frontend/modules/_blog/templates/_sidebar.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var $wp_post wpPost
+ * @var $wp_user wpUser
  * @var $data array
  */
 
@@ -34,15 +35,27 @@ else if ($data['is_page'])
   echo '<!-- Blog Sidebar Widget8 //-->';
   echo '<!-- Blog Sidebar Widget9 //-->';
 }
-else
+else if ($data['is_author'])
 {
   echo '<!-- Blog Sidebar Widget1 //-->';
 
   include_component(
     '_sidebar', 'widgetCollectiblesForSale',
-    array('wp_post' => $wp_post, 'limit' => 3)
+    array('wp_user' => $wp_user, 'limit' => 3)
   );
 
+  echo '<!-- Blog Sidebar Widget2 //-->';
+  echo '<!-- Blog Sidebar Widget3 //-->';
+  echo '<!-- Blog Sidebar Widget4 //-->';
+  echo '<!-- Blog Sidebar Widget5 //-->';
+  echo '<!-- Blog Sidebar Widget6 //-->';
+  echo '<!-- Blog Sidebar Widget7 //-->';
+  echo '<!-- Blog Sidebar Widget8 //-->';
+  echo '<!-- Blog Sidebar Widget9 //-->';
+}
+else
+{
+  echo '<!-- Blog Sidebar Widget1 //-->';
   echo '<!-- Blog Sidebar Widget2 //-->';
   echo '<!-- Blog Sidebar Widget3 //-->';
   echo '<!-- Blog Sidebar Widget4 //-->';
