0758ed71d331c366335645057254f4e5ca08948a
Kiril Angov <kiril.angov@gmail.com>
2012-05-08 11:01:41 +0300

    Small fixes for branded pages

--- apps/frontend/modules/aent/templates/_sidebarCollectible.php
+++ apps/frontend/modules/aent/templates/_sidebarCollectible.php
@@ -24,6 +24,7 @@
 ?>
 
 <?php if ($brand === 'Pawn Stars'): ?>
+<!--
 <div class="banner-sidebar-promo-300-90">
   <a href="<?= url_for('@aetn_storage_wars'); ?>" title="Check out items seen on Storage Wars">
     <img src="/images/banners/storage-wars-banner.jpg" alt="">
@@ -32,15 +33,17 @@
       </span>
   </a>
 </div>
+//-->
 <div class="banner-sidebar-promo-300-90">
   <a href="<?= url_for('@aetn_american_pickers'); ?>" title="Check out items seen on American Pickers">
     <img src="/images/banners/american-pickers-banner.jpg" alt="">
-      <span>
-        Check out items seen on American Pickers
-      </span>
+    <span>
+      Check out items seen on American Pickers
+    </span>
   </a>
 </div>
 <?php elseif ($brand === 'American Pickers'): ?>
+<!--
 <div class="banner-sidebar-promo-300-90">
   <a href="<?= url_for('@aetn_storage_wars'); ?>" title="Check out items seen on Storage Wars">
     <img src="/images/banners/storage-wars-banner.jpg" alt="Check out items seen on Storage Wars">
@@ -49,12 +52,13 @@
     </span>
   </a>
 </div>
+//-->
 <div class="banner-sidebar-promo-300-90">
   <a href="<?= url_for('@aetn_pawn_stars'); ?>" title="Check out items seen on Pawn Stars">
     <img src="/images/banners/pawn-stars-banner.jpg" alt="Check out items seen on Pawn Stars">
-      <span>
-        Check out items seen on Pawn Stars
-      </span>
+    <span>
+      Check out items seen on Pawn Stars
+    </span>
   </a>
 </div>
 <?php endif; ?>
