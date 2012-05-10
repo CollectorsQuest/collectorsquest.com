750629297f090b86667bb16b8f8c5c09e075f16a
Kiril Angov <kiril.angov@gmail.com>
2012-05-10 17:21:31 +0300

    Pass the blog title in the $data array

--- apps/frontend/modules/_blog/actions/actions.class.php
+++ apps/frontend/modules/_blog/actions/actions.class.php
@@ -7,6 +7,19 @@ class _blogActions extends cqFrontendActions
     // We do not want the web debug bar on blog requests
     sfConfig::set('sf_web_debug', false);
 
+    $key = $this->getRequestParameter('key');
+
+    if (function_exists('xcache_get'))
+    {
+      $this->data = xcache_get($key);
+    }
+    else
+    {
+      $this->data = zend_shm_cache_fetch($key);
+    }
+
+    $this->getResponse()->setTitle($this->data['title']);
+
     return sfView::SUCCESS;
   }
 }
