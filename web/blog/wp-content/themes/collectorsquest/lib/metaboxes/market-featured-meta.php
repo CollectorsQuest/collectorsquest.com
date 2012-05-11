4bf0ef346e011fa17d9ec87e033f12e647871942
Kiril Angov <kiril.angov@gmail.com>
2012-05-11 18:53:19 +0300

    More understandable labels for market-featured-meta.php

--- web/blog/wp-content/themes/collectorsquest/lib/metaboxes/market-featured-meta.php
+++ web/blog/wp-content/themes/collectorsquest/lib/metaboxes/market-featured-meta.php
@@ -1,17 +1,17 @@
-<div class="my_meta_control" style="height:220px;">
+<div class="my_meta_control" style="height:130px;">
 
   <p>Enter comma separated ID numbers. (1,2,3)</p>
 
   <div style="width:31%;margin-right:2%;float:left">
-    <label>Item 1:
+    <label>Collectible for Sale (ID):
     <?php $mb->the_field('cq_collectible_id_1'); ?>
 
-    <textarea name="<?php $mb->the_name(); ?>" cols="5" rows="5"><?php $mb->the_value(); ?></textarea></label>
+    <input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"></label>
 
     <?php $selected = ' selected="selected"'; ?>
     <?php $mb->the_field('cq_collectible_text_1'); ?>
 
-    <label>Item 1 text:
+    <label>Ribbon Text:
       <select name="<?php $mb->the_name(); ?>">
         <option value=""></option>
         <option value="affordable"<?php if ($mb->get_the_value() == 'affordable') echo $selected; ?>>Affordable</option>
@@ -23,15 +23,15 @@
   </div>
 
   <div style="width:31%;margin-right:2%;float:left">
-    <label>Item 2:
+    <label>Collectible for Sale (ID):
     <?php $mb->the_field('cq_collectible_id_2'); ?>
 
-    <textarea name="<?php $mb->the_name(); ?>" cols="5" rows="5"><?php $mb->the_value(); ?></textarea></label>
+    <input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"></label>
 
     <?php $selected = ' selected="selected"'; ?>
     <?php $mb->the_field('cq_collectible_text_2'); ?>
 
-    <label>Item 2 text:
+    <label>Ribbon Text:
       <select name="<?php $mb->the_name(); ?>">
         <option value=""></option>
         <option value="affordable"<?php if ($mb->get_the_value() == 'affordable') echo $selected; ?>>Affordable</option>
@@ -43,15 +43,15 @@
   </div>
 
   <div style="width:31%;float:left">
-    <label>Item 3:
+    <label>Collectible for Sale (ID):
     <?php $mb->the_field('cq_collectible_id_3'); ?>
 
-    <textarea name="<?php $mb->the_name(); ?>" cols="5" rows="5"><?php $mb->the_value(); ?></textarea></label>
+    <input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"></label>
 
     <?php $selected = ' selected="selected"'; ?>
     <?php $mb->the_field('cq_collectible_text_3'); ?>
 
-    <label>Item 3 text:
+    <label>Ribbon Text:
       <select name="<?php $mb->the_name(); ?>">
         <option value=""></option>
         <option value="affordable"<?php if ($mb->get_the_value() == 'affordable') echo $selected; ?>>Affordable</option>
