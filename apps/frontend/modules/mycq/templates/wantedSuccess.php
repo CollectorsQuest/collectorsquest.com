51b8f71a300c834c2143224b19160c4551b7acdc
Teo <teo.marinov@gmail.com>
2012-05-14 01:38:18 +0300

    Implement 041223_CQ_Wanted_List_v5.psd to html

--- apps/frontend/modules/mycq/templates/wantedSuccess.php
+++ apps/frontend/modules/mycq/templates/wantedSuccess.php
@@ -1,1 +1,157 @@
-<h1>041223_CQ_Wanted_List_v5.psd</h1>
+<div class="section-title spacer-top-20">
+  <h2 class="Chivo webfont">Wanted List</h2>
+</div>
+
+<div class="blue-action-bar spacer-12">
+  <div class="row-fluid">
+    <div class="span2">
+      <small>View as:</small>
+      <a href="#" class="grid-view active">&nbsp;</a>
+      <a href="#" class="list_view">&nbsp;</a>
+    </div>
+    <div class="span3">
+      <small>Showing 1-25 of 58 Wanted Items</small>
+    </div>
+    <div class="span7">
+      <div class="pagination pull-right">
+        <ul>
+          <li><a href="#">Prev</a></li>
+          <li class="active">
+            <a href="#">1</a>
+          </li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">Next</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row collectible_wanted-list">
+  <div class="row-content">
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+        <strong class="pull-right">$15.00</strong>
+      </p>
+    </div>
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+        <strong class="pull-right">$15.00</strong>
+      </p>
+    </div>
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+        <strong class="pull-right">$15.00</strong>
+      </p>
+    </div>
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+      </p>
+    </div>
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+        <strong class="pull-right">$15.00</strong>
+      </p>
+    </div>
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+        <strong class="pull-right">$15.00</strong>
+      </p>
+    </div>
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+        <strong class="pull-right">$15.00</strong>
+      </p>
+    </div>
+    <div class="span3 collectible_wanted-list_grid_view_square link">
+      <a href="#">
+        <img alt="" src="http://placehold.it/175x175">
+      </a>
+      <a href="#" class="remove" title="Remove item"><i class="remove-collectible"></i> Remove</a>
+      <p>
+        <a href="#" class="target" title="Transformer - Per...">
+          Transformer - Per...
+        </a>
+        <strong class="pull-right">$15.00</strong>
+      </p>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="blue-action-bar spacer-12">
+  <div class="row-fluid">
+    <div class="span2">
+      <small>View as:</small>
+      <a href="#" class="grid-view active">&nbsp;</a>
+      <a href="#" class="list_view">&nbsp;</a>
+    </div>
+    <div class="span3">
+      <small>Showing 1-25 of 58 Wanted Items</small>
+    </div>
+    <div class="span7">
+      <div class="pagination pull-right">
+        <ul>
+          <li><a href="#">Prev</a></li>
+          <li class="active">
+            <a href="#">1</a>
+          </li>
+          <li><a href="#">2</a></li>
+          <li><a href="#">3</a></li>
+          <li><a href="#">Next</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
