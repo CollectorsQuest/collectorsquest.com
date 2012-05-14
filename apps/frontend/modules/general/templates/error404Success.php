7a4840db34b314e8293ec27e98143074ac10f67c
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-05-14 10:34:21 +0300

    Added 404 page content

--- apps/frontend/modules/general/templates/error404Success.php
+++ apps/frontend/modules/general/templates/error404Success.php
@@ -1,2 +1,23 @@
-<h1>404 - Page Not Found</h1>
-<p>We're sorry, but the page you requested could not be found. Please try one of the links above for further assistance.</p>
+<header>
+  <h1>Sorry, the page you&rsquo;re looking for isn&rsquo;t here anymore.</h1>
+
+  <h2>You didn&rsquo;t do anything wrong. We may have moved the page you&rsquo;re looking for somewhere else.</h2>
+</header>
+
+<section class="faq">
+  <h1>Did you follow a link from here?</h1>
+
+  <p>If you reached this page from another part of collectorsquest.com, please
+    <a href="<?=url_for('feedback')?>">let us know</a> so we can correct our mistake.</p>
+
+  <h1>Did you follow a link from another site?</h1>
+
+  <p>Links from other sites can sometimes be outdated or misspelled.
+    <a href="<?=url_for('feedback')?>">Let us know</a> where you came from and we can try to contact the other site in order to fix the problem.
+  </p>
+
+  <h1>Did you type the URL?</h1>
+
+  <p>You may have typed the address (URL) incorrectly. Check to make sure you&rsquo;ve got the exact right spelling, capitalization, etc. The
+    <a href="<?=url_for('homepage')?>">Collectorsquest home page</a> has links to our most popular content.</p>
+</section>
