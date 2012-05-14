7840b12acf643f04700762104b3f13e2d9722ca7
Kiril Angov <kiril.angov@gmail.com>
2012-05-14 22:03:30 +0300

    Todo https://basecamp.com/1759305/projects/281910-cqnext-com-testing/todos/6337650-green-status-bar

--- apps/frontend/templates/_flash_messages.php
+++ apps/frontend/templates/_flash_messages.php
@@ -7,19 +7,16 @@
 <?php elseif ($sf_user->hasFlash('error')): ?>
 <div class="alert alert-error fade in" data-alert="alert" style="margin-top: 15px;">
   <a class="close" data-dismiss="alert">×</a>
-  <strong style="font-variant: small-caps;"><?= __('Error:', array(), 'flash'); ?></strong>&nbsp;
   <?= implode('<br/><br/>', array_filter((array) $sf_user->getFlash('error', null, true))); ?>
 </div>
 <?php elseif ($sf_user->hasFlash('success')): ?>
 <div class="alert alert-success fade in" data-alert="alert" style="margin-top: 15px;">
   <a class="close" data-dismiss="alert">×</a>
-  <strong style="font-variant: small-caps;"><?= __('Success:', array(), 'flash'); ?></strong>&nbsp;
   <?= $sf_user->getFlash('success', null, true); ?>
 </div>
 <?php elseif ($sf_user->hasFlash('info')): ?>
 <div class="alert alert-info fade in" data-alert="alert" style="margin-top: 15px;">
   <a class="close" data-dismiss="alert">×</a>
-  <strong style="font-variant: small-caps;"><?= __('Notice:', array(), 'flash'); ?></strong>&nbsp;
   <?= $sf_user->getFlash('info', null, true); ?>
 </div>
 <?php elseif ($sf_user->hasFlash('hightlight')): ?>
