1a5ecc050edf872fbc0b146fa4d04a2a620df2ce
Kiril Angov <kiril.angov@gmail.com>
2012-05-15 18:53:23 +0300

    Add missing "→" in the blog breadcrumbs

--- apps/frontend/modules/_blog/templates/_breadcrumbs.php
+++ apps/frontend/modules/_blog/templates/_breadcrumbs.php
@@ -2,6 +2,6 @@
   <div class="pull-right">
     <span style="color: #887777">You are here:</span>
     <?php include_component('iceBreadcrumbsModule', 'breadcrumbs'); ?>
-    <?= $data['breadcrumbs'] ?>
+    → <?= $data['breadcrumbs'] ?>
   </div>
 </div>
