c0caf14d0d8da677d4ca85db528f15a3912522dd
Evan Stein <estein.work@gmail.com>
2012-05-16 14:47:35 -0400

    Fix for correct comment count

--- web/blog/wp-content/themes/collectorsquest/functions.php
+++ web/blog/wp-content/themes/collectorsquest/functions.php
@@ -573,3 +573,16 @@ function kpl_user_bio_visual_editor_unfiltered() {
   remove_all_filters('pre_user_description');
 }
 add_action('admin_init','kpl_user_bio_visual_editor_unfiltered');
+
+
+// correcting comment count
+add_filter('get_comments_number', 'comment_count', 0);
+function comment_count( $count ) {
+  if ( ! is_admin() ) {
+    global $id;
+    $comments_by_type = &separate_comments(get_comments('status=approve&post_id=' . $id));
+    return count($comments_by_type['comment']);
+  } else {
+    return $count;
+  }
+}
