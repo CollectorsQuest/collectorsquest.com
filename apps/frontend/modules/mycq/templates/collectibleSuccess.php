b3888876d9712bb15f3f66c0f8547745645fac47
Teo <teo.marinov@gmail.com>
2012-05-20 22:41:43 +0300

    Work on /mycq/collectible/

--- apps/frontend/modules/mycq/templates/collectibleSuccess.php
+++ apps/frontend/modules/mycq/templates/collectibleSuccess.php
@@ -100,84 +100,104 @@
 </form>
 
 
+<form class="form-horizontal">
+  <div class="row-fluid">
+    <div class="span4">
+      <div id="main-image-set">
+        <div class="main-image-set-container">
+          <ul class="thumbnails">
+            <li class="span12">
+              <a class="thumbnail" href="#">
+                <img alt="" src="http://placehold.it/300x300">
+              </a>
+            </li>
+            <li class="span4">
+              <a class="thumbnail" href="#">
+                <img alt="" src="http://placehold.it/97x97">
+              </a>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+              </div>
+            </li>
+          </ul>
+          <ul class="statistic-2">
+            <li>
+              XXX Views
+            </li>
+            <li>
+              In XXX wanted lists
+            </li>
+            <li>
+              XXX Comments
+            </li>
+          </ul>
+        </div>
+      </div>
 
+    </div><!-- ./span4 -->
+    <div class="span8">
+      <?php
+      $link = link_to(
+        'View public item page &raquo;', '/collector/1212/tofunky',
+        array('class' => 'text-v-middle link-align')
+      );
+      cq_sidebar_title('Nurz', $link, array('left' => 8, 'right' => 4, 'class'=>'spacer-top-reset row-fluid sidebar-title'));
+      ?>
 
-<div id="main-image-set">
-  <div class="main-image-set-container">
-    <ul class="thumbnails">
-      <li class="span9">
-        <a href="#" class="thumbnail">
-          <img src="http://placehold.it/460x436" alt="">
-        </a>
-      </li>
-      <li class="span3">
-        <a href="#" class="thumbnail">
-          <img src="http://placehold.it/143x143" alt="">
-        </a>
-      </li>
-      <li class="span3">
-        <a href="#" class="thumbnail">
-          <img src="http://placehold.it/143x143" alt="">
-        </a>
-      </li>
-      <li class="span3">
-        <a href="#" class="thumbnail">
-          <img src="http://placehold.it/143x143" alt="">
-        </a>
-      </li>
-    </ul>
-    <ul class="statistic-2">
-      <li>
-        XXX Views
-      </li>
-      <li>
-        In XXX wanted lists
-      </li>
-      <li>
-        XXX Comments
-      </li>
-    </ul>
+      <div class="control-group">
+        <label class="control-label" for="input01">Collectible Name <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xlarge" id="input01">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="input01">Assign to Collection <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xlarge" id="input01">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="input01">Tags <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xlarge" id="input01">
+          <p class="help-block">Choose at least three descriptive words for your item, separated by commas</p>
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="textarea">Collectible Description</label>
+        <div class="controls">
+          <textarea class="input-xlarge" id="textarea" rows="3"></textarea>
+        </div>
+      </div>
+      <div class="control-group">
+        <label for="textarea" class="control-label">Permalink</label>
+        <div class="controls spacer-top-5">
+          <span class="brown">http://collectorsquest.com/item/542698</span>
+        </div>
+      </div>
+    </div><!-- ./span8 -->
+
+    <div class="row-fluid">
+      <div class="span12">
+
+        <div class="form-actions text-center spacer-inner-15">
+          <button type="submit" class="btn btn-primary blue-button">Save changes</button>
+          <button class="btn gray-button spacer-left">Cancel</button>
+        </div>
+
+      </div>
+    </div>
   </div>
-</div>
+</form>
 
 
-<div id="main-image-set">
-  <div class="main-image-set-container">
-    <ul class="thumbnails">
-      <li class="span9">
-        <div class="thumbnail">
-          <i class="icon icon-download-alt drop-zone-large ui-droppable"></i>
-        </div>
-      </li>
-      <li class="span3">
-        <a href="#" class="thumbnail">
-          <img src="http://placehold.it/143x143" alt="">
-        </a>
-      </li>
-      <li class="span3">
-        <div class="thumbnail">
-          <i class="icon icon-download-alt drop-zone ui-droppable"></i>
-        </div>
-      </li>
-      <li class="span3">
-        <div class="thumbnail">
-          <i class="icon icon-download-alt drop-zone ui-droppable"></i>
-        </div>
-      </li>
-    </ul>
-    <ul class="statistic-2">
-      <li>
-        XXX Views
-      </li>
-      <li>
-        In XXX wanted lists
-      </li>
-      <li>
-        XXX Comments
-      </li>
-    </ul>
-  </div>
-</div>
 
 
 <div id="mycq-tabs" class="spacer-top-reset">
