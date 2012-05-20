d64687a6996c0f0dcb9c857799eb8c22c1101891
Teo <teo.marinov@gmail.com>
2012-05-20 20:35:31 +0300

    Implement 041220_CQ_Edit_Collectible_Item_v3.psd and 041220_CQ_Edit_Sale_Item_v6.psd to html

--- apps/frontend/modules/mycq/templates/collectibleSuccess.php
+++ apps/frontend/modules/mycq/templates/collectibleSuccess.php
@@ -1,1 +1,265 @@
-<h1>041220_CQ_Edit_Collectible_Item_v3.psd and 041220_CQ_Edit_Sale_Item_v6.psd</h1>
+<!-- 041220_CQ_Edit_Collectible_Item_v3.psd and 041220_CQ_Edit_Sale_Item_v6.psd -->
+
+<?php
+$link = link_to(
+  'View public item page &raquo;', '/collector/1212/tofunky',
+  array('class' => 'text-v-middle link-align')
+);
+cq_sidebar_title('Nurz', $link, array('left' => 8, 'right' => 4, 'class'=>'spacer-top-reset row-fluid sidebar-title'));
+?>
+
+<div id="main-image-set">
+  <div class="main-image-set-container">
+    <ul class="thumbnails">
+      <li class="span9">
+        <div class="thumbnail">
+          <i class="icon icon-download-alt drop-zone-large ui-droppable"></i>
+        </div>
+      </li>
+      <li class="span3">
+        <div class="thumbnail">
+            <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+        </div>
+      </li>
+      <li class="span3">
+        <div class="thumbnail">
+            <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+        </div>
+      </li>
+      <li class="span3">
+        <div class="thumbnail">
+            <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+        </div>
+      </li>
+    </ul>
+    <ul class="statistic-2">
+      <li>
+        XXX Views
+      </li>
+      <li>
+        In XXX wanted lists
+      </li>
+      <li>
+        XXX Comments
+      </li>
+    </ul>
+  </div>
+</div>
+
+
+<div id="main-image-set">
+  <div class="main-image-set-container">
+    <ul class="thumbnails">
+      <li class="span9">
+        <a href="#" class="thumbnail">
+          <img src="http://placehold.it/460x436" alt="">
+        </a>
+      </li>
+      <li class="span3">
+        <a href="#" class="thumbnail">
+          <img src="http://placehold.it/143x143" alt="">
+        </a>
+      </li>
+      <li class="span3">
+        <a href="#" class="thumbnail">
+          <img src="http://placehold.it/143x143" alt="">
+        </a>
+      </li>
+      <li class="span3">
+        <a href="#" class="thumbnail">
+          <img src="http://placehold.it/143x143" alt="">
+        </a>
+      </li>
+    </ul>
+    <ul class="statistic-2">
+      <li>
+        XXX Views
+      </li>
+      <li>
+        In XXX wanted lists
+      </li>
+      <li>
+        XXX Comments
+      </li>
+    </ul>
+  </div>
+</div>
+
+
+<div id="main-image-set">
+  <div class="main-image-set-container">
+    <ul class="thumbnails">
+      <li class="span9">
+        <div class="thumbnail">
+          <i class="icon icon-download-alt drop-zone-large ui-droppable"></i>
+        </div>
+      </li>
+      <li class="span3">
+        <a href="#" class="thumbnail">
+          <img src="http://placehold.it/143x143" alt="">
+        </a>
+      </li>
+      <li class="span3">
+        <div class="thumbnail">
+          <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+        </div>
+      </li>
+      <li class="span3">
+        <div class="thumbnail">
+          <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+        </div>
+      </li>
+    </ul>
+    <ul class="statistic-2">
+      <li>
+        XXX Views
+      </li>
+      <li>
+        In XXX wanted lists
+      </li>
+      <li>
+        XXX Comments
+      </li>
+    </ul>
+  </div>
+</div>
+
+
+<div id="mycq-tabs" class="spacer-top-reset">
+  <div class="row-fluid instruction-box"><!-- .instruction-box -->
+    <div class="row-fluid instruction-box-inner">
+      <div class="span2">
+        <span class="gray-arrow-up pull-right"></span>
+      </div>
+      <div class="span8">
+        <span class="hint-text spacer-top-reset"><strong>Hint:</strong> Drag your alternate views for this<br> item into the drop area.</span>
+      </div>
+      <div class="span2">
+        <span class="gray-arrow-up"></span>
+      </div>
+    </div>
+  </div><!-- /.instruction-box -->
+
+  <div class="collectible-container-inner">
+  <div class="row-fluid sidebar-title">
+    <div class="span9 link-align">
+      <h3 class="Chivo webfont">Items to Sort (14)</h3>
+    </div>
+    <div class="span3">
+      <span class="btn btn-primary blue-button fileinput-button float-right">
+        <i class="icon-plus icon-white"></i>&nbsp;<span>Upload Photos</span>
+        <input type="file" name="files[]" multiple="multiple">
+      </span>
+    </div>
+  </div>
+
+  <div class="collectibles-to-sort">
+      <ul class="thumbnails">
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+        <li class="span2">
+          <a href="#" class="thumbnail">
+            <img src="http://placehold.it/72x72" alt="">
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <form class="form-horizontal">
+    <div class="form-inner-white">
+      <div class="control-group">
+        <label class="control-label" for="input01">Collectible Name <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xxlarge" id="input01">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="input01">Assign to Collection <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xxlarge" id="input01">
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="input01">Tags <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xxlarge" id="input01">
+          <p class="help-block">Choose at least three descriptive words for your item, separated by commas</p>
+        </div>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="textarea">Collectible Description</label>
+        <div class="controls">
+          <textarea class="input-xxlarge" id="textarea" rows="3"></textarea>
+        </div>
+      </div>
+      <div class="control-group">
+        <label for="textarea" class="control-label">Permalink</label>
+        <div class="controls spacer-top-5">
+          <span class="brown">http://collectorsquest.com/item/542698</span>
+        </div>
+      </div>
+    </div>
+    <div class="form-actions text-center spacer-inner-15">
+      <button type="submit" class="btn btn-primary blue-button">Save changes</button>
+      <button class="btn gray-button spacer-left">Cancel</button>
+    </div>
+
+  </form>
+</div>
+
+
+
