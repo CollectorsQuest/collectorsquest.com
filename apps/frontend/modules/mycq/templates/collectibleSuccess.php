564e32ab4a8853bf4ca67c56960977ccfbdac626
Teo <teo.marinov@gmail.com>
2012-05-20 22:33:19 +0300

    Implement/Redesign 041220_CQ_Edit_Collectible_Item_v3.psd

--- apps/frontend/modules/mycq/templates/collectibleSuccess.php
+++ apps/frontend/modules/mycq/templates/collectibleSuccess.php
@@ -1,50 +1,105 @@
 <!-- 041220_CQ_Edit_Collectible_Item_v3.psd and 041220_CQ_Edit_Sale_Item_v6.psd -->
+<form class="form-horizontal">
+  <div class="row-fluid">
+    <div class="span4">
+      <div id="main-image-set">
+        <div class="main-image-set-container">
+          <ul class="thumbnails">
+            <li class="span12">
+              <div class="thumbnail">
+                <i class="icon icon-download-alt drop-zone-large ui-droppable"></i>
+                <span class="info-text">
+                   Drag and drop the main image<br> of your collectible here.
+                </span>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+              </div>
+            </li>
+            <li class="span4">
+              <div class="thumbnail">
+                <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+              </div>
+            </li>
+          </ul>
+          <ul class="statistic-2">
+            <li>
+              XXX Views
+            </li>
+            <li>
+              In XXX wanted lists
+            </li>
+            <li>
+              XXX Comments
+            </li>
+          </ul>
+        </div>
+      </div>
 
-<?php
-$link = link_to(
-  'View public item page &raquo;', '/collector/1212/tofunky',
-  array('class' => 'text-v-middle link-align')
-);
-cq_sidebar_title('Nurz', $link, array('left' => 8, 'right' => 4, 'class'=>'spacer-top-reset row-fluid sidebar-title'));
-?>
+    </div><!-- ./span4 -->
+    <div class="span8">
+      <?php
+      $link = link_to(
+        'View public item page &raquo;', '/collector/1212/tofunky',
+        array('class' => 'text-v-middle link-align')
+      );
+      cq_sidebar_title('Nurz', $link, array('left' => 8, 'right' => 4, 'class'=>'spacer-top-reset row-fluid sidebar-title'));
+      ?>
 
-<div id="main-image-set">
-  <div class="main-image-set-container">
-    <ul class="thumbnails">
-      <li class="span9">
-        <div class="thumbnail">
-          <i class="icon icon-download-alt drop-zone-large ui-droppable"></i>
+      <div class="control-group">
+        <label class="control-label" for="input01">Collectible Name <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xlarge" id="input01">
         </div>
-      </li>
-      <li class="span3">
-        <div class="thumbnail">
-            <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="input01">Assign to Collection <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xlarge" id="input01">
         </div>
-      </li>
-      <li class="span3">
-        <div class="thumbnail">
-            <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="input01">Tags <span class="red-bold">*</span></label>
+        <div class="controls">
+          <input type="text" class="input-xlarge" id="input01">
+          <p class="help-block">Choose at least three descriptive words for your item, separated by commas</p>
         </div>
-      </li>
-      <li class="span3">
-        <div class="thumbnail">
-            <i class="icon icon-download-alt drop-zone ui-droppable"></i>
+      </div>
+      <div class="control-group">
+        <label class="control-label" for="textarea">Collectible Description</label>
+        <div class="controls">
+          <textarea class="input-xlarge" id="textarea" rows="3"></textarea>
         </div>
-      </li>
-    </ul>
-    <ul class="statistic-2">
-      <li>
-        XXX Views
-      </li>
-      <li>
-        In XXX wanted lists
-      </li>
-      <li>
-        XXX Comments
-      </li>
-    </ul>
+      </div>
+      <div class="control-group">
+        <label for="textarea" class="control-label">Permalink</label>
+        <div class="controls spacer-top-5">
+          <span class="brown">http://collectorsquest.com/item/542698</span>
+        </div>
+      </div>
+    </div><!-- ./span8 -->
+
+    <div class="row-fluid">
+      <div class="span12">
+
+        <div class="form-actions text-center spacer-inner-15">
+          <button type="submit" class="btn btn-primary blue-button">Save changes</button>
+          <button class="btn gray-button spacer-left">Cancel</button>
+        </div>
+
+      </div>
+    </div>
   </div>
-</div>
+</form>
+
+
 
 
 <div id="main-image-set">
@@ -219,47 +274,11 @@ cq_sidebar_title('Nurz', $link, array('left' => 8, 'right' => 4, 'class'=>'space
     </div>
   </div>
 
-  <form class="form-horizontal">
-    <div class="form-inner-white">
-      <div class="control-group">
-        <label class="control-label" for="input01">Collectible Name <span class="red-bold">*</span></label>
-        <div class="controls">
-          <input type="text" class="input-xxlarge" id="input01">
-        </div>
-      </div>
-      <div class="control-group">
-        <label class="control-label" for="input01">Assign to Collection <span class="red-bold">*</span></label>
-        <div class="controls">
-          <input type="text" class="input-xxlarge" id="input01">
-        </div>
-      </div>
-      <div class="control-group">
-        <label class="control-label" for="input01">Tags <span class="red-bold">*</span></label>
-        <div class="controls">
-          <input type="text" class="input-xxlarge" id="input01">
-          <p class="help-block">Choose at least three descriptive words for your item, separated by commas</p>
-        </div>
-      </div>
-      <div class="control-group">
-        <label class="control-label" for="textarea">Collectible Description</label>
-        <div class="controls">
-          <textarea class="input-xxlarge" id="textarea" rows="3"></textarea>
-        </div>
-      </div>
-      <div class="control-group">
-        <label for="textarea" class="control-label">Permalink</label>
-        <div class="controls spacer-top-5">
-          <span class="brown">http://collectorsquest.com/item/542698</span>
-        </div>
-      </div>
-    </div>
-    <div class="form-actions text-center spacer-inner-15">
-      <button type="submit" class="btn btn-primary blue-button">Save changes</button>
-      <button class="btn gray-button spacer-left">Cancel</button>
-    </div>
-
-  </form>
 </div>
+
+
+
+
 
 
 
