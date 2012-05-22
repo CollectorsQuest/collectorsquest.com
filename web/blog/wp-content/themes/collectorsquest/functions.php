adcc83e6109ea65f61e9d390da9bbd7fee73d67c
Evan Stein <estein.work@gmail.com>
2012-05-22 09:22:53 -0400

    Tweaks for images

--- web/blog/wp-content/themes/collectorsquest/functions.php
+++ web/blog/wp-content/themes/collectorsquest/functions.php
@@ -779,3 +779,35 @@ function my_attachment_fields_to_edit_filter($form_fields, $post) {
   return $form_fields;
 }
 add_filter('attachment_fields_to_edit', 'my_attachment_fields_to_edit_filter', 100, 2);
+
+
+
+/**
+http://rathercurious.net
+*/
+class fixImageMargins{
+  public $xs = 0; //change this to change the amount of extra spacing
+
+  public function __construct(){
+    add_filter('img_caption_shortcode', array(&$this, 'fixme'), 10, 3);
+  }
+  public function fixme($x=null, $attr, $content){
+
+    extract(shortcode_atts(array(
+      'id'    => '',
+      'align'    => 'alignnone',
+      'width'    => '',
+      'caption' => ''
+    ), $attr));
+
+    if ( 1 > (int) $width || empty($caption) ) {
+      return $content;
+    }
+
+    if ( $id ) $id = 'id="' . $id . '" ';
+
+    return '<div ' . $id . 'class="wp-caption ' . $align . '" style="width: ' . ((int) $width + $this->xs) . 'px">'
+      . $content . '<p class="wp-caption-text">' . $caption . '</p></div>';
+  }
+}
+$fixImageMargins = new fixImageMargins();
