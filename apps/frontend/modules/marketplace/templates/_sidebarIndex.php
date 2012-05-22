ae6a9f5d6b35a8a2080605f4199364a5301e3a2a
Teo <teo.marinov@gmail.com>
2012-05-23 00:22:44 +0300

    Small fixes, cleanup and tweaks

--- apps/frontend/modules/marketplace/templates/_sidebarIndex.php
+++ apps/frontend/modules/marketplace/templates/_sidebarIndex.php
@@ -9,9 +9,11 @@
 <?php include_component('_sidebar', 'widgetMarketplaceCategories'); ?>
 <?php include_component('_sidebar', 'widgetFeaturedSellers', array('title' => 'Spotlight')); ?>
 
+<div class="spacer-20">
 <?php
   echo link_to(
     image_tag('banners/040412_CQ_Market_blue.gif'),
     '@legacy_seller_signup'
   );
 ?>
+</div>
