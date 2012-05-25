4922a32968cfea65cc6f4617709dfb091fa507da
Teo <teo.marinov@gmail.com>
2012-05-25 23:23:56 +0300

    Fix todo http://goo.gl/ZDSPj

--- apps/frontend/templates/_footer_signup_external_buttons.php
+++ apps/frontend/templates/_footer_signup_external_buttons.php
@@ -1,10 +1,10 @@
 Sign up using&nbsp;
 <a href="#" rel="tooltip" title="Sign up using facebook" class="s-16-icon-facebook">
-  <i class="hide-text">Sign up using facebook</i>
+  <i class="hide-text">Sign up using Facebook</i>
 </a>
 <a href="#" title="Sign up using twitter" class="s-16-icon-twitter">
-  <i class="hide-text">Sign up using twitter</i>
+  <i class="hide-text">Sign up using Twitter</i>
 </a>
 <a href="#" title="Sign up using google" class="s-16-icon-google">
-  <i class="hide-text">Sign up using google</i>
+  <i class="hide-text">Sign up using Google</i>
 </a>
