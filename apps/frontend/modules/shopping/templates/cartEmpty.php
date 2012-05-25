d4552e06329c8cfa636c5b5d25616611a6673c69
Teo <teo.marinov@gmail.com>
2012-05-25 06:42:56 +0300

    Fix typo and button rollover color. Todo: http://goo.gl/Xr6xc

--- apps/frontend/modules/shopping/templates/cartEmpty.php
+++ apps/frontend/modules/shopping/templates/cartEmpty.php
@@ -1,6 +1,6 @@
 <div class="empty-cart-message">
   <p>Your cart is <span>empty</span></p>
   <div class="button-center" style="margin-left: 20px; margin-top: 20px;">
-    <?= link_to('Shop the Marketplace', '@marketplace', array('class' => 'btn btn-primary blue')); ?>
+    <?= link_to('Shop the Market', '@marketplace', array('class' => 'btn btn-primary blue-button')); ?>
   </div>
 </div>
