157aaddca26411c0e11c5a261b3fc7538e3d88ba
IvanTanev <vankata.t@gmail.com>
2012-05-26 16:55:53 +0300

    Update passowrd recovery page with new Bootstrap compatible captcha widget

--- apps/frontend/modules/general/templates/recoverPasswordSuccess.php
+++ apps/frontend/modules/general/templates/recoverPasswordSuccess.php
@@ -1,36 +1,15 @@
+<?php
+  /** @var $form PasswordRecoveryForm */ $form;
+?>
+
+<form class="form-horizontal">
+</form>
 <div class="password-recovery-form">
 
   <?= form_tag('@recover_password', array('class' => 'form-horizontal')); ?>
     <fieldset>
       <h1 class="text-center spacer-inner-bottom-30">Forgot your username and/or password?</h1>
- <?php /*
-        <?= $form->renderHiddenFields() ?>
-        <div class="control-group">
-          <?= $form['email']->renderLabel(null, array('class' => 'control-label')); ?>
-          <div class="controls">
-            <?= $form['email']->renderError(null, array('class' => 'help-inline')); ?>
-            <?= $form['email']; ?>
-          </div>
-        </div>
- */?>
-      <div class="control-group">
-        <label class="control-label" for="input01">Email: <span class="red-bold">*</span></label>
-        <div class="controls">
-          <input type="text" class="input-large" id="input01">
-          <p class="help-block">Enter your email address.</p>
-        </div>
-      </div>
-      <div class="control-group">
-        <label class="control-label" for="input02">Captcha: <span class="red-bold">*</span></label>
-        <div class="controls">
-          <a href="#" class="inline"><?= (image_tag('frontend/dev/captcha.png')); ?></a>
-          <span class="arrow-l-r"></span>
-          <input type="text" class="input-large inline input-captcha" maxlength="5" id="input02">
-        </div>
-      </div>
-
-
-
+        <?= $form->renderUsing('Bootstrap'); ?>
     </fieldset>
     <div class="form-actions">
       <input class="btn btn-primary blue-button" type="submit" value="Recover your account!" />
---
+++ lib/widget/cqWidgetBootstrapCaptcha.class.php
@@ -0,0 +1,60 @@
+<?php
+
+class cqWidgetBootstrapCaptcha extends sfWidgetForm
+{
+  protected function configure($options = array(), $attributes = array())
+  {
+    parent::configure($options, $attributes);
+
+    $this->addOption('width');
+    $this->addOption('height');
+    $this->addOption('background-color');
+    $this->addOption('font-size');
+    $this->addOption('code-length', 5);
+    $this->addOption('format', '%captcha% <span class="arrow-l-r"></span> %input%');
+  }
+
+  public function render($name, $value = null, $attributes = array(), $errors = array())
+  {
+    $vars['%captcha%'] =  $this->renderContentTag('a',
+      $this->renderTag('img', array(
+          'src' => $this->getCaptchaUrl()."?r=' + Math.random() + '&" . $this->getCaptchaParams(),
+          'onClick' => "this.src=this.src.replace(/(.+r=)(.+)(&amp;w.+)/, '$1' + Math.random() + '$3' + '&reload=1')",
+          'width' => $this->getOption('width'),
+          'height' => $this->getOption('height'),
+      )),
+      array('onClick' => 'return false;', 'style' => 'text-decoration:none')
+    );
+
+    $attributes = array_merge($attributes, array(
+        'class' => (isset($attributes['class']) ? $attributes['class'] : '')
+                    . ' input-captcha',
+        'type' => 'text',
+        'name' => $name,
+        'value' => $value,
+        'maxlength' => $this->getOption('code-length'),
+    ));
+    $vars['%input%'] = $this->renderTag('input', $attributes);
+
+    return strtr($this->getOption('format'), $vars);
+  }
+
+  protected function getCaptchaUrl()
+  {
+    return sfContext::getInstance()->getRouting()->generate(
+      'ice_captcha_image',
+      array(),
+      true
+    );
+  }
+
+  protected function getCaptchaParams()
+  {
+    return sprintf(
+      'w=%d&h=%d&bc=%s&fs=%d&cl=%d',
+      $this->getOption('width'), $this->getOption('height'),
+      $this->getOption('background-color'), $this->getOption('font-size'),
+      $this->getOption('code-length')
+    );
+  }
+}
