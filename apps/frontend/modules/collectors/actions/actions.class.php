80729507cb222d1616c0a96eb78955a1773a6b57
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-05-28 18:17:54 +0300

    Added collectors/index logic

--- apps/frontend/modules/collectors/actions/actions.class.php
+++ apps/frontend/modules/collectors/actions/actions.class.php
@@ -3,8 +3,22 @@
 class collectorsActions extends cqFrontendActions
 {
 
-  public function executeIndex()
+  /**
+   * @param sfWebRequest $request
+   *
+   * @return string
+   */
+  public function executeIndex(sfWebRequest $request)
   {
+    $q = CollectorQuery::create()
+          ->orderByCreatedAt(Criteria::DESC);
+
+    $pager = new PropelModelPager($q, 36);
+    $pager->setPage($request->getParameter('page', 1));
+    $pager->init();
+
+    $this->pager = $pager;
+
     return sfView::SUCCESS;
   }
 
--- apps/frontend/modules/collectors/templates/indexSuccess.php
+++ apps/frontend/modules/collectors/templates/indexSuccess.php
@@ -1,1 +1,11 @@
+<?php
+/*
+ * @var $pager PropelModelPager
+ * @var $collector Collector
+ */
+?>
 <?php echo cq_page_title('Collectors'); ?>
+
+<?php foreach ($pager->getResults() as $collector): ?>
+<?php include_partial('collector/collector_grid_view_span6', array('collector'=> $collector)) ?>
+<?php endforeach; ?>
