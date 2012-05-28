f71d17e51a3e5e0b20aa561b6697d415282aee70
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-05-28 20:48:29 +0300

    Fixed styling for collection when there are no collectibles

--- apps/frontend/modules/collection/templates/indexNoCollectibles.php
+++ apps/frontend/modules/collection/templates/indexNoCollectibles.php
@@ -1,11 +1,50 @@
 <?php
 /**
- * @var cqUser      $sf_user
+ * @var cqFrontendUser      $sf_user
  * @var Collection  $collection
  */
 ?>
 
 <?= cq_page_title($collection, 'by '. link_to_collector($collection->getCollector(), 'text')); ?>
+
+<div class="blue-actions-panel spacer-20">
+  <div class="row-fluid">
+    <div class="pull-left">
+      <ul>
+        <li>
+          By <?= link_to_collector($collection->getCollector(), 'text'); ?>
+          </li>
+        <li>
+          <?php
+          echo format_number_choice(
+            '[0] no collectibles yet|[1] 1 Collectible|(1,+Inf] %1% Collectibles',
+            array('%1%' => number_format($collection->getNumItems())), $collection->getNumItems()
+          );
+          ?>
+        </li>
+        <li>
+          <?php
+          echo format_number_choice(
+            '[0] no views yet|[1] 1 View|(1,+Inf] %1% Views',
+            array('%1%' => number_format($collection->getNumViews())), $collection->getNumViews()
+          );
+          ?>
+        </li>
+      </ul>
+    </div>
+    <div class="pull-right share">
+      <!-- AddThis Button BEGIN -->
+      <a class="btn btn-lightblue btn-mini-social addthis_button_email">
+        <i class="mail-icon-mini"></i> Email
+      </a>
+      <a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:width="40"></a>
+      <a class="addthis_button_tweet" tw:twitter:data-count="none"></a>
+      <a class="addthis_button_google_plusone" g:plusone:size="medium" g:plusone:annotation="none"></a>
+      <a class="addthis_button_pinterest_pinit" pi:pinit:media="<?= image_tag_collectible($collectible, 'original'); ?>" pi:pinit:layout="horizontal"></a>
+      <!-- AddThis Button END -->
+    </div>
+  </div>
+</div>
 
 <i class="icon-exclamation-sign" style="float: left; font-size: 46px; margin-right: 10px; color: #DF912F;"></i>
 <?php
