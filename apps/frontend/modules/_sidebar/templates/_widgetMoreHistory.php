c2a8ac907fb29ba20434382696b5b4bfb11aef9b
Teo <teo.marinov@gmail.com>
2012-05-30 17:02:57 +0300

    Change "More Pawn Stars" / "More American Pickers" modules http://goo.gl/aXa9m

--- apps/frontend/modules/_sidebar/templates/_widgetMoreHistory.php
+++ apps/frontend/modules/_sidebar/templates/_widgetMoreHistory.php
@@ -1,3 +1,11 @@
-<?php
-
+<?php cq_sidebar_title('More HISTORY') ?>
+<a href="http://www.history.com/shows/cajun-pawn-stars" title="Cajun Pawn Stars" target="_blank">
+  <?php echo image_tag('banners/cps_more_history_promo.jpg', array('alt'=>'Cajun Pawn Stars'));?>
+</a>
+<a href="http://www.history.com/shows/american-restoration" title="American Restoration" target="_blank">
+  <?php echo image_tag('banners/ar_more_history_promo.jpg', array('alt'=>'American Restoration'));?>
+</a>
+<a href="http://www.history.com/shows/united-stats-of-america" title="United Stats of America" target="_blank">
+  <?php echo image_tag('banners/usoa_more_history_promo.jpg', array('alt'=>'United Stats of America'));?>
+</a>
 
