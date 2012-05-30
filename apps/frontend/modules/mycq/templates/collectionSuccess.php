392c55691d0761c096db183c79427e6d845a67e7
Kiril Angov <kiril.angov@gmail.com>
2012-05-30 15:30:57 +0300

    Fix to-do: https://basecamp.com/1759305/projects/281910-cqnext-com-testing/todos/7426652-edit-collection

--- apps/frontend/modules/mycq/templates/collectionSuccess.php
+++ apps/frontend/modules/mycq/templates/collectionSuccess.php
@@ -21,7 +21,7 @@
           <i class="icon icon-plus icon-white"></i>
         </a>
         <a class="blue-link" href="#">
-          Click to Add Main<br>Thumbnail or Drag and Drop<br>from "Items to Sort" below
+          Drag and Drop from <br>"Items to Sort"
         </a>
       <?php endif; ?>
     </div>
