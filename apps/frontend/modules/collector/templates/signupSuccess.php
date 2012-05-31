60f67ab61b155e58d43b2fdaf541a8212d12163a
Kiril Angov <kiril.angov@gmail.com>
2012-05-31 12:19:57 +0300

    Little formatting on the signupSuccess.php

--- apps/frontend/modules/collector/templates/signupSuccess.php
+++ apps/frontend/modules/collector/templates/signupSuccess.php
@@ -1,17 +1,27 @@
-<?php
-use_javascripts_for_form($form);
-?>
+<?php use_javascripts_for_form($form); ?>
+
 <div class="span6">
   <h3>Why Should You Join?</h3>
   <dl>
     <dt>Show Off Your Collection</dt>
-    <dd>You bought it, now brag about it. Let other collectors see what's hiding in your closets. Or use as a a digital archive on the go so you don't buy the same thing twice! (It has happened to all of us).</dd>
+    <dd>
+      You bought it, now brag about it. Let other collectors see what's hiding in your closets.
+      Or use as a a digital archive on the go so you don't buy the same thing twice!
+      (It has happened to all of us).
+    </dd>
     <dt>Meet Other Collectors</dt>
-    <dd>Find other like-minded collectors or branch out and meet new friends who can show you a cool thing or two.</dd>
+    <dd>
+      Find other like-minded collectors or branch out and meet new friends
+      who can show you a cool thing or two.
+    </dd>
     <dt>Hear about the Latest Trends in Collecting</dt>
-    <dd>If it's out there, you'll hear about it here. Daily blog and video coverage on the latest and greatest out in the collecting universe.</dd>
+    <dd>
+      If it's out there, you'll hear about it here. Daily blog and video coverage on the
+      latest and greatest out in the collecting universe.
+    </dd>
   </dl>
 </div>
+
 <div class="span6">
   <?= form_tag('@collector_signup', array('class' => 'form-horizontal')) ?>
   <fieldset>
@@ -20,5 +30,5 @@ use_javascripts_for_form($form);
       <input type="submit" class="btn btn-primary" value="Submit" />
     </div>
   </fieldset>
-  </form>
+  <?= '</form>' ?>
 </div>
