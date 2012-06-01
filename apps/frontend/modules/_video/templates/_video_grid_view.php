6ae9fae728477b5381471156189ea474eaf70406
Kiril Angov <kiril.angov@gmail.com>
2012-06-01 11:22:54 +0300

    Fix linking to videos from the search page

--- apps/frontend/modules/_video/templates/_video_grid_view.php
+++ apps/frontend/modules/_video/templates/_video_grid_view.php
@@ -5,9 +5,12 @@
  */
 ?>
 
-<div id="video_<?= $video->id; ?>_grid_view" data-id="<?= $video->id; ?>" class="video_grid_view fade-white link">
+<div id="video_<?= $video->id; ?>_grid_view" data-id="<?= $video->id; ?>"
+     class="video_grid_view fade-white link">
   <div class="mosaic-overlay">
     <p class="details"><?= $video->getTitle(); ?></p>
   </div>
-  <?= image_tag($video->getThumbnail(), array('class'=> 'mosaic-backdrop')); ?>
+  <a href="<?= $video->getPlayUrl() ?>" title="<?= $video->getTitle() ?>" class="target">
+    <?= image_tag($video->getThumbnail(), array('class' => 'mosaic-backdrop')); ?>
+  </a>
 </div>
