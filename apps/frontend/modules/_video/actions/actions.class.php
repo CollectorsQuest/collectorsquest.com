84e53ae89414fc5a1de517181a58936862bb7ac8
Kiril Angov <kiril.angov@gmail.com>
2012-06-01 11:33:40 +0300

    Temporary redirecting the video headers to collectorsquest.com

--- apps/frontend/modules/_video/actions/actions.class.php
+++ apps/frontend/modules/_video/actions/actions.class.php
@@ -9,6 +9,11 @@ class _videoActions extends cqFrontendActions
 
   public function executeHeader()
   {
+    if (sfConfig::get('sf_environment') !== 'prod')
+    {
+      $this->redirect('http://www.collectorsquest.com/_video/header', 302);
+    }
+
     sfConfig::set('sf_web_debug', false);
 
     return sfView::SUCCESS;
@@ -16,6 +21,11 @@ class _videoActions extends cqFrontendActions
 
   public function executeFooter()
   {
+    if (sfConfig::get('sf_environment') !== 'prod')
+    {
+      $this->redirect('http://www.collectorsquest.com/_video/footer', 302);
+    }
+
     sfConfig::set('sf_web_debug', false);
 
     return sfView::SUCCESS;
