ced86151a5d6b5809f6c0d3f05ff6b51b1588ad3
Kiril Angov <kiril.angov@gmail.com>
2012-06-02 16:40:32 +0300

    Fix for to-do: https://basecamp.com/1759305/projects/281910-cqnext-com-testing/todos/7691091-missing-image

--- lib/model/CollectionCollectiblePeer.php
+++ lib/model/CollectionCollectiblePeer.php
@@ -4,5 +4,26 @@ require 'lib/model/om/BaseCollectionCollectiblePeer.php';
 
 class CollectionCollectiblePeer extends BaseCollectionCollectiblePeer
 {
+  /**
+   * @static
+   * @param  array  $parameters
+   *
+   * @return CollectionCollectible
+   */
+  public static function getObjectForRoute($parameters)
+  {
+    $collectible = null;
+    $parameters['id'] = str_replace(array('.html', '.htm'), '', $parameters['id']);
 
+    $q = CollectionCollectibleQuery::create()
+      ->filterByCollectibleId($parameters['id'])
+      ->orderByCollectibleId(Criteria::DESC);
+
+    if (preg_match('/-(\d+)$/i', $parameters['slug'], $m))
+    {
+      $q->filterByCollectionId((int) $m[1]);
+    }
+
+    return $collectible = $q->findOne();
+  }
 }
