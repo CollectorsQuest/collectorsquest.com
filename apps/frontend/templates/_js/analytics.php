96971079198637e1fd7978c47def40c03c90f5fc
Kiril Angov <kiril.angov@gmail.com>
2012-06-02 14:48:35 +0300

    Loading google analytics async

--- apps/frontend/templates/_js/analytics.php
+++ apps/frontend/templates/_js/analytics.php
@@ -6,11 +6,13 @@
 ?>
 
 <script>
-  Modernizr.load({
-    load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-  });
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
-  <?php if ($sf_user->isAuthenticated()): ?>
+<?php if ($sf_user->isAuthenticated()): ?>
   function woopraReady(tracker)
   {
     tracker.setDomain('<?= sfConfig::get('app_domain_name'); ?>');
@@ -25,5 +27,5 @@
   Modernizr.load({
     load: '//static.woopra.com/js/woopra.js'
   });
-  <?php endif; ?>
+<?php endif; ?>
 </script>
