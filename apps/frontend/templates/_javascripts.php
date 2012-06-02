bbbe0fa584a0c98f979f12e71fc8a872b6fdbd34
Kiril Angov <kiril.angov@gmail.com>
2012-06-02 14:31:48 +0300

    Reverting to jquery 1.7.1 because of the homepage roudabout does not work with the new version (only the square shape we are using has problems)

--- apps/frontend/templates/_javascripts.php
+++ apps/frontend/templates/_javascripts.php
@@ -1,5 +1,5 @@
 <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script>
   window.jQuery || document.write('<script src="<?= cq_javascript_src('frontend/jquery.js'); ?>"><\/script>');
 </script>
