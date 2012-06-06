0aad3fac8823ba216b845c4bfe2c9fdf589b7fa8
Teo <teo.marinov@gmail.com>
2012-06-06 22:22:48 +0300

    Update text To-do http://goo.gl/Q8Cdf

--- apps/frontend/modules/collector/templates/signupSuccess.php
+++ apps/frontend/modules/collector/templates/signupSuccess.php
@@ -11,21 +11,21 @@ cq_page_title(
   <div class="span4 signup-text-bg">
     <h3>Why Should You Join?</h3>
     <dl class="signup-text">
-      <dt>Show Off Your Collection</dt>
+      <dt>Show off your collection</dt>
       <dd>
-        You bought it, now brag about it. Let other collectors see what's hiding in your closets.
-        Or use as a digital archive on the go so you don't buy the same thing twice!
-        (It has happened to all of us).
+        You bought it, now brag about it! Let other collectors see what's
+        hiding in your closets, or create a digital archive to use on the
+        go so you don't buy the same thing twice! (It's happened to all of us).
       </dd>
-      <dt>Meet Other Collectors</dt>
+      <dt>Meet other collectors</dt>
       <dd>
-        Find other like-minded collectors or branch out and meet new friends
-        who can show you a cool thing or two.
+        Find other like-minded collectors or branch out and meet new
+        friends who can show you a cool thing or two.
       </dd>
-      <dt>Hear about the Latest Trends in Collecting</dt>
+      <dt>Hear about the latest trends in Collecting</dt>
       <dd>
-        If it's out there, you'll hear about it here. Daily blog and video coverage on the
-        latest and greatest out in the collecting universe.
+        If it's out there, you'll hear about it here in our daily blog
+        and video coverage of the latest and greatest from the collecting universe.
       </dd>
     </dl>
   </div>
