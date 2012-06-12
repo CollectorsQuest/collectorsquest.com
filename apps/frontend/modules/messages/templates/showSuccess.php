acc6a300fca40e515b65ceb7aab349add5342092
IvanTanev <vankata.t@gmail.com>
2012-06-13 02:59:18 +0300

    Fix long username bleed over message in pm threads

--- apps/frontend/modules/messages/templates/showSuccess.php
+++ apps/frontend/modules/messages/templates/showSuccess.php
@@ -16,7 +16,7 @@
       <?endif; ?>
     >
       <td class="sender" rowspan="2">
-        <span>By <?= link_to($sender, array('sf_route' => 'collector_by_slug', 'sf_subject' => $sender)); ?></span>
+        <span>By&nbsp;<?= link_to($sender, array('sf_route' => 'collector_by_slug', 'sf_subject' => $sender)); ?></span>
         <br/>
         <span><?= time_ago_in_words_or_exact_date($message->getCreatedAt()); ?></span>
         <br/>
