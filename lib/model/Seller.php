035dcaf711aeb6052bff53004d61823a347e3e39
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-06-13 16:47:42 +0300

    Added calculations of seller credits

--- lib/model/Seller.php
+++ lib/model/Seller.php
@@ -2,8 +2,37 @@
 
 class Seller extends Collector
 {
+
   public function hasPackageCredits()
   {
-    return true;
+    return 0 < $this->getCreditsLeft();
+  }
+
+  /**
+   * @return int
+   * @todo unit tests
+   */
+  public function getTotalPackageCredits()
+  {
+    $q = PackageTransactionQuery::create()
+        ->filterByCollector($this)
+        ->filterByPaymentStatus(PackageTransactionPeer::STATUS_PAID)
+        ->filterByExpiryDate(time(), Criteria::GREATER_EQUAL)
+        ->clearSelectColumns()
+        ->addAsColumn('total', 'SUM(credits)');
+
+    return (int)PackageTransactionPeer::doSelectStmt($q)->fetchColumn(0);
+  }
+
+  public function getCreditsLeft()
+  {
+    $totalCredits = $this->getTotalPackageCredits();
+
+    $creditsUsed = PackageTransactionCreditQuery::create()
+        ->filterByCollectorId($this->getId())
+        ->filterByExpiryDate(time(), Criteria::GREATER_EQUAL)
+        ->count();
+
+    return $totalCredits - $creditsUsed;
   }
 }
