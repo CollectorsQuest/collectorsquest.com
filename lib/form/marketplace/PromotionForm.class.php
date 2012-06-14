196f3ab966fca17f190ac023ce412174bf081e92
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-06-14 10:26:31 +0300

    Added prefill of promotion code (as in https://basecamp.com/1759305/projects/19165-collectorsquest-com/todos/8453410-for-new-promotion) Fixed calendar image

--- lib/form/marketplace/PromotionForm.class.php
+++ lib/form/marketplace/PromotionForm.class.php
@@ -10,16 +10,19 @@
 class PromotionForm extends BasePromotionForm
 {
   protected $amountTypes = array('Fix'=>'Fix', 'Percentage'=>'Percentage');
-  
+
+  public function __construct($object = null, $options = array(), $CSRFSecret = null)
+  {
+    parent::__construct($object, $options, $CSRFSecret);
+
+    $this->setDefault('promotion_code', sprintf('CQ%d-%s', date('Y'), ShoppingOrderPeer::getUuidFromId(date('mdHis'))));
+  }
+
   public function configure()
   {
-    $this->widgetSchema['expiry_date'] = new sfWidgetFormJQueryDate(array(
-        'culture' => $this->getOption('culture'),
-        'image' =>  '/images/backend/theme/medium/calendar.png',
-//        'date_widget' => new sfWidgetFormInput(),
-      ));
+    $this->widgetSchema['expiry_date'] = new sfWidgetFormJQueryDate();
     
-    $this->widgetSchema['amount_type'] = new sfWidgetFormChoice(array('choices'=> $this->amountTypes, 'expanded'=>true));
+    $this->widgetSchema['amount_type'] = new sfWidgetFormChoice(array('choices'=> $this->amountTypes, 'expanded'=>true), array('class'=>'unstyled'));
   }
 
 }
