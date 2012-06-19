62a72324fdf5df1679c13881ed6389c383c60347
Teo <teo.marinov@gmail.com>
2012-06-19 19:13:56 +0300

    Work on MyCQ Collection edit page. (to-do http://goo.gl/Z9XP8) Added: "Add a new Collection" box, search and functional buttons.

--- apps/frontend/modules/mycq/templates/collectionsSuccess.php
+++ apps/frontend/modules/mycq/templates/collectionsSuccess.php
@@ -4,7 +4,6 @@
  */
 ?>
 
-<?php /**
 <div id="mycq-tabs">
   <ul class="nav nav-tabs">
     <li class="active"><a href="#" target="_self" title="Your Collection">Your Collection</a></li>
@@ -149,53 +148,6 @@
       </div><!-- .tab-content-inner -->
     </div><!-- #tab4.tab-pane -->
   </div><!-- .tab-content -->
-</div>
- */ ?>
-
-<div id="mycq-tabs">
-  <div class="tab-content">
-    <div class="tab-pane active" id="tab1">
-
-      <a name="my-collections"></a>
-      <div class="tab-content-inner spacer-top-35">
-        <div class="row-fluid sidebar-title spacer-inner-bottom">
-          <div class="span5 link-align">
-            <h3 class="Chivo webfont">My Collections (<?= $total ?>)</h3>
-          </div>
-          <div class="span7">
-            <?php if ($total > 11): ?>
-            <div class="sort-search-box">
-              <div class="input-append">
-                <form action="<?= url_for('@ajax_mycq?section=component&page=collections') ?>"
-                      id="form-mycq-collections" method="post">
-                  <div class="btn-group">
-                    <div class="append-left-gray">Sort by <strong id="sortByName">Most Recent</strong></div>
-                    <a class="btn gray-button dropdown-toggle" data-toggle="dropdown" href="#">
-                      <span class="caret arrow-up"></span><br><span class="caret arrow-down"></span>
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a data-sort="most-recent" data-name="Most Recent" class="sortBy" href="javascript:">Sort by <strong>Most Recent</strong></a></li>
-                      <li><a data-sort="most-relevant" data-name="Most Relevant" class="sortBy" href="javascript:">Sort by <strong>Most Relevant</strong></a></li>
-                    </ul>
-                  </div>
-                  <input type="text" class="input-sort-by" id="appendedPrependedInput" name="q"><button class="btn gray-button" type="submit"><strong>Search</strong></button>
-                  <input type="hidden" value="most-recent" id="sortByValue" name="s">
-                </form>
-              </div>
-            </div>
-            <?php endif; ?>
-          </div>
-        </div>
-
-        <div class="mycq-collections">
-          <div class="row thumbnails">
-            <?php include_component('mycq', 'collections'); ?>
-          </div>
-        </div>
-
-      </div><!-- /.tab-content-inner -->
-    </div>
-  </div><!-- /.tab-content -->
 </div>
 
 <script>
