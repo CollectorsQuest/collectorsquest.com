0a1c203b4c103d445cbfacb593826cecfbdcbcf8
Kiril Angov <kiril.angov@gmail.com>
2012-06-19 20:09:23 +0300

    Fixing the My CQ dropbox to the top of the screen for easier drag and drop

--- apps/frontend/modules/mycq/templates/_dropbox.php
+++ apps/frontend/modules/mycq/templates/_dropbox.php
@@ -99,5 +99,34 @@ $(document).ready(function()
         }
       });
     }, true));
+
+  // Calculate the height of <header>
+  // Use outerHeight() instead of height() if have padding
+  var aboveHeight = $('#slot1').outerHeight();
+
+  //when scroll
+  $(window).scroll(function()
+  {
+    // if scrolled down more than the header’s height
+    if ($(window).scrollTop() > aboveHeight)
+    {
+      // if yes, add “fixed” class to the <nav>
+      // add padding top to the #content
+      // (value is same as the height of the nav)
+      $('#dropzone-wrapper')
+        .addClass('fixed')
+        .css('top','0').next()
+        .css('padding-top','60px');
+
+    }
+    else
+    {
+      // when scroll up or less than aboveHeight,
+      // remove the “fixed” class, and the padding-top
+      $('#dropzone-wrapper')
+        .removeClass('fixed').next()
+        .css('padding-top','0');
+    }
+  });
 });
 </script>
