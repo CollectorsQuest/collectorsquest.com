300981210a20884d73dc4c13ed93c23ac2fcf0ba
Teo <teo.marinov@gmail.com>
2012-06-20 14:38:45 +0300

    Work on to-do: http://goo.gl/1bvKm (Collector's Guide page)

--- apps/frontend/modules/misc/templates/indexSuccess.php
+++ apps/frontend/modules/misc/templates/indexSuccess.php
@@ -1,44 +1,65 @@
-<?= cq_page_title('Unlock the essential guide to collecting—sign up today!') ?>
+<div class="guide-splash-container">
+  <div class="wrapper-top">
+    <div class="row-fluid">
+      <div class="span7">
+        <?php
+        cq_sidebar_title(
+          'Unlock the essential guide to collecting—sign up today!', null,
+          array('class' => 'row-fluid title-red spacer-bottom-30')
+        );
+        ?>
+        <div class="row-fluid">
+          <div class="span6">
+            <div class="spacer-right-30">
+              <p><b>Quest Your Best: The Essential Guide to Collecting</b> has something for every collector, whether you're just
+              beginning to acquire treasures or you're a dedicated hunter looking for the next, perfect addition to your display
+              case.</p>
 
-<div class="row-fluid">
-  <div class="span4">
-    <p><b>Quest Your Best: The Essential Guide to Collecting</b> has something for every collector, whether you're just
-      beginning to acquire treasures or you're a dedicated hunter looking for the next, perfect addition to your display
-      case.</p>
-
-    <p>Sign up to become a member and get this electronic guide for free. It just takes a minute to complete the form on
-      the right, and we’ll email you a link to get the PDF
-      (<a href="http://get.adobe.com/reader/">Adobe Acrobat required</a>)</p>
+            <p>Sign up to become a member and get this electronic guide for free. It just takes a minute to complete the form on
+              the right, and we’ll email you a link to get the PDF
+              (<a href="http://get.adobe.com/reader/">Adobe Acrobat required</a>)</p>
+            </div>
+          </div>
+          <div class="span6">
+            <img src="/images/frontend/misc/guide-splash-page-img.png" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="span5">
+        <div class="sign-up-form">
+          <?php include_partial('global/footer_login_signup', array(
+          'signup_form'=> $signup_form,
+          'login_form' => $login_form
+           )) ?>
+        </div>
+      </div>
+    </div>
   </div>
+  <div class="wrapper-bottom">
+    <?php
+    cq_sidebar_title(
+      'Why else should you join?', null,
+      array('class' => 'row-fluid section-title-yellow  spacer-bottom-30')
+    );
+    ?>
+    <div class="row-fluid">
+      <div class="span4">
+        <h4 class="Chivo webfont">Show off your collection</h4>
+        <p>You bought it, now brag about it! Let other collectors see what's hiding in your closets, or create a digital
+          archive to use on the go so you don't buy the same thing twice! (It's happened to all of us).</p>
+      </div>
 
-  <div class="span6">
-    <?php include_partial('global/footer_login_signup', array(
-    'signup_form'=> $signup_form,
-    'login_form' => $login_form
-  )) ?>
-  </div>
-</div>
+      <div class="span4">
+        <h4 class="Chivo webfont">Meet other collectors</h4>
+        <p>Find other like-minded collectors or branch out and meet new friends who can show you a cool thing or two.</p>
+      </div>
 
-<div class="row-fluid">
-  <h3>Why else should you join?</h3>
+      <div class="span4">
+        <h4 class="Chivo webfont">Hear about the latest<br> trends in collecting</h4>
+        <p>If it's out there, you'll hear about it here in our daily blog and video coverage of the latest and greatest
+          from the collecting universe.</p>
+      </div>
 
-  <div class="span3">
-    <h4>Show off your collection</h4>
-
-    <p>You bought it, now brag about it! Let other collectors see what's hiding in your closets, or create a digital
-      archive to use on the go so you don't buy the same thing twice! (It's happened to all of us).</p>
-  </div>
-
-  <div class="span3">
-    <h4>Meet other collectors</h4>
-
-    <p>Find other like-minded collectors or branch out and meet new friends who can show you a cool thing or two.</p>
-  </div>
-
-  <div class="span3">
-    <h4>Hear about the latest trends in collecting</h4>
-
-    <p>If it's out there, you'll hear about it here in our daily blog and video coverage of the latest and greatest
-      from the collecting universe.</p>
+    </div>
   </div>
 </div>
