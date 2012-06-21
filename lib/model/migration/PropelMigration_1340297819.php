fa4bfd8cf9865e1729169af67080925a4beda4b0
Ivan Tanev <vankata.t@gmail.com>
2012-06-21 20:04:37 +0300

    Add aggregate column credits_used to package_transaction

---
+++ lib/model/migration/PropelMigration_1340297819.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Add aggregate column to package_transaction:
+ *   -  credits_used integer aggregate column
+ *
+ * Generated on 2012-06-21 12:56:59 by root
+ */
+class PropelMigration_1340297819
+{
+
+	public function preUp($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postUp($manager)
+	{
+		// add the post-migration code here
+	}
+
+	public function preDown($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postDown($manager)
+	{
+		// add the post-migration code here
+	}
+
+	/**
+	 * Get the SQL statements for the Up migration
+	 *
+	 * @return array list of the SQL strings to execute for the Up migration
+	 *               the keys being the datasources
+	 */
+	public function getUpSQL()
+	{
+		return array (
+  'propel' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `package_transaction` ADD `credits_used` INTEGER DEFAULT 0 NOT NULL AFTER `credits`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+	}
+
+	/**
+	 * Get the SQL statements for the Down migration
+	 *
+	 * @return array list of the SQL strings to execute for the Down migration
+	 *               the keys being the datasources
+	 */
+	public function getDownSQL()
+	{
+		return array (
+  'propel' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `package_transaction` DROP `credits_used`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+	}
+
+}
