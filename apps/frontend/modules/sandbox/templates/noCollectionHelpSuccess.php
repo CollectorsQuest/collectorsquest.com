1d503f7af7199946d4af495ee91a013fa7873bc9
Teo <teo.marinov@gmail.com>
2012-06-22 05:05:50 +0300

    Work on the "no Collections created yet" page screen

--- apps/frontend/modules/sandbox/templates/noCollectionHelpSuccess.php
+++ apps/frontend/modules/sandbox/templates/noCollectionHelpSuccess.php
@@ -1,6 +1,37 @@
 
 
 <div class="no-collection-help">
+
+  <div class="row-fluid sidebar-title spacer-top-reset spacer-inner-bottom-5">
+    <div class="span5 link-align">
+      <h3 class="Chivo webfont">My Collections (0)</h3>
+    </div>
+    <div class="span7">
+      <?php if ($total > 0): ?>
+      <div class="sort-search-box">
+        <div class="input-append pull-right">
+          <form action="<?= url_for('@ajax_mycq?section=component&page=collections') ?>"
+                id="form-mycq-collections" method="post">
+            <div class="btn-group">
+              <div class="append-left-gray">Sort by <strong id="sortByName">Most Recent</strong></div>
+              <a class="btn gray-button dropdown-toggle" data-toggle="dropdown" href="#">
+                <span class="caret arrow-up"></span><br><span class="caret arrow-down"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a data-sort="most-recent" data-name="Most Recent" class="sortBy" href="javascript:">Sort by <strong>Most Recent</strong></a></li>
+                <li><a data-sort="most-relevant" data-name="Most Relevant" class="sortBy" href="javascript:">Sort by <strong>Most Relevant</strong></a></li>
+              </ul>
+            </div>
+            <input type="text" class="input-sort-by" id="appendedPrependedInput" name="q"><button class="btn gray-button" type="submit"><strong>Search</strong></button>
+            <input type="hidden" value="most-recent" id="sortByValue" name="s">
+          </form>
+        </div>
+      </div>
+      <?php endif; ?>
+    </div>
+  </div>
+
+
   <div class="row-fluid">
     <div class="span9 welcome-mycq">
       <div class="rectangle">
