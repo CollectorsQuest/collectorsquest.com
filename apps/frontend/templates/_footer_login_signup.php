960a036e73c1fd34f51eb5cf328ddce51f7ffe11
Kiril Angov <kiril.angov@gmail.com>
2012-06-27 13:16:49 +0300

    Changes to _footer_login_signup.php
    * Fix wrapping issue for _footer_login_signup.php
    * Code formatting in _footer_login_signup.php

--- apps/frontend/templates/_footer_login_signup.php
+++ apps/frontend/templates/_footer_login_signup.php
@@ -1,21 +1,28 @@
 <?php
-/* @var $sf_user cqFrontendUser */
-/* @var $signup_form CollectorSignupStep1Form */
-/* @var $login_form CollectorLoginForm */
-use_javascripts_for_form($signup_form);
+  /**
+   * @var $sf_request sfWebRequest
+   * @var $sf_user cqFrontendUser
+   *
+   * @var $signup_form CollectorSignupStep1Form
+   * @var $login_form CollectorLoginForm
+   */
+  use_javascripts_for_form($signup_form);
 
-$signup_action = isset($signup_action) ? $signup_action : '@collector_signup';
-$login_action = isset($login_action) ? $login_action : '@login';
-$display = $sf_request->getParameter('display', 'signup');
+  $signup_action = isset($signup_action) ? $signup_action : '@collector_signup';
+  $login_action = isset($login_action) ? $login_action : '@login';
+  $display = $sf_request->getParameter('display', 'signup');
 ?>
 
 <div id="footer-form-signup" style="<?= 'signup' == $display ? '' : 'display: none;'?>">
   <h2 class="Chivo webfont">Sign Up</h2>
 
-  <form action="<?= url_for($signup_action, true); ?>" method="post" class="form-horizontal form-footer">
+  <form action="<?= url_for($signup_action, true); ?>"
+        method="post" class="form-horizontal form-footer">
+
     <?= $signup_form->renderUsing('BootstrapWithRowFluid'); ?>
+
     <div class="row-fluid spacer-7">
-      <div class="span9 spacer-inner-top">
+      <div class="span8 spacer-inner-top">
         <?php
           include_partial(
             'global/footer_signup_external_buttons',
@@ -23,7 +30,7 @@ $display = $sf_request->getParameter('display', 'signup');
           );
         ?>
       </div>
-      <div class="span3">
+      <div class="span4">
         <button type="submit" class="btn btn-primary pull-right">Sign Up</button>
       </div>
     </div>
@@ -31,7 +38,8 @@ $display = $sf_request->getParameter('display', 'signup');
 
   <div id="footer-control-login">
     <span class="pull-right">
-      Already have an account? <?= link_to('Log In', '@login', array('id' => 'footer-control-login-button')); ?>
+      Already have an account?
+      <?= link_to('Log In', '@login', array('id' => 'footer-control-login-button')); ?>
     </span>
   </div>
 </div><!-- #footer-form-signup -->
@@ -39,7 +47,8 @@ $display = $sf_request->getParameter('display', 'signup');
 <div id="footer-form-login" style="<?= 'login' == $display ? '' : 'display: none;' ?>">
   <h2 class="Chivo webfont">Log In</h2>
 
-  <form action="<?= url_for($login_action, true); ?>" class="form-horizontal form-footer" method="post">
+  <form action="<?= url_for($login_action, true); ?>"
+        method="post" class="form-horizontal form-footer">
     <?php $login_form->renderHiddenFields(); ?>
 
     <?= $login_form['username']->renderRow(); ?>
@@ -76,7 +85,9 @@ $display = $sf_request->getParameter('display', 'signup');
     </div>
     <div class="row-fluid">
       <div class="span12">
-        <span class="pull-right"><?= link_to('Forgot your password?', '@recover_password'); ?></span>
+        <span class="pull-right">
+          <?= link_to('Forgot your password?', '@recover_password'); ?>
+        </span>
       </div>
     </div>
   </form>
@@ -84,7 +95,12 @@ $display = $sf_request->getParameter('display', 'signup');
   <div id="footer-control-signup" style="display: none">
     <span class="pull-right">
       Don't have an account yet?
-      <?= link_to('Sign up now!', '@collector_signup', array('id' => 'footer-control-signup-button')); ?>
+      <?php
+        echo link_to(
+          'Sign up now!', '@collector_signup',
+          array('id' => 'footer-control-signup-button')
+        );
+      ?>
     </span>
   </div>
 </div> <!-- #footer-form-login -->
