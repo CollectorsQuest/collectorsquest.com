0177e674fd193fc081a4c7cab50197b5bb0e2146
IvanTanev <vankata.t@gmail.com>
2012-06-27 15:23:38 +0300

    Hammers can have fallbacks too.

--- apps/frontend/templates/_footer_signup_external_buttons.php
+++ apps/frontend/templates/_footer_signup_external_buttons.php
@@ -1,10 +1,10 @@
 <?= ucfirst($action); ?> with&nbsp;
-<a href="<?= url_for('@login#modal-rpx-login-holder'); ?>" title="Login using Facebook" class="s-16-icon-facebook" onclick="$('#modal-login-holder').modal('show').find('a[href=#modal-login-openid-pane]').tab('show'); return false;">
+<a href="<?= url_for('@login#rpx-login'); ?>" title="Login using Facebook" class="s-16-icon-facebook" onclick="return jQuery && $('#modal-login-holder').modal('show').find('a[href=#modal-login-openid-pane]').tab('show') && false; ">
   <i class="hide-text">Login using Facebook</i>
 </a>
-<a href="<?= url_for('@login#modal-rpx-login-holder'); ?>" title="Login using Twitter" class="s-16-icon-twitter" onclick="$('#modal-login-holder').modal('show').find('a[href=#modal-login-openid-pane]').tab('show'); return false;">
+<a href="<?= url_for('@login#rpx-login'); ?>" title="Login using Twitter" class="s-16-icon-twitter" onclick="return jQuery && $('#modal-login-holder').modal('show').find('a[href=#modal-login-openid-pane]').tab('show') && false;">
   <i class="hide-text">Login using Twitter</i>
 </a>
-<a href="<?= url_for('@login#modal-rpx-login-holder'); ?>" title="Login using Google" class="s-16-icon-google" onclick="$('#modal-login-holder').modal('show').find('a[href=#modal-login-openid-pane]').tab('show'); return false;">
+<a href="<?= url_for('@login#rpx-login'); ?>" title="Login using Google" class="s-16-icon-google" onclick="return jQuery && $('#modal-login-holder').modal('show').find('a[href=#modal-login-openid-pane]').tab('show') && false;">
   <i class="hide-text">Login using Google</i>
 </a>
