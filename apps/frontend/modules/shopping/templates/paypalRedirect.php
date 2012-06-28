97ef7bdf7d678683af5be44a3e13618e032685b2
Kiril Angov <kiril.angov@gmail.com>
2012-06-28 20:00:37 +0300

    Fix for the "PayPal Cancel" not redirecting properly

--- apps/frontend/modules/shopping/templates/paypalRedirect.php
+++ apps/frontend/modules/shopping/templates/paypalRedirect.php
@@ -4,17 +4,15 @@
   <title>Redirecting...</title>
   <script src="//www.paypalobjects.com/js/external/dg.js"></script>
   <script>
-    if (
-      (top && (_dgFlow = top.dgFlow) && (_top = top)) ||
-      (top.opener && (_dgFlow = top.opener.top.dgFlow) && (_top = top.opener.top))
-    ) {
+    var _top = top;
+
+    if (top.opener && (_top = top.opener.top) && (_dgFlow = top.opener.top.dgFlow))
+    {
       _dgFlow.closeFlow();
       top.close();
     }
 
-    if (_top !== undefined) {
-      _top.location.replace('<?= url_for($url); ?>');
-    }
+    (_top || top).location.replace('<?= url_for($url); ?>');
   </script>
 </head>
 <body style="background: #fff;">
