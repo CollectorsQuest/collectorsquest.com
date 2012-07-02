4f40c8f47f596962e2b9ac2f2db272a31c818b1d
Kiril Angov <kiril.angov@gmail.com>
2012-07-02 23:37:08 +0300

    Fixing migrations to include entries for Propel connection "blog" (otherwise the migration version does not get updated)

--- lib/model/migration/PropelMigration_1341164495.php
+++ lib/model/migration/PropelMigration_1341164495.php
@@ -7,80 +7,85 @@
 class PropelMigration_1341164495
 {
 
-	public function preUp($manager)
-	{
-		// add the pre-migration code here
-	}
+  public function preUp($manager)
+  {
+    // add the pre-migration code here
+  }
 
-	public function postUp($manager)
-	{
-		// add the post-migration code here
-	}
+  public function postUp($manager)
+  {
+    // add the post-migration code here
+  }
 
-	public function preDown($manager)
-	{
-		// add the pre-migration code here
-	}
+  public function preDown($manager)
+  {
+    // add the pre-migration code here
+  }
 
-	public function postDown($manager)
-	{
-		// add the post-migration code here
-	}
+  public function postDown($manager)
+  {
+    // add the post-migration code here
+  }
 
-	/**
-	 * Get the SQL statements for the Up migration
-	 *
-	 * @return array list of the SQL strings to execute for the Up migration
-	 *               the keys being the datasources
-	 */
-	public function getUpSQL()
-	{
-		return array (
-  'propel' => '
-# This is a fix for InnoDB in MySQL >= 4.1.x
-# It "suspends judgement" for fkey relationships until are tables are set.
-SET FOREIGN_KEY_CHECKS = 0;
+  /**
+   * Get the SQL statements for the Up migration
+   *
+   * @return array list of the SQL strings to execute for the Up migration
+   *               the keys being the datasources
+   */
+  public function getUpSQL()
+  {
+    return array(
+      'propel' => '
+        # This is a fix for InnoDB in MySQL >= 4.1.x
+        # It "suspends judgement" for fkey relationships until are tables are set.
+        SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE `private_message_extra_property`
-(
-	`id` INTEGER NOT NULL AUTO_INCREMENT,
-	`property_name` VARCHAR(255) NOT NULL,
-	`property_value` TEXT,
-	`private_message_id` INTEGER NOT NULL,
-	PRIMARY KEY (`id`),
-	INDEX `private_message_extra_property_FI_1` (`private_message_id`),
-	CONSTRAINT `private_message_extra_property_FK_1`
-		FOREIGN KEY (`private_message_id`)
-		REFERENCES `private_message` (`id`)
-		ON DELETE CASCADE
-) ENGINE=InnoDB;
+        CREATE TABLE `private_message_extra_property`
+        (
+          `id` INTEGER NOT NULL AUTO_INCREMENT,
+          `property_name` VARCHAR(255) NOT NULL,
+          `property_value` TEXT,
+          `private_message_id` INTEGER NOT NULL,
+          PRIMARY KEY (`id`),
+          INDEX `private_message_extra_property_FI_1` (`private_message_id`),
+          CONSTRAINT `private_message_extra_property_FK_1`
+            FOREIGN KEY (`private_message_id`)
+            REFERENCES `private_message` (`id`)
+            ON DELETE CASCADE
+        ) ENGINE=InnoDB;
 
-# This restores the fkey checks, after having unset them earlier
-SET FOREIGN_KEY_CHECKS = 1;
-',
-);
-	}
+        # This restores the fkey checks, after having unset them earlier
+        SET FOREIGN_KEY_CHECKS = 1;
+      ',
+      'blog' => '
+        SET FOREIGN_KEY_CHECKS = 0;
+        SET FOREIGN_KEY_CHECKS = 1;
+      '
+    );
+  }
 
-	/**
-	 * Get the SQL statements for the Down migration
-	 *
-	 * @return array list of the SQL strings to execute for the Down migration
-	 *               the keys being the datasources
-	 */
-	public function getDownSQL()
-	{
-		return array (
-  'propel' => '
-# This is a fix for InnoDB in MySQL >= 4.1.x
-# It "suspends judgement" for fkey relationships until are tables are set.
-SET FOREIGN_KEY_CHECKS = 0;
+  /**
+   * Get the SQL statements for the Down migration
+   *
+   * @return array list of the SQL strings to execute for the Down migration
+   *               the keys being the datasources
+   */
+  public function getDownSQL()
+  {
+    return array(
+      'propel'  => '
+        SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS `private_message_extra_property`;
+        DROP TABLE IF EXISTS `private_message_extra_property`;
 
-# This restores the fkey checks, after having unset them earlier
-SET FOREIGN_KEY_CHECKS = 1;
-',
-);
-	}
+        SET FOREIGN_KEY_CHECKS = 1;
+      ',
+      'blog' => '
+        SET FOREIGN_KEY_CHECKS = 0;
+        SET FOREIGN_KEY_CHECKS = 1;
+      '
+    );
+  }
 
 }
