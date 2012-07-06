b5a86dc170abb2e3b685415b6f4c6d0486065504
Kiril Angov <kiril.angov@gmail.com>
2012-07-06 23:55:53 +0300

    Text copy tweaks

--- apps/frontend/modules/shopping/templates/_sidebarSignup.php
+++ apps/frontend/modules/shopping/templates/_sidebarSignup.php
@@ -1,23 +1,27 @@
-<?php cq_sidebar_title('Signup for an Account', null, array('style' => 'margin-top: 0;')); ?>
+<?php if (!$sf_user->isAuthenticated()): ?>
 
-<div class="signup-form-sidebar">
-  <form action="<?= url_for('@collector_signup', true); ?>" method="post"
-        class="form-horizontal">
-    <?= $form->renderUsing('BootstrapWithRowFluid'); ?>
-    <div class="row-fluid spacer-7">
-      <div class="span9 spacer-inner-top">
-        <?php
-          include_partial(
-            'global/footer_signup_external_buttons',
-            array('action' => 'signup')
-          );
-        ?>
+  <?php cq_sidebar_title('Signup for an Account', null, array('style' => 'margin-top: 0;')); ?>
+
+  <div class="signup-form-sidebar">
+    <form action="<?= url_for('@collector_signup', true); ?>" method="post"
+          class="form-horizontal">
+      <?= $form->renderUsing('BootstrapWithRowFluid'); ?>
+      <div class="row-fluid spacer-7">
+        <div class="span9 spacer-inner-top">
+          <?php
+            include_partial(
+              'global/footer_signup_external_buttons',
+              array('action' => 'signup')
+            );
+          ?>
+        </div>
+        <div class="span3">
+          <button type="submit" class="btn btn-primary pull-right">
+            Submit
+          </button>
+        </div>
       </div>
-      <div class="span3">
-        <button type="submit" class="btn btn-primary pull-right">
-          Submit
-        </button>
-      </div>
-    </div>
-  </form>
-</div>
+    </form>
+  </div>
+
+<?php endif; ?>
