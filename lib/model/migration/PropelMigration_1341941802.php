dcd83d008eb0bc443724f42b7a2ca97e5efdffa2
IvanTanev <vankata.t@gmail.com>
2012-07-10 21:55:24 +0300

    [DB Update] Add collector.uuid field

---
+++ lib/model/migration/PropelMigration_1341941802.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Add collector.uuid field
+ */
+class PropelMigration_1341941802
+{
+
+	public function preUp($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postUp($manager)
+	{
+		// add the post-migration code here
+	}
+
+	public function preDown($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postDown($manager)
+	{
+		// add the post-migration code here
+	}
+
+	/**
+	 * Get the SQL statements for the Up migration
+	 *
+	 * @return array list of the SQL strings to execute for the Up migration
+	 *               the keys being the datasources
+	 */
+	public function getUpSQL()
+	{
+		return array (
+      'propel' => '
+        # This is a fix for InnoDB in MySQL >= 4.1.x
+        # It "suspends judgement" for fkey relationships until are tables are set.
+        SET FOREIGN_KEY_CHECKS = 0;
+
+
+        ALTER TABLE `collector`
+        ADD `uuid` VARCHAR(32) AFTER `session_id`;
+        CREATE UNIQUE INDEX `collector_U_7` ON `collector` (`uuid`);
+
+        ALTER TABLE `collector_archive`
+        ADD `uuid` VARCHAR(32) AFTER `session_id`;
+
+
+        # This restores the fkey checks, after having unset them earlier
+        SET FOREIGN_KEY_CHECKS = 1;
+      ',
+      'blog' => '
+        SET FOREIGN_KEY_CHECKS = 0;
+        SET FOREIGN_KEY_CHECKS = 1;
+      '
+    );
+	}
+
+	/**
+	 * Get the SQL statements for the Down migration
+	 *
+	 * @return array list of the SQL strings to execute for the Down migration
+	 *               the keys being the datasources
+	 */
+	public function getDownSQL()
+	{
+		return array (
+      'propel' => '
+        # This is a fix for InnoDB in MySQL >= 4.1.x
+        # It "suspends judgement" for fkey relationships until are tables are set.
+        SET FOREIGN_KEY_CHECKS = 0;
+
+
+        ALTER TABLE `collector`
+        DROP `uuid`;
+
+        ALTER TABLE `collector_archive`
+        DROP `uuid`;
+
+
+        # This restores the fkey checks, after having unset them earlier
+        SET FOREIGN_KEY_CHECKS = 1;
+      ',
+      'blog' => '
+        SET FOREIGN_KEY_CHECKS = 0;
+        SET FOREIGN_KEY_CHECKS = 1;
+      '
+    );
+	}
+
+}
