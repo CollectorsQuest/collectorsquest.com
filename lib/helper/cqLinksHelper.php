9fe0d9f43c724927ea19a588a80263efa73f4c56
Kiril Angov <kiril.angov@gmail.com>
2012-07-18 22:56:37 +0300

    Work for to-do: https://basecamp.com/1759305/projects/19290-collectorsquest-com/todos/10940420-please-remove-tod

--- lib/helper/cqLinksHelper.php
+++ lib/helper/cqLinksHelper.php
@@ -411,7 +411,7 @@ function link_to_blog_author(wpUser $author, $type = 'text', $options = array())
   switch ($type)
   {
     case "image":
-      if (!$avatar_url = $author->getAvatarUrl('64'))
+      if (!$avatar_url = $author->getAvatarUrl('40'))
       {
         $avatar_url = 'blog/avatar-' . str_replace(' ', '-', strtolower($author->getDisplayName()));
       }
