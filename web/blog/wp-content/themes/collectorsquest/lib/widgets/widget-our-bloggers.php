dc004f26e09cd3c4e2cf04ad22aff6e7b95ae063
Kiril Angov <kiril.angov@gmail.com>
2012-07-18 23:16:33 +0300

    Removing blogger #6 from the sidebar widget

--- web/blog/wp-content/themes/collectorsquest/lib/widgets/widget-our-bloggers.php
+++ web/blog/wp-content/themes/collectorsquest/lib/widgets/widget-our-bloggers.php
@@ -75,7 +75,7 @@ class cq_our_bloggers_widget extends WP_Widget {
         }
         $exclude = implode(',', $exclude);
         $exclude = str_replace(",7", "", $exclude);
-        $blogusers = get_users('exclude=' . $exclude . ',4,11,13,23');
+        $blogusers = get_users('exclude=' . $exclude . ',4,6,11,13,23');
       }
       $authors = array();
       foreach ($blogusers as $bloguser) {
