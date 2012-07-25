9bcc973f610370ae9e2ee1d77a0491fa3c3ab1f6
TeodorMarinov <teo.marinov@gmail.com>
2012-07-25 12:54:34 +0300

    Fixed Toolbar Search field to-do https://basecamp.com/1759305/projects/824949-collectorsquest-com/todos/11370020-toolbar-search

--- apps/frontend/modules/mycq/templates/collectionsSuccess.php
+++ apps/frontend/modules/mycq/templates/collectionsSuccess.php
@@ -36,21 +36,11 @@ cq_sidebar_title(
       </ul>
     </div>
     <div class="span6">
-      <?php if ($total > 0): ?>
-      <div class="sort-search-box">
+      <?php if ($total > 11): ?>
+      <div class="mini-input-append-search">
         <div class="input-append pull-right">
           <form action="<?= url_for('@ajax_mycq?section=component&page=collections') ?>"
                 id="form-mycq-collections" method="post">
-            <div class="btn-group">
-              <div class="append-left-gray">Sort by <strong id="sortByName">Most Recent</strong></div>
-              <a class="btn gray-button dropdown-toggle" data-toggle="dropdown" href="#">
-                <span class="caret arrow-up"></span><br><span class="caret arrow-down"></span>
-              </a>
-              <ul class="dropdown-menu">
-                <li><a data-sort="most-recent" data-name="Most Recent" class="sortBy" href="javascript:">Sort by <strong>Most Recent</strong></a></li>
-                <li><a data-sort="most-relevant" data-name="Most Relevant" class="sortBy" href="javascript:">Sort by <strong>Most Relevant</strong></a></li>
-              </ul>
-            </div>
             <input type="text" class="input-sort-by" id="appendedPrependedInput" name="q"><button class="btn gray-button" type="submit"><strong>Search</strong></button>
             <input type="hidden" value="most-recent" id="sortByValue" name="s">
           </form>
