428f51f699638f2ff6ab7f388bcc319ca016c6f0
TeodorMarinov <teo.marinov@gmail.com>
2012-07-30 09:03:55 +0300

    Added green "ADD IT" rollover on the /collection/details page

--- apps/frontend/modules/mycq/templates/_collectionMultimedia.php
+++ apps/frontend/modules/mycq/templates/_collectionMultimedia.php
@@ -8,26 +8,35 @@
 
 <div class="drop-zone-large thumbnail collection">
   <?php if (isset($image) && $image instanceof iceModelMultimedia): ?>
-    <?= image_tag_multimedia($image, '190x190'); ?>
-    <span class="icon-plus-holder h-center dn spacer-top-25">
-      <i class="icon icon-download-alt icon-white"></i>
-    </span>
-    <i class="icon icon-remove-sign" data-multimedia-id="<?= $image->getId(); ?>"></i>
-    <span class="multimedia-edit holder-icon-edit"
-          data-original-image-url="<?= src_tag_multimedia($image, 'original') ?>"
-          data-post-data='<?= $aviary_hmac_message; ?>'>
+    <div class="alt-view-img">
+      <?= image_tag_multimedia($image, '190x190'); ?>
+      <i class="icon icon-remove-sign" data-multimedia-id="<?= $image->getId(); ?>"></i>
+      <span class="multimedia-edit holder-icon-edit"
+            data-original-image-url="<?= src_tag_multimedia($image, 'original') ?>"
+            data-post-data='<?= $aviary_hmac_message; ?>'>
 
-      <i class="icon icon-camera"></i><br/>
-      Edit Photo
-    </span>
+        <i class="icon icon-camera"></i><br/>
+        Edit Photo
+      </span>
+    </div>
   <?php else: ?>
-    <span class="icon-plus-holder h-center">
-      <i class="icon icon-plus icon-white"></i>
-    </span>
-    <div class="info-text">
-      Drag and drop from <br>"Uploaded Photos"
+    <div class="alt-view-slot">
+      <span class="icon-plus-holder h-center">
+        <i class="icon icon-plus icon-white"></i>
+      </span>
+      <div class="info-text">
+        Drag and drop from <br>"Uploaded Photos"
+      </div>
     </div>
   <?php endif; ?>
+  <div class="hidden">
+    <span class="icon-plus-holder h-center">
+     <i class="icon icon-plus"></i>
+    </span>
+    <span class="info-text spacer-top-5">
+      ADD ITEM
+    </span>
+  </div>
 </div>
 
 <script>
@@ -39,7 +48,7 @@ $(document).ready(function()
       var $icon = $(this);
 
       $icon.hide();
-      $icon.parent('div.ui-droppable').showLoading();
+      $icon.parent('div.alt-view-img').showLoading();
 
       $.ajax({
         url: '<?= url_for('@ajax_mycq?section=multimedia&page=delete&encrypt=1'); ?>',
@@ -48,12 +57,12 @@ $(document).ready(function()
         {
           $('#main-image').load(
             '<?= url_for('@ajax_mycq?section=component&page=collectionMultimedia&collection_id='. $collection->getId()); ?>',
-            function () { $icon.parent('div.ui-droppable').hideLoading(); }
+            function () { $icon.parent('div.alt-view-img').hideLoading(); }
           );
         },
         error: function()
         {
-          $icon.parent('div.ui-droppable').hideLoading();
+          $icon.parent('div.alt-view-img').hideLoading();
           $icon.show();
         }
       });
@@ -63,30 +72,23 @@ $(document).ready(function()
   {
     over: function(event, ui)
     {
-      $(this).addClass('ui-state-highlight');
-      $(this).find('.icon-plus-holder i')
-        .removeClass('icon-plus')
-        .addClass('icon-download-alt');
-      $(this).find('img').hide();
-      $(this).find('span.icon-plus-holder').show();
+      var $this = $(this);
+      $this.addClass('ui-state-highlight');
+      $this.addClass('over');
+      $this.find('img').hide();
     },
     out: function(event, ui)
     {
-      $(this).removeClass('ui-state-highlight');
-      $(this).find('.icon-plus-holder i')
-        .removeClass('icon-download-alt')
-        .addClass('icon-plus');
-      $(this).find('span.icon-plus-holder').hide();
-      $(this).find('img').show();
+      var $this = $(this);
+      $this.removeClass('ui-state-highlight');
+      $this.removeClass('over');
+      $this.find('img').show();
     },
     drop: function(event, ui)
     {
       var $this = $(this);
-
       $this.removeClass('ui-state-highlight');
-      $this.find('.holder-icon-edit i')
-        .removeClass('icon-download-alt')
-        .addClass('icon-plus');
+      $this.removeClass('over');
       ui.draggable.draggable('option', 'revert', false);
 
       $this.showLoading();
