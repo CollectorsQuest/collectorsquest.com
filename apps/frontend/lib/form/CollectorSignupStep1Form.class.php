38bdd1f199e3fb04d87f63ace4a3044bc56d52f2
denis.galas <denis@ddi-dev.com>
2012-07-30 16:59:29 +0300

    email validation only one error message

--- apps/frontend/lib/form/CollectorSignupStep1Form.class.php
+++ apps/frontend/lib/form/CollectorSignupStep1Form.class.php
@@ -2,128 +2,141 @@
 
 class CollectorSignupStep1Form extends BaseForm
 {
-  public function configure()
-  {
-    $this->setWidgets(array(
+	public function configure()
+	{
+		$this->setWidgets(array(
       'username'        => new sfWidgetFormInputText(array(
           'label'       => 'Username'
-        ), array(
+		), array(
           'pattern'     => '^(?=^[a-zA-Z])[a-zA-Z0-9\.\_]*$',
           'required'    => 'required',
           'placeholder' => 'Username',
-      )),
+		)),
       'password'        => new sfWidgetFormInputPassword(array(), array(
           'required'    => 'required',
           'placeholder' => 'Password',
-      )),
+		)),
       'password_again'  => new sfWidgetFormInputPassword(array(
           'label'       => 'Confirm Password'
-        ), array(
+		), array(
           'required'    => 'required',
           'placeholder' => 'Confirm Password'
-      )),
+		)),
       'email'           => new sfWidgetFormInputText(array(), array(
           'type'        => 'email',
           'required'    => 'required',
           'placeholder' => 'Email'
-      )),
+		)),
       'seller'          => new sfWidgetFormSelectRadio(array(
         'label'       => 'Choose one',
         'choices'     => array(
-          0 => 'I collect',
-          1 => 'I sell',
-          2 => 'I collect and sell',
-        ),
+		0 => 'I collect',
+		1 => 'I sell',
+		2 => 'I collect and sell',
+		),
 
         'formatter'   =>   function($widget, $inputs) {
-          $rows = array();
-          foreach ($inputs as $input)
-          {
-            $rows[] = $widget->renderContentTag('label',
-              $input['input'].$widget->getOption('label_separator').strip_tags($input['label']),
-              array('class'=>'radio'));
-          }
+		$rows = array();
+		foreach ($inputs as $input)
+		{
+			$rows[] = $widget->renderContentTag('label',
+			$input['input'].$widget->getOption('label_separator').strip_tags($input['label']),
+			array('class'=>'radio'));
+		}
 
-          return !$rows ? '' : $widget->renderContentTag('div', implode($widget->getOption('separator'), $rows), array('class' => $widget->getOption('class')));
-        }
+		return !$rows ? '' : $widget->renderContentTag('div', implode($widget->getOption('separator'), $rows), array('class' => $widget->getOption('class')));
+		}
 
-      ), array(
+		), array(
         'required' => 'required',
         'style' => 'padding-top: 3px;'
-      )),
+		)),
       'newsletter'  => new sfWidgetFormInputCheckbox(array(), array(
         'checked'   => 'checked'
-      )),
-    ));
+		)),
+		));
 
-    $this->setValidators(array(
+		$this->setValidators(array(
       'username'       => new sfValidatorRegex(
-        array(
+		array(
           'pattern'    => '/^(?=^[a-zA-Z])[a-zA-Z0-9\.\_]*$/',
           'min_length' => 3,
           'max_length' => 50,
           'required'   => true,
-        ),
-        array(
+		),
+		array(
           'invalid'    => 'Only letters, numbers, periods and underscores allowed. Must start with a letter.',
           'max_length' => 'The username "%value%" is too long (%max_length% characters max).',
           'min_length' => 'The username "%value%" is too short (%min_length% characters min).',
-        )
-      ),
+		)
+		),
       'password'       => new sfValidatorString(
-        array(
+		array(
           'min_length' => 6,
           'max_length' => 50,
           'required'   => true,
-         ), array(
+		), array(
           'max_length' => 'The password is too long (%max_length% characters max).',
           'min_length' => 'The password is too short (%min_length% characters min).',
-       )),
+		)),
       'seller'         => new sfValidatorChoice(array(
           'choices'    => array(0,1,2),
           'required'   => true,
-      )),
+		)),
       'email'          => new sfValidatorEmail(
-        array(
+		array(
           'required'   => true
-        ), array(
+		), array(
           'invalid'    => 'This email address is invalid.',
-        )),
+		)),
       'newsletter'     => new sfValidatorBoolean(array('required' => false))
-    ));
+		));
 
-    $this->validatorSchema['password_again'] = clone $this->validatorSchema['password'];
+		$this->validatorSchema['password_again'] = clone $this->validatorSchema['password'];
 
-    $this->mergePostValidator(new sfValidatorPropelUnique(
-      array('model' => 'Collector', 'column' => array('username')),
-      array('invalid' => 'This username is already taken, please choose another one!')
-    ));
+		$this->mergePostValidator(new sfValidatorPropelUnique(
+		array('model' => 'Collector', 'column' => array('username')),
+		array('invalid' => 'This username is already taken, please choose another one!')
+		));
 
-    if (isset($_POST['signup_step1']['email']) && $_POST['signup_step1']['email'] != '')
-    {
-	    $this->mergePostValidator(new sfValidatorPropelUnique(
-	      array('model' => 'Collector', 'column' => array('email')),
-	      array('invalid' => 'This email already has an account, did you forget your password?')
-	    ));
-    }
+		$this->mergePostValidator(
+		new sfValidatorCallback(array('callback' => array($this, 'postValidate')))
+		);
 
-    $this->mergePostValidator(new sfValidatorSchemaCompare(
+		$this->mergePostValidator(new sfValidatorSchemaCompare(
       'password', sfValidatorSchemaCompare::EQUAL, 'password_again',
-      array('throw_global_error' => true),
-      array('invalid' => 'The two passwords do not match, please enter them again!')
-    ));
+		array('throw_global_error' => true),
+		array('invalid' => 'The two passwords do not match, please enter them again!')
+		));
 
-    $this->setDefault('seller', 0);
+		$this->setDefault('seller', 0);
 
-    $this->widgetSchema->setNameFormat('signup_step1[%s]');
-    $this->widgetSchema->setFormFormatterName('Bootstrap');
-  }
+		$this->widgetSchema->setNameFormat('signup_step1[%s]');
+		$this->widgetSchema->setFormFormatterName('Bootstrap');
+	}
 
-  public function getJavaScripts()
-  {
-    return array(
+	public function getJavaScripts()
+	{
+		return array(
         'jquery/mailcheck.js',
-    );
-  }
+		);
+	}
+
+	public function postValidate($validator, $values)
+	{
+		// only run the post validator if the email has passed validation
+		// if it hasn't passed validation, it will be blank and we can skip this
+		if ($values['email'] && $values['email'] != '')
+		{
+			$v = new sfValidatorPropelUnique(
+			array('model' => 'Collector', 'column' => array('email')),
+			array('invalid' => 'This email already has an account, did you forget your password?')
+			);
+			$v->clean($values);
+		}
+
+		return $values;
+	}
+
 
 }
