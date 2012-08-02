324b526f8a5a8713dd935bcd5cb30b9d35053aa7
IvanTanev <vankata.t@gmail.com>
2012-08-02 12:55:48 +0300

    Implement the option to manually set category names that should always be sorted to the top or bottom

--- lib/model/ContentCategory.php
+++ lib/model/ContentCategory.php
@@ -81,16 +81,41 @@ class ContentCategory extends BaseContentCategory
       return true;
     }
 
-    $names = $siblings->toKeyValue('Id', 'Name');
+    $names = $sorted_names = $siblings->toKeyValue('Id', 'Name');
 
-    $iter = new ArrayIterator($names);
-    $iter->natcasesort();
+    // use native case sort for sorted names
+    natcasesort($sorted_names);
 
-    if ($names === $iter->getArrayCopy())
+    // handle force names sorted to top or bottom
+    foreach (ContentCategoryPeer::$force_order_to_top as $name_to_top)
+    {
+      if (false !== $key = array_search($name_to_top, $sorted_names))
+      {
+        unset ($sorted_names[$key]);
+        // we want to add the element in the first position, but with a specific key
+        // so we use the specail properties of the addition operator for arrays
+        $sorted_names = array($key => $name_to_top) + $sorted_names;
+      }
+    }
+    foreach (ContentCategoryPeer::$force_order_to_bottom as $name_to_bottom)
+    {
+      if (false !== $key = array_search($name_to_bottom, $sorted_names))
+      {
+        unset ($sorted_names[$key]);
+        // we want to add the element in the last position, but with a specific key
+        // so we use the specail properties of the addition operator for arrays
+        $sorted_names = $sorted_names + array($key => $name_to_bottom);
+      }
+    }
+
+    if ($sorted_names === $names)
     {
       // siblings already sorted, nothing to do here.
       return true;
     }
+
+    // create an iterator for the sorted names
+    $iter = new ArrayIterator($sorted_names);
 
     // move internal array pointer to first element
     $iter->rewind();
