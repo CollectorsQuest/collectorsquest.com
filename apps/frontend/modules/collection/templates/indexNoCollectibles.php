41ee0d6ad455c249460f05b5810e0b18d5d7fa31
TeodorMarinov <teo.marinov@gmail.com>
2012-08-08 11:53:22 +0300

    Fix for displaying "more interesting collections" on the "empty collection" page

--- apps/frontend/modules/collection/templates/indexNoCollectibles.php
+++ apps/frontend/modules/collection/templates/indexNoCollectibles.php
@@ -43,27 +43,32 @@ else
   }
   else
   {
-    echo __('We are sorry but this collection is currently empty.');
-    echo '&nbsp;';
+    echo '
+    <div class="alert spacer-top-40">
+      <button type="button" class="close" data-dismiss="alert">×</button>
+      <i class="icon-exclamation-sign alert-icon-orange"></i>
+      We are sorry but this collection is currently empty.
+    </div>
+    ';
     //echo __('If you are interested, you can use <b>"Message Collector"</b> to the right and encourage the collector to upload pictures of their collectibles.');
   }
 }
 ?>
 
-<br clear="all"><br>
 <?php if (!empty($collections)): ?>
 <?php cq_section_title(__('Here are some more interesting collections:')); ?>
-<br>
-<?php
-  foreach ($collections as $i => $collection)
-  {
-    // Show the collection (in grid, list or hybrid view)
-    include_partial(
-      'collection/collection_grid_view',
-      array('collection' => $collection, 'i' => $i)
-    );
-
-    echo (($i + 1) % 3 == 0) ? '<br clear="all">' : null;
-  }
-  ?>
+  <div class="row spacer-top-20">
+    <div id="collectibles" class="row-content">
+      <?php
+        foreach ($collections as $i => $collection)
+        {
+          // Show the collection (in grid, list or hybrid view)
+          include_partial(
+            'collection/collection_grid_view_square_small',
+            array('collection' => $collection, 'i' => $i)
+          );
+        }
+        ?>
+    </div>
+  </div>
 <?php endif; ?>
