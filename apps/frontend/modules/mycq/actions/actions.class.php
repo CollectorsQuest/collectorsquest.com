eec49e3713f33369cd669f0917e5d44832f3a3ed
Kiril Angov <kiril.angov@gmail.com>
2012-08-09 01:55:57 +0900

    Disabling the MyCQ Home (even in env == dev)

--- apps/frontend/modules/mycq/actions/actions.class.php
+++ apps/frontend/modules/mycq/actions/actions.class.php
@@ -4,6 +4,10 @@ class mycqActions extends cqFrontendActions
 {
   public function executeIndex()
   {
+    // Redirect to Collections if the homepage is not allowed
+    $this->redirectUnless(IceGateKeeper::open('mycq_homepage'), '@mycq_collections');
+
+    // Set the "home" as the active mycq menu item
     SmartMenu::setSelected('mycq_menu', 'home');
 
     $this->collector = $this->getUser()->getCollector();
