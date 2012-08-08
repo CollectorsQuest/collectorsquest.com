2f1a5aae852c13e22af505d98f928699c8ebf519
denis.galas <denis@ddi-dev.com>
2012-08-08 12:28:13 +0300

    changed label style for copy message on compose private message

--- apps/frontend/modules/messages/templates/composeSuccess.php
+++ apps/frontend/modules/messages/templates/composeSuccess.php
@@ -9,6 +9,7 @@
   SmartMenu::setSelected('mycq_messages_sidebar', 'compose');
 ?>
 
+<?php unset($form['copy_for_sender']); ?>
 <?= form_tag('@messages_compose', array('class' => 'form-horizontal form-private-message-compose')) ?>
   <fieldset>
     <?= $form->renderUsing('Bootstrap', array(
@@ -16,6 +17,17 @@
         'subject' => array('class' => 'span7'),
         'body'    => array('class' => 'span7', 'rows' => 6),
     )); ?>
+    
+    <div class="control-group ">
+      <label for="message_copy_for_sender" class=" control-label">&nbsp;</label>
+      <div class="controls">
+        <label for="message_copy_for_sender">
+        	<input type="checkbox" name="<?= $form->getName() ?>[copy_for_sender]"
+                 id="<?= $form->getName() ?>_copy_for_sender">
+         	Email me a copy of this message to my email address
+        </label>
+      </div>
+    </div>
     <div class="form-actions">
       <input type="submit" class="btn btn-primary" value="Send" />
     </div>
