fa0677dfe63512aed7693f37539b71285111016a
IvanTanev <vankata.t@gmail.com>
2012-08-16 19:26:34 +0300

    Better JS documentation for bsWidgetFormInputTypeAhead.class.php

--- lib/widget/bsWidgetFormInputTypeAhead.class.php
+++ lib/widget/bsWidgetFormInputTypeAhead.class.php
@@ -51,38 +51,41 @@ $(document).ready(function()
     ev.stopPropagation();
     ev.preventDefault();
 
-    //filter out up/down, tab, enter, and escape keys
+    // filter out up/down, tab, enter, and escape keys
     if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 )
     {
-
-      //active used so we aren't triggering duplicate keyup events
+      // active used so we aren't triggering duplicate keyup events
       if( !self.data('active') && self.val().length > 0)
       {
+        // the self executing getJsonLoop function will call itself again
+        // if at the end of retrieving the search suggestions the value of the
+        // search field differs from the one with which the first search was made
         (function getJsonLoop(){
+          // the data active attribute is used to store the search term for the
+          // currently active request
           self.data('active', self.val());
 
-          //Do data request. Insert your own API logic here.
+          // Do data request. Insert your own API logic here.
           $.getJSON("%s", { q: self.val() }, function(data)
           {
-            //set this to true when your callback executes
-            //self.data('active',true);
-
-            //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
+            // populate array for new typeahead source
             var arr = [];
             $.each(data, function(i, value) {
               arr.push(value);
             });
 
-            //set your results into the typehead's source
+            // set your results into the typehead's source
             self.data('typeahead').source = arr;
 
-            //trigger keyup on the typeahead to make it search
+            // trigger keyup on the typeahead to make execute its internal search
             self.trigger('keyup');
 
+            // if the current search field value differs from our search
             if (self.data('active') != self.val()) {
+              // execute the search loop again
               getJsonLoop();
             } else {
-              //All done, set to false to prepare for the next remote query.
+              // all done, set current search false as cleanup
               self.data('active', false);
             }
           });
@@ -93,7 +96,9 @@ $(document).ready(function()
     // if enter was pressed
     if (13 === ev.keyCode) {
       var Typeahead = self.data('typeahead');
+      // and the typeahead is currently hidden, or there is no selection made
       if ( !Typeahead.shown || 0 === Typeahead.\$menu.find('.active').length) {
+        // submit the search form
         self.parents('form').submit();
       }
     }
