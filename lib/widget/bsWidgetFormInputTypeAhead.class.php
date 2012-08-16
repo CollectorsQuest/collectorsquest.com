2658847c5d39a24ea1d9224008bbb96d7472951f
IvanTanev <vankata.t@gmail.com>
2012-08-16 19:02:22 +0300

    Vastly improve search usability
     - Pressing enter when no autocomplete selection is made subits the form
     - JSON suggestions will now correctly handle fast writing:
     Previously one could start writing the word "Green", but only "Gr" would
     be sent to the server. If the typer was faster than the server resonse for
     the search of "Gr" then they would be left with suggestions for "Gr" only
     instead of the whole word, "Green"
    
    refs: https://basecamp.com/1759305/projects/19290-collectorsquest-com/todos/10504708-search-is-acting#comment_19376822

--- lib/widget/bsWidgetFormInputTypeAhead.class.php
+++ lib/widget/bsWidgetFormInputTypeAhead.class.php
@@ -47,43 +47,54 @@ $(document).ready(function()
 {
   $('#%s').typeahead(%s).on('keyup', function(ev)
   {
+    var self = $(this);
     ev.stopPropagation();
     ev.preventDefault();
 
     //filter out up/down, tab, enter, and escape keys
     if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 )
     {
-      var self = $(this);
-
-      //set typeahead source to empty
-      self.data('typeahead').source = [];
 
       //active used so we aren't triggering duplicate keyup events
       if( !self.data('active') && self.val().length > 0)
       {
-        self.data('active', true);
+        (function getJsonLoop(){
+          self.data('active', self.val());
 
-        //Do data request. Insert your own API logic here.
-        $.getJSON("%s", { q: $(this).val() }, function(data)
-        {
-          //set this to true when your callback executes
-          self.data('active',true);
+          //Do data request. Insert your own API logic here.
+          $.getJSON("%s", { q: self.val() }, function(data)
+          {
+            //set this to true when your callback executes
+            //self.data('active',true);
 
-          //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
-          var arr = [];
-          $.each(data, function(i, value) {
-            arr.push(value);
+            //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
+            var arr = [];
+            $.each(data, function(i, value) {
+              arr.push(value);
+            });
+
+            //set your results into the typehead's source
+            self.data('typeahead').source = arr;
+
+            //trigger keyup on the typeahead to make it search
+            self.trigger('keyup');
+
+            if (self.data('active') != self.val()) {
+              getJsonLoop();
+            } else {
+              //All done, set to false to prepare for the next remote query.
+              self.data('active', false);
+            }
           });
+        })();
+      }
+    }
 
-          //set your results into the typehead's source
-          self.data('typeahead').source = arr;
-
-          //trigger keyup on the typeahead to make it search
-          self.trigger('keyup');
-
-          //All done, set to false to prepare for the next remote query.
-          self.data('active', false);
-        });
+    // if enter was pressed
+    if (13 === ev.keyCode) {
+      var Typeahead = self.data('typeahead');
+      if ( !Typeahead.shown || 0 === Typeahead.\$menu.find('.active').length) {
+        self.parents('form').submit();
       }
     }
   });
