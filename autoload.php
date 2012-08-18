cc95e797b2946b94dae16c85963888dc1b81ab26
Kiril Angov <kiril.angov@gmail.com>
2012-08-19 01:05:43 +0900

    Updating Composer autoload.php and composer.lock files

--- autoload.php
+++ autoload.php
@@ -5,21 +5,29 @@ if (!class_exists('Composer\\Autoload\\ClassLoader', false)) {
     require __DIR__ . '/composer' . '/ClassLoader.php';
 }
 
-return call_user_func(function() {
-    $loader = new \Composer\Autoload\ClassLoader();
-    $composerDir = __DIR__ . '/composer';
+if (!class_exists('ComposerAutoloaderInit', false)) {
+    class ComposerAutoloaderInit
+    {
+        public static function getLoader()
+        {
+            $loader = new \Composer\Autoload\ClassLoader();
+            $composerDir = __DIR__ . '/composer';
 
-    $map = require $composerDir . '/autoload_namespaces.php';
-    foreach ($map as $namespace => $path) {
-        $loader->add($namespace, $path);
+            $map = require $composerDir . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->add($namespace, $path);
+            }
+
+            $classMap = require $composerDir . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+
+            $loader->register();
+
+            return $loader;
+        }
     }
+}
 
-    $classMap = require $composerDir . '/autoload_classmap.php';
-    if ($classMap) {
-        $loader->addClassMap($classMap);
-    }
-
-    $loader->register();
-
-    return $loader;
-});
+return ComposerAutoloaderInit::getLoader();
