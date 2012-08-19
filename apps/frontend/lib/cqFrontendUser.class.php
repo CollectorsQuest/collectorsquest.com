CollectorsQuest/collectorsquest.com

commit 345c5557b1efbec0050fe9c15794efbc404247bc

[Major Change] Creating class cqContext and making all of our code use that instead of the default sfContext. This is done mainly for IDE autocomplete and for future extensibility

apps/frontend/lib/cqFrontendUser.class.php
@@ -24,10 +24,10 @@ class cqFrontendUser extends cqBaseUser
   {
     if (!($this->collector instanceof Collector))
     {
-      if ($this->collector === null && ($this->getAttribute("id", null, "collector") !== null))
+      if ($this->collector === null && ($this->getAttribute('id', null, 'collector') !== null))
       {
         $this->collector = CollectorPeer::retrieveByPK(
-          $this->getAttribute("id", null, "collector")
+          $this->getAttribute('id', null, 'collector')
         );
 
         if (!$this->collector)
@@ -43,9 +43,9 @@ class cqFrontendUser extends cqBaseUser
         $this->collector = new Collector();
       }
     }
-    else if ($this->collector->getId() == null && $this->getAttribute("id", null, "collector") !== null)
+    else if ($this->collector->getId() == null && $this->getAttribute('id', null, 'collector') !== null)
     {
-      $this->collector = CollectorPeer::retrieveByPK($this->getAttribute("id", null, "collector"));
+      $this->collector = CollectorPeer::retrieveByPK($this->getAttribute('id', null, 'collector'));
     }
 
     return $this->collector;
@@ -69,7 +69,10 @@ class cqFrontendUser extends cqBaseUser
       return $country_code;
     }
 
-    return cqStatic::getGeoIpCountryCode(sfContext::getInstance()->getRequest()->getRemoteAddress(), true) ?: $default;
+    // Get the IP address of the request
+    $ip_address = cqContext::getInstance()->getRequest()->getRemoteAddress();
+
+    return cqStatic::getGeoIpCountryCode($ip_address, true) ?: $default;
   }
 
   /**
@@ -99,9 +102,13 @@ class cqFrontendUser extends cqBaseUser
    */
   public function getSeller($strict = false)
   {
-    if ($collector = $this->getCollector($strict))
+    if (($collector = $this->getCollector($strict)) && $collector->getIsSeller())
     {
-      return $collector->getSeller();
+      $seller = new Seller();
+      $collector->copyInto($seller, $deep = false, $make_new = false);
+      $seller->setId($this->getId());
+
+      return $seller;
     }
 
     return null;
@@ -111,9 +118,12 @@ class cqFrontendUser extends cqBaseUser
   {
     $q = ShoppingCartQuery::create();
 
-    if ($this->isAuthenticated()) {
+    if ($this->isAuthenticated())
+    {
       $q->filterByCollector($this->getCollector());
-    } else {
+    }
+    else
+    {
       $q->filterByCollectorId(null, Criteria::EQUAL);
       $q->filterByCookieUuid($this->getCookieUuid());
     }
@@ -157,7 +167,7 @@ class cqFrontendUser extends cqBaseUser
   public function setUsernameCookie()
   {
     /** @var $response sfWebResponse */
-    $response = sfContext::getInstance()->getResponse();
+    $response = cqContext::getInstance()->getResponse();
 
     if ($this->isAuthenticated())
     {
@@ -183,7 +193,7 @@ class cqFrontendUser extends cqBaseUser
    */
   public function setMycqDropboxOpenState($is_open)
   {
-    sfContext::getInstance()->getResponse()->setCookie(
+    cqContext::getInstance()->getResponse()->setCookie(
       self::DROPBOX_OPEN_STATE_COOKIE_NAME,
       // boolean value
       !!$is_open,
@@ -214,7 +224,7 @@ class cqFrontendUser extends cqBaseUser
     }
     else
     {
-      return (boolean) sfContext::getInstance()->getRequest()->getCookie(
+      return (boolean) cqContext::getInstance()->getRequest()->getCookie(
         self::DROPBOX_OPEN_STATE_COOKIE_NAME, true
       );
     }
@@ -225,7 +235,7 @@ class cqFrontendUser extends cqBaseUser
    */
   public function clearMycqDropboxOpenStateCookie()
   {
-    sfContext::getInstance()->getResponse()->setCookie(
+    cqContext::getInstance()->getResponse()->setCookie(
       self::DROPBOX_OPEN_STATE_COOKIE_NAME,
       '',
       // 10 years
@@ -241,7 +251,7 @@ class cqFrontendUser extends cqBaseUser
   public function getUsernameFromCookie()
   {
     $username_cookie = sfConfig::get('app_collector_username_cookie_name', 'cq_username');
-    return sfContext::getInstance()->getRequest()->getCookie($username_cookie);
+    return cqContext::getInstance()->getRequest()->getCookie($username_cookie);
   }
 
   /**
@@ -252,7 +262,7 @@ class cqFrontendUser extends cqBaseUser
     // remove the username cookie
     $expiration_time = sfConfig::get('app_collector_username_cookie_expiration_age', 15 * 24 * 3600);
     $username_cookie = sfConfig::get('app_collector_username_cookie_name', 'cq_username');
-    sfContext::getInstance()->getResponse()->setCookie($username_cookie, '', time() - $expiration_time);
+    cqContext::getInstance()->getResponse()->setCookie($username_cookie, '', time() - $expiration_time);
   }
 
   /**
@@ -323,7 +333,8 @@ class cqFrontendUser extends cqBaseUser
     $collector = $collector ?: $this->getCollector();
 
     // We cannot do anything without a Collector
-    if (!($collector instanceof Collector) || $collector->isNew()) {
+    if (!($collector instanceof Collector) || $collector->isNew())
+    {
       return false;
     }
 
@@ -369,7 +380,7 @@ class cqFrontendUser extends cqBaseUser
       // Only send the email if the email is not verified
       if ($collector_email)
       {
-        $cqEmail = new cqEmail(sfContext::getInstance()->getMailer());
+        $cqEmail = new cqEmail(cqContext::getInstance()->getMailer());
         $cqEmail->send($collector->getUserType() . '/welcome_verify_email', array(
           'to' => $collector->getEmail(),
           'params' => array(
@@ -505,7 +516,8 @@ class cqFrontendUser extends cqBaseUser
         if (
           $prop_name === CollectorPeer::PROPERTY_VISITOR_INFO_FIRST_VISIT_AT &&
           $collector->getProperty($prop_name)
-        ) {
+        )
+        {
           continue;
         }
 
@@ -586,7 +598,7 @@ class cqFrontendUser extends cqBaseUser
   protected function setVisitorInfoCookieData($data)
   {
     /** @var $response sfWebResponse */
-    $response = sfContext::getInstance()->getResponse();
+    $response = cqContext::getInstance()->getResponse();
     $response->setCookie(
       self::VISITOR_INFO_COOKIE_NAME,
       base64_encode(json_encode($data)),
@@ -608,7 +620,7 @@ class cqFrontendUser extends cqBaseUser
     );
 
     /** @var $request sfWebRequest */
-    $request = sfContext::getInstance()->getRequest();
+    $request = cqContext::getInstance()->getRequest();
     $raw_data = $request->getCookie(self::VISITOR_INFO_COOKIE_NAME, null);
 
     if (!$raw_data)

lib/model/Collector.php
@@ -496,7 +496,7 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
       $limit = $limit - $found;
 
       /** @var $sf_user cqBaseUser */
-      $sf_user = sfContext::getInstance()->getUser();
+      $sf_user = cqContext::getInstance()->getUser();
 
       if ($sf_user->isAuthenticated())
       {
@@ -923,20 +923,6 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
   public function isFacebookOnly()
   {
     return ($this->hasFacebook() && preg_match('/^fb(\d+)$/', $this->getUsername()));
-  }
-
-  public function getSeller()
-  {
-    $seller = null;
-
-    if ($this->getIsSeller())
-    {
-      $seller = new Seller();
-      $this->copyInto($seller, $deep = true, $make_new = false);
-      $seller->setId($this->getId());
-    }
-
-    return $seller;
   }
 
   /**

apps/backend/lib/widget/BackendWidgetFormModelTypeAhead.class.php
@@ -10,7 +10,7 @@ class BackendWidgetFormModelTypeAhead extends bsWidgetFormInputTypeAhead
     $model = lcfirst(sfInflector::camelize(strtolower($model)));
     $field = lcfirst(sfInflector::camelize(strtolower($field)));
 
-    $options['source'] = sfContext::getInstance()->getController()->genUrl(
+    $options['source'] = cqContext::getInstance()->getController()->genUrl(
       '@ajax_typeahead?section='. $model .'&page='. $field
     );
 

apps/backend/modules/collectionCategories/lib/filter/BackendCollectionCategoryFormFilter.class.php
@@ -10,6 +10,7 @@
  */
 class BackendCollectionCategoryFormFilter extends BaseCollectionCategoryFormFilter
 {
+
   public function configure()
   {
     $this->setupParentIdField();
@@ -18,11 +19,16 @@ class BackendCollectionCategoryFormFilter extends BaseCollectionCategoryFormFilt
   public function setupParentIdField()
   {
     $this->widgetSchema['parent_id'] = new bsWidgetFormInputTypeAhead(array(
-      'source' => $this->getOption('url_parent', sfContext::getInstance()->getController()->genUrl('collectionCategories/parent')),
+      'source' => $this->getOption(
+        'url_parent', cqContext::getInstance()->getController()->genUrl('collectionCategories/parent')
+      ),
     ));
 
-    $this->validatorSchema['parent_id'] = new sfValidatorPropelChoice(array('required' => false, 'model' => 'CollectionCategory', 'column' => 'id'));
+    $this->validatorSchema['parent_id'] = new sfValidatorPropelChoice(array(
+        'required' => false, 'model' => 'CollectionCategory', 'column' => 'id'
+    ));
   }
+
   /**
    * @param CollectionCategoryQuery $criteria
    * @param string $field
@@ -47,4 +53,5 @@ class BackendCollectionCategoryFormFilter extends BaseCollectionCategoryFormFilt
 
     return $criteria;
   }
+
 }

apps/backend/modules/collections/lib/filter/BackendCollectorCollectionFormFilter.class.php
@@ -13,7 +13,10 @@ class BackendCollectorCollectionFormFilter extends BaseCollectorCollectionFormFi
   public function configure()
   {
     $this->widgetSchema['collection_category_id'] = new bsWidgetFormInputTypeAhead(array(
-      'source'    => $this->getOption('collection_category_id_url', sfContext::getInstance()->getController()->genUrl('collections/collectionCategory')),
+      'source' => $this->getOption(
+        'collection_category_id_url',
+        cqContext::getInstance()->getController()->genUrl('collections/collectionCategory')
+      ),
     ));
     $this->setupIdField();
   }
@@ -71,4 +74,5 @@ class BackendCollectorCollectionFormFilter extends BaseCollectorCollectionFormFi
 
     return $criteria;
   }
+
 }

apps/backend/modules/comments/templates/_model_object.php
@@ -10,5 +10,5 @@ unset($routing_params['sf_route']);
 
 echo link_to(
   sprintf('%s (%s)', $model_object, get_class($model_object)),
-  sfContext::getInstance()->getConfiguration()->generateFrontendUrl($route_name, $routing_params)
+  cqContext::getInstance()->getConfiguration()->generateFrontendUrl($route_name, $routing_params)
 );

apps/frontend/config/app.yml
@@ -68,8 +68,8 @@ all:
         collectibles_for_sale:   { name: "Items for Sale / Sold", uri: "@mycq_marketplace" }
         # collectibles_sold:       { name: "Sold Items", uri: "@mycq_marketplace_sold" }
         collectibles_purchased:  { name: "Purchases", uri: "@mycq_marketplace_purchased", check_lock: shopping_cart }
-        settings:                { name: "Store Settings",  uri: "@mycq_marketplace_settings", check_eval: "sfContext::getInstance()->getUser()->getCollector()->hasBoughtCredits()" }
-        packages:                { name: "Credit History",  uri: "@mycq_marketplace_credit_history", check_eval: "sfContext::getInstance()->getUser()->getCollector()->hasBoughtCredits()" }
+        settings:                { name: "Store Settings",  uri: "@mycq_marketplace_settings", check_eval: "cqContext::getInstance()->getUser()->getCollector()->hasBoughtCredits()" }
+        packages:                { name: "Credit History",  uri: "@mycq_marketplace_credit_history", check_eval: "cqContext::getInstance()->getUser()->getCollector()->hasBoughtCredits()" }
 
     mycq_messages_sidebar:
       items:

apps/frontend/lib/filter/cqVisitorInfoFilter.class.php.php
@@ -148,7 +148,7 @@ class cqVisitorInfoFilter extends sfFilter
 
       $response->addDelayedFunction(function()
       {
-        if (( $collector = sfContext::getInstance()->getUser()->getCollector($strict = true) ))
+        if ($collector = cqContext::getInstance()->getUser()->getCollector($strict = true))
         {
           $collector->save();
         }

commit b3b95d37ff18d6c9e2da997a88702fcb190ed8b1

Implement Seller class as a wrapper for Collector
Fixes regressions introduced by 345c5557b1efbec0050fe9c15794efbc404247bc

apps/frontend/lib/cqFrontendUser.class.php
@@ -97,18 +97,14 @@ class cqFrontendUser extends cqBaseUser
   }
 
   /**
-   * @param  boolean  $strict
-   * @return null|Seller
+   * @param     boolean  $strict
+   * @return    null|Seller
    */
   public function getSeller($strict = false)
   {
-    if (($collector = $this->getCollector($strict)) && $collector->getIsSeller())
+    if (($collector = $this->getCollector($strict)))
     {
-      $seller = new Seller();
-      $collector->copyInto($seller, $deep = false, $make_new = false);
-      $seller->setId($this->getId());
-
-      return $seller;
+      return $collector->getSeller();
     }
 
     return null;

lib/model/Collector.php
@@ -78,6 +78,9 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
 
   protected $collCollectiblesInCollections;
 
+  /** @var Collector */
+  protected $seller;
+
   public function initializeProperties()
   {
     $this->registerProperty('SINGUP_NUM_COMPLETED_STEPS', 1);
@@ -945,9 +948,34 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
       ->count();
   }
 
+  /**
+   * @return    boolean
+   */
   public function getIsSeller()
   {
-    return $this->getUserType() == 'Seller';
+    return CollectorPeer::TYPE_SELLER == $this->getUserType();
+  }
+
+  /**
+   * Get a Seller wrapped Collector
+   *
+   * @return    Seller|null
+   */
+  public function getSeller()
+  {
+    if ($this->getIsSeller())
+    {
+      if (null === $this->seller)
+      {
+        $this->seller = new Seller($this);
+      }
+
+      return $this->seller;
+    }
+    else
+    {
+      return null;
+    }
   }
 
   public function fromArray($array, $keyType = BasePeer::TYPE_PHPNAME)

lib/model/Seller.php
@@ -1,24 +1,138 @@
 <?php
 
-class Seller extends Collector
+/**
+ * A wrapper object around Collector
+ *
+ * @method     int getSingupNumCompletedSteps() Return the number of completed signup steps
+ * @method     Collector setSingupNumCompletedSteps(int $v) Set the number of completed signup steps
+ * @method     Collector setCqnextAccessAllowed(boolean $v)
+ *
+ *
+ * @method     Collector setSellerSettingsPaypalAccountId(string $v)
+ * @method     string    getSellerSettingsPaypalAccountId()
+ *
+ * @method     Collector setSellerSettingsPaypalAccountStatus(string $v)
+ * @method     string    getSellerSettingsPaypalAccountStatus()
+ *
+ * @method     Collector setSellerSettingsPaypalBusinessName(string $v)
+ * @method     string    getSellerSettingsPaypalBusinessName()
+ *
+ * @method     Collector setSellerSettingsPaypalEmail(string $v)
+ * @method     string    getSellerSettingsPaypalEmail()
+ *
+ * @method     Collector setSellerSettingsPaypalFirstName(string $v)
+ * @method     string    getSellerSettingsPaypalFirstName()
+ *
+ * @method     Collector setSellerSettingsPaypalLastName(string $v)
+ * @method     string    getSellerSettingsPaypalLastName()
+ *
+ *
+ * @method     Collector setSellerSettingsPhoneCode(string $v)
+ * @method     string    getSellerSettingsPhoneCode()
+ *
+ * @method     Collector setSellerSettingsPhoneNumber(string $v)
+ * @method     string    getSellerSettingsPhoneNumber()
+ *
+ * @method     Collector setSellerSettingsPhoneExtension(string $v)
+ * @method     string    getSellerSettingsPhoneExtension()
+ *
+ * @method     Collector setSellerSettingsStoreName(string $v)
+ * @method     string    getSellerSettingsStoreName()
+ *
+ * @method     Collector setSellerSettingsStoreTitle(string $v)
+ * @method     string    getSellerSettingsStoreTitle()
+ *
+ * @method     Collector setSellerSettingsStoreDescription(string $v)
+ * @method     string    getSellerSettingsStoreDescription()
+ *
+ * @method     Collector setSellerSettingsReturnPolicy(string $v)
+ * @method     string    getSellerSettingsReturnPolicy()
+ *
+ * @method     Collector setSellerSettingsPaymentAccepted(string $v)
+ * @method     string    getSellerSettingsPaymentAccepted()
+ *
+ * @method     Collector setSellerSettingsWelcome(string $v)
+ * @method     string    getSellerSettingsWelcome()
+ *
+ * @method     Collector setSellerSettingsShipping(string $v)
+ * @method     string    getSellerSettingsShipping()
+ *
+ * @method     Collector setSellerSettingsRefunds(string $v)
+ * @method     string    getSellerSettingsRefunds()
+ *
+ * @method     Collector setSellerSettingsAdditionalPolicies(string $v)
+ * @method     string    getSellerSettingsAdditionalPolicies()
+ *
+ *
+ * @method     Collector setVisitorInfoNumVisits(int $v)
+ * @method     int       getVisitorInfoNumVisits()
+ *
+ * @method     Collector setVisitorInfoNumPageViews(int $v)
+ * @method     int       getVisitorInfoNumPageViews()
+ */
+class Seller
 {
 
-  public function getBusinessName()
+  /** @var  Collector */
+  protected $collector;
+
+  public function __construct(Collector $collector)
   {
-    return $this->getSellerSettingsPaypalBusinessName();
+    $this->collector = $collector;
   }
 
+  /**
+   * Forward all undefined method calls to the wrapped Collector object
+   *
+   * @param     string $name
+   * @param     array $arguments
+   *
+   * @return    mixed
+   */
+  public function __call($name, $arguments)
+  {
+    return call_user_func_array(array($this->collector, $name), $arguments);
+  }
+
+  /**
+   * Get the wrapped Collector object
+   *
+   * @return    Collector
+   */
+  public function getCollector()
+  {
+    return $this->collector;
+  }
+
+  /**
+   * The PayPal business name
+   *
+   * @return string
+   */
+  public function getBusinessName()
+  {
+    return $this->collector->getSellerSettingsPaypalBusinessName();
+  }
+
+  /**
+   * The collector's full name, as registered with PayPal
+   *
+   * @return    string
+   */
   public function getFullName()
   {
     return implode(' ', array(
-      $this->getSellerSettingsPaypalFirstName(),
-      $this->getSellerSettingsPaypalLastName()
+      $this->collector->getSellerSettingsPaypalFirstName(),
+      $this->collector->getSellerSettingsPaypalLastName()
     ));
   }
 
+  /**
+   * @return    string
+   */
   public function getPayPalEmail()
   {
-    return $this->getSellerSettingsPaypalEmail();
+    return $this->collector->getSellerSettingsPaypalEmail();
   }
 
   /**
@@ -41,7 +155,7 @@ class Seller extends Collector
   public function getPackageCreditsSum()
   {
     return (integer) PackageTransactionQuery::create()
-      ->filterByCollector($this)
+      ->filterByCollector($this->collector)
       ->paidFor()
       ->notExpired()
       ->withColumn('SUM(PackageTransaction.Credits)', 'CreditsTotal')
@@ -57,7 +171,7 @@ class Seller extends Collector
   public function getCreditsLeft()
   {
     return (int) PackageTransactionQuery::create()
-      ->filterByCollector($this)
+      ->filterByCollector($this->collector)
       ->paidFor()
       ->notExpired()
       ->withColumn('SUM(PackageTransaction.Credits) - SUM(PackageTransaction.CreditsUsed)', 'CreditsLeft')