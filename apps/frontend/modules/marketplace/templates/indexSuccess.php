5f38ed0031f195dabe98058df28ca77651f1c476
IvanTanev <vankata.t@gmail.com>
2012-08-20 22:57:22 +0300

    Link marketplace banner to seller_signup, and from there to collector_signup

--- apps/frontend/modules/marketplace/templates/indexSuccess.php
+++ apps/frontend/modules/marketplace/templates/indexSuccess.php
@@ -45,7 +45,7 @@
   <?php
   if (!$sf_user->isAuthenticated())
   {
-    echo link_to(image_tag('banners/show_and_sell_red_635x111.png'), '@collector_signup');
+    echo link_to(image_tag('banners/show_and_sell_red_635x111.png'), '@seller_signup');
   }
   elseif ($sf_user->getSeller() && !$sf_user->getSeller()->hasPackageCredits())
   {
