a96d5ade3c6ba51ad6aa336072438d212f76470f
Kiril Angov <kiril.angov@gmail.com>
2012-08-21 23:13:41 +0900

    Last minute changes

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -10,13 +10,6 @@
   $sf_cache_key .= $sf_user->isAuthenticated() ? 'authenticated' : 'not_authenticated';
 ?>
 <!doctype html>
-<<<<<<< HEAD
-<!--[if lt IE 7 ]>    <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://opengraph.org/schema/" lang="en" class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if IE 7 ]>       <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://opengraph.org/schema/" lang="en" class="no-js lt-ie10 lt-ie9 lt-ie8"><![endif]-->
-<!--[if IE 8 ]>       <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://opengraph.org/schema/" lang="en" class="no-js lt-ie10 lt-ie9"><![endif]-->
-<!--[if IE 9]>        <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://opengraph.org/schema/" lang="en" class="no-js lt-ie10"><![endif]-->
-<!--[if gt IE 9]><!--><html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://opengraph.org/schema/" lang="en" class="no-js"><!--<![endif]-->
-=======
 <!--[if IE 8 ]>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://opengraph.org/schema/"
       lang="en" class="no-js lt-ie10 lt-ie9">
@@ -29,7 +22,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://opengraph.org/schema/"
       lang="en" class="no-js">
 <!--<![endif]-->
->>>>>>> develop
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# collectorsquest: http://ogp.me/ns/fb/collectorsquest#">
   <?php include_partial('global/head'); ?>
 
