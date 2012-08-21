4e964c29872c518862a2c054b2518d6fb887b9a8
TeodorMarinov <teo.marinov@gmail.com>
2012-08-21 22:56:50 +0300

    Work on to-do Improve the layout of the purchased packages tab
    https://basecamp.com/1759305/projects/878205-collectorsquest-com/todos/12688595-improve-the-layout

--- apps/frontend/modules/mycq/templates/marketplaceCreditHistorySuccess.php
+++ apps/frontend/modules/mycq/templates/marketplaceCreditHistorySuccess.php
@@ -15,14 +15,69 @@
     <div class="tab-pane active">
       <div class="tab-content-inner spacer">
 
+        <table class="table table-credit-history">
+          <thead>
+          <tr>
+            <th>Package</th>
+            <th>Credits Purchased</th>
+            <th>Purchased On</th>
+            <th>Expires On</th>
+            <th>Status</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="not-paid">
+            <td>unlimited</td>
+            <td>unlimited</td>
+            <td>August 18, 2012</td>
+            <td>
+              -
+            </td>
+            <td>
+              <span class="red">
+                processing<br>payment
+              </span>
+            </td>
+          </tr><tr>
+            <td>100 credits</td>
+            <td>100</td>
+            <td>August 18, 2012</td>
+            <td>August 18, 2013</td>
+            <td>paid</td>
+          </tr>
+          <tr class="alert">
+            <td>100 credits</td>
+            <td>1</td>
+            <td>August 18, 2011</td>
+            <td><strong>August 18, 2012</strong></td>
+            <td>
+              expiring<br>soon
+            </td>
+          </tr>
+          <tr class="expired">
+            <td>100 credits</td>
+            <td>0</td>
+            <td>2012-06-17 15:57:11</td>
+            <td>2012-06-19 12:57:11</td>
+            <td>expired</td>
+          </tr>
+          </tbody>
+        </table>
+        <div class="cf spacer-bottom-20">
+          <button type="submit" class="btn btn-primary pull-right" value="Buy Credits">
+            <i class="icon-plus"></i>
+            <span>Buy Credits</span>
+          </button>
+        </div>
+
         <table class="table table-bordered">
           <thead>
             <tr>
               <th>Package</th>
               <th>Credits Purchased</th>
               <th>Credits Used</th>
-              <th>Purchased At</th>
-              <th>Expires At</th>
+              <th>Purchased On</th>
+              <th>Expires On</th>
               <?php if ('dev' == sfConfig::get('sf_environment')): ?>
               <th>Payment Status</th>
               <?php endif; ?>
