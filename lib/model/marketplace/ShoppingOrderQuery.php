8427a0b9090fc0510ce8ab8b7fe541a0fb25296b
Kiril Angov <kiril.angov@gmail.com>
2012-08-21 23:27:05 +0900

    Fixing how items purchased are being displayed (we want to show only the paid orders, not everything)

--- lib/model/marketplace/ShoppingOrderQuery.php
+++ lib/model/marketplace/ShoppingOrderQuery.php
@@ -4,8 +4,26 @@ require 'lib/model/marketplace/om/BaseShoppingOrderQuery.php';
 
 class ShoppingOrderQuery extends BaseShoppingOrderQuery
 {
+
+  /**
+   * @return ShoppingOrderQuery
+   */
   public function search($q)
   {
     return $this;
   }
+
+  /**
+   * @return ShoppingOrderQuery
+   */
+  public function paid()
+  {
+    return $this
+      ->rightJoinShoppingPaymentRelatedByShoppingPaymentId()
+      ->useShoppingPaymentRelatedByShoppingPaymentIdQuery()
+        ->filterByStatus(ShoppingPaymentPeer::STATUS_COMPLETED)
+      ->endUse()
+      ->groupBy(ShoppingOrderPeer::ID);
+  }
+
 }
