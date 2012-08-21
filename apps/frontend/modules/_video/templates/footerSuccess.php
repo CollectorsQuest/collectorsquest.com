700800b3978be0edc02acd036b67cd932510f2e9
Kiril Angov <kiril.angov@gmail.com>
2012-08-21 23:12:08 +0900

    Footer for video module

--- apps/frontend/modules/_video/templates/footerSuccess.php
+++ apps/frontend/modules/_video/templates/footerSuccess.php
@@ -6,15 +6,20 @@
  */
 ?>
 <!doctype html>
-<!--[if lt IE 7 ]><html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if IE 7 ]><html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" lang="en" class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if IE 8 ]><html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" lang="en" class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!--><html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://ogp.me/ns/fb#" lang="en" class="no-js"><!--<![endif]-->
+<!--[if IE 8 ]>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js lt-ie10 lt-ie9">
+<![endif]-->
+<!--[if IE 9]>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js lt-ie10">
+<![endif]-->
+<!--[if gt IE 9]>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js">
+<!--<![endif]-->
 <head>
   <?php include_partial('global/head'); ?>
   <base target="_top" />
 </head>
-<body id="<?= 'body-'. $sf_params->get('module') .'-'. $sf_params->get('action'); ?>" data-controller="<?= $sf_params->get('module'); ?>" data-action="<?= $sf_params->get('action'); ?>">
+<body id="body-_video-footer" data-controller="_video" data-action="footer">
   <?php
     $sf_cache_key  = (int) $sf_user->getId() .'_';
     $sf_cache_key .= $sf_user->isAuthenticated() ? 'authenticated' : 'not_authenticated';
