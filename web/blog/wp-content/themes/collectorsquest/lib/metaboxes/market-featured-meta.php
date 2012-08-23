1f56806624d5babad375aef318499becd91e37fa
Anton Ruzhekov <anton@collectorsquest.com>
2012-08-23 14:37:41 +0300

    added free text option for ribbon text editable from backend MOCK-UP only first Collectible item working (ref: https://basecamp.com/1759305/projects/878190-collectorsquest-com/todos/12931045-in-the-edit)

--- web/blog/wp-content/themes/collectorsquest/lib/metaboxes/market-featured-meta.php
+++ web/blog/wp-content/themes/collectorsquest/lib/metaboxes/market-featured-meta.php
@@ -12,15 +12,49 @@
     <?php $mb->the_field('cq_collectible_text_1'); ?>
 
     <label>Ribbon Text:
-      <select name="<?php $mb->the_name(); ?>">
+      <select name="<?php $mb->the_name(); ?>" id="select_1">
         <option value=""></option>
-        <option value="affordable"<?php if ($mb->get_the_value() == 'affordable') echo $selected; ?>>Affordable</option>
-        <option value="rare"<?php if ($mb->get_the_value() == 'rare') echo $selected; ?>>Rare</option>
-        <option value="unique"<?php if ($mb->get_the_value() == 'unique') echo $selected; ?>>Unique</option>
-        <option value="as-seen-on"<?php if ($mb->get_the_value() == 'as-seen-on') echo $selected; ?>>As Seen On</option>
+        <option value="Affordable"<?php if ($mb->get_the_value() == 'Affordable') echo $selected; ?>>Affordable</option>
+        <option value="Rare"<?php if ($mb->get_the_value() == 'Rare') echo $selected; ?>>Rare</option>
+        <option value="Unique"<?php if ($mb->get_the_value() == 'Unique') echo $selected; ?>>Unique</option>
+        <option value="As Seen On"<?php if ($mb->get_the_value() == 'As Seen On') echo $selected; ?>>As Seen On</option>
+
+        <?php
+          $free_text_1_selected = false;
+           if (!in_array($mb->get_the_value(), array('', 'Affordable', 'Rare', 'Unique', 'As Seen On'))):
+             $free_text_1_selected = true;
+           endif;
+        ?>
+
+        <option value="Enter Free Text"<?php if ($free_text_1_selected) echo $selected; ?>>Enter Free Text</option>
       </select>
+      <input name="<?php $mb->the_name(); ?>" value="<?php echo $mb->get_the_value() ?>" id="input_1" style="display: none;" />
     </label>
   </div>
+
+  <script>
+    jQuery(document).ready(function() {
+      <?php if ($free_text_1_selected): ?>
+          jQuery('input#input_1').show();
+      <?php endif; ?>
+
+      jQuery("select#select_1").change(function () {
+        jQuery("select#select_1 option:selected").each(function (i, selected) {
+          var text = jQuery(selected).text();
+          if (text != 'Enter Free Text')
+          {
+            jQuery('input#input_1').val(text);
+            jQuery('input#input_1').hide();
+          }
+          else
+          {
+            jQuery('input#input_1').val('');
+            jQuery('input#input_1').show();
+          }
+        });
+      });
+    });
+  </script>
 
   <div style="width:31%;margin-right:2%;float:left">
     <label>Collectible for Sale (ID):
