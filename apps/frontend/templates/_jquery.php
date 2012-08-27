ede78dcea9c4b09ccc5a2f31ea860cf9f0e216f4
Kiril Angov <kiril.angov@gmail.com>
2012-08-28 00:33:28 +0900

    Use exact version for google ajax cdn to enable gzip and better caching

--- apps/frontend/templates/_jquery.php
+++ apps/frontend/templates/_jquery.php
@@ -1,7 +1,7 @@
 
   <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min.js"></script>
   <script>
     window.jQuery    || document.write('<script src="<?= cq_javascript_src('frontend/jquery.js'); ?>"><\/script>');
     window.jQuery.ui || document.write('<script src="<?= cq_javascript_src('frontend/jquery.ui.js'); ?>"><\/script>');
