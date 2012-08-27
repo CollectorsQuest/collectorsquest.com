2bc70d941ca4e1103df3c82c575601b9ca184f74
Kiril Angov <kiril.angov@gmail.com>
2012-08-28 03:24:07 +0900

    Updated the MailChimp subscribe sidebar widget (ref: https://basecamp.com/1759305/projects/19290-collectorsquest-com/todos/13796524-create-a-form-for)

--- apps/frontend/modules/_sidebar/templates/_widgetMailChimpSubscribe.php
+++ apps/frontend/modules/_sidebar/templates/_widgetMailChimpSubscribe.php
@@ -1,14 +1,14 @@
 
 <div id="mc_embed_signup">
-  <?php cq_sidebar_title('Subscribe to Our Mailing List', null); ?>
-  <form action="http://collectorsquest.us5.list-manage2.com/subscribe/post?u=0335e4e35c5166241792a7a47&id=4b51c2b29c"
+  <?php cq_sidebar_title('Subscribe To Our Newsletter', null); ?>
+  <form action="http://collectorsquest.us5.list-manage.com/subscribe/post?u=0335e4e35c5166241792a7a47&id=87adbea7fa"
         method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
         class="form-horizontal validate" target="_blank" novalidate>
 
     <div class="row-fluid spacer-7 ">
       <div class="span4">
         <label class=" control-label" for="mce-EMAIL" style=" width: 100%; text-align: right;">
-          Email address &nbsp;
+          Email Address &nbsp;
         </label>
       </div>
       <div class="span7 ">
@@ -17,7 +17,7 @@
     </div>
     <div class="row-fluid spacer-7 ">
       <div class="span4">
-        <label class=" control-label" style=" width: 100%; text-align: right">And you are &nbsp;</label>
+        <label class=" control-label" style=" width: 100%; text-align: right">I am a &nbsp;</label>
       </div>
       <div class="span7 ">
         <div style="padding-top: 3px;" class="radio_list">
@@ -36,11 +36,9 @@
       <div class="response" id="mce-error-response" style="display:none"></div>
       <div class="response" id="mce-success-response" style="display:none"></div>
     </div>
-    <div class="clear" style="text-align: right;">
-      <input type="submit" value="Subscribe to Mailing List"
+    <div class="clear" style="margin-left: 100px;">
+      <input type="submit" value="Subscribe"
              name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">
     </div>
-
-    <input type="hidden" name="MERGE16" value="Yes">
   </form>
 </div>
