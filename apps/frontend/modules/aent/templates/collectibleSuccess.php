CollectorsQuest/collectorsquest.com

commit 410d234a6fb0914a9d9ff9929198567a793f5d04

Changes to how we load jQuery.
* WordPress was loading it's own version of jQuery and most of the WordPress code depends
   on jQuery being loaded in the header so changes were needed to support that.
* Overwriting jQuery and jQuery UI scripts with empty.js to not break WordPress code but also
   not to load unneeded javascript resources.

apps/frontend/templates/_head.php
@@ -61,10 +61,16 @@
           apiKey: '<?= cqConfig::getCredentials('aviary', 'api_key'); ?>',
           postUrl: '<?= url_for('@mycq_aviary_update_image', true); ?>'
         }
-      }
+      },
+      docready: [],
+      ready: function(fn) { window.cq.docready.push(fn) },
+      _ready: null
     };
 
     // http://stackoverflow.com/a/8567229
-    var docready=[],$=function(){return{ready:function(fn){docready.push(fn)}}};
+    var $ = function() {
+      return { ready: window.cq.ready }
+    };
   </script>
   <script src="<?= cq_javascript_src('frontend/head.js'); ?>"></script>
+  <?php include_component_slot('jquery_header'); ?>

apps/frontend/templates/_javascripts.php
@@ -1,10 +1,4 @@
-<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-<script>
-  window.jQuery    || document.write('<script src="<?= cq_javascript_src('frontend/jquery.js'); ?>"><\/script>');
-  window.jQuery.ui || document.write('<script src="<?= cq_javascript_src('frontend/jquery.ui.js'); ?>"><\/script>');
-</script>
+<?php include_component_slot('jquery_footer'); ?>
 
 <?php if ($sf_params->get('gcf')): ?>
 <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
@@ -29,12 +23,16 @@
     both: '<?= javascript_path('frontend/scripts.common.bundle.' . GIT_REVISION . '.js'); ?>',
     complete: function ()
     {
+      // Restore the original jQuery ready() function
+      jQuery.fn.ready = window.cq._ready;
+
       // http://stackoverflow.com/a/8567229
       (function ($, window, document)
       {
-        for (func in window.docready) {
-          $(document).ready(window.docready[func]);
+        for (func in window.cq.docready) {
+          $(document).ready(window.cq.docready[func]);
         }
+        window.cq.docready=[];
       }(jQuery, this, this.document));
 
       // Execute the controller in scripts.common.bundle.js

apps/frontend/templates/_jquery.php
@@ -1,0 +1,14 @@
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+  <script>
+    window.jQuery    || document.write('<script src="<?= cq_javascript_src('frontend/jquery.js'); ?>"><\/script>');
+    window.jQuery.ui || document.write('<script src="<?= cq_javascript_src('frontend/jquery.ui.js'); ?>"><\/script>');
+
+    // Store a reference to the original remove method.
+    window.cq._ready = jQuery.fn.ready;
+
+    // Define overriding method. (http://stackoverflow.com/a/8567229)
+    jQuery.fn.ready = window.cq.ready;
+  </script>

apps/frontend/modules/global/actions/components.class.php
@@ -39,6 +39,11 @@ class globalComponents extends cqFrontendComponents
     return sfView::SUCCESS;
   }
 
+  public function executeJquery()
+  {
+    return sfView::SUCCESS;
+  }
+
   public function executePagination(sfWebRequest $request)
   {
     /** @var $pager sfPropelPager */

web/blog/wp-content/themes/collectorsquest/index.new.php
@@ -73,10 +73,16 @@
     $data['breadcrumbs'] = $crumbs;
   }
 
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', '/wp-content/themes/collectorsquest/js/empty.js', array(), '1.7.2', 1);
+  wp_enqueue_script('jquery');
+
   ob_start();
   wp_head();
   $head = ob_get_clean();
 
+  // Optimize the WordPress javascripts (avoid duplicates with out own scripts)
+  include __DIR__ .'/javascripts.php';
 
   ob_start();
   get_header();

web/blog/wp-content/themes/collectorsquest/javascripts.php
@@ -1,0 +1,45 @@
+<?php
+
+$scripts = array(
+  'jquery-ui-core' => '1.8.20',
+  'jquery-effects-core' => '1.8.20',
+  'jquery-effects-blind' => '1.8.20',
+  'jquery-effects-bounce' => '1.8.20',
+  'jquery-effects-clip' => '1.8.20',
+  'jquery-effects-drop' => '1.8.20',
+  'jquery-effects-explode' => '1.8.20',
+  'jquery-effects-fade' => '1.8.20',
+  'jquery-effects-fold' => '1.8.20',
+  'jquery-effects-highlight' => '1.8.20',
+  'jquery-effects-pulsate' => '1.8.20',
+  'jquery-effects-scale' => '1.8.20',
+  'jquery-effects-shake' => '1.8.20',
+  'jquery-effects-slide' => '1.8.20',
+  'jquery-effects-transfer' => '1.8.20',
+
+  'jquery-ui-accordion' => '1.8.20',
+  'jquery-ui-autocomplete' => '1.8.20',
+  'jquery-ui-datepicker' => '1.8.20',
+  'jquery-ui-dialog' => '1.8.20',
+  'jquery-ui-draggable' => '1.8.20',
+  'jquery-ui-droppable' => '1.8.20',
+  'jquery-ui-mouse' => '1.8.20',
+  'jquery-ui-position' => '1.8.20',
+  'jquery-ui-progressbar' => '1.8.20',
+  'jquery-ui-resizable' => '1.8.20',
+  'jquery-ui-selectable' => '1.8.20',
+  'jquery-ui-slider' => '1.8.20',
+  'jquery-ui-sortable' => '1.8.20',
+  'jquery-ui-tabs' => '1.8.20',
+  'jquery-ui-widget' => '1.8.20'
+);
+
+foreach ($scripts as $name => $version)
+{
+  if ($wp_scripts->query($name, 'queue'))
+  {
+    wp_deregister_script($name);
+    wp_register_script($name, '/wp-content/themes/collectorsquest/js/empty.js', array(), $version, 1);
+    wp_enqueue_script($name);
+  }
+}

web/blog/wp-content/themes/collectorsquest/js/empty.js
@@ -1,0 +1,1 @@
+// indeed empty ;)

web/js/frontend/application.js
@@ -540,7 +540,7 @@ var COMMON = window.COMMON = (function(){
       });
 
       // if the username cookie is set and has a value that is truthy
-      if ($.cookie(window.cq.cookies.username)) {
+      if (window.cq.cookies && $.cookie(window.cq.cookies.username)) {
         // triger the event to show the login form in the footer
         $('#footer-control-login-button').trigger('click');
         // and set the username to the value of the cookie

commit f0e00808ea200bb7880ff1da2de2ee289840a3d6

Fix for the way we load jQuery (broken in 410d234a6fb0914a9d9ff9929198567a793f5d04)

apps/frontend/modules/aent/templates/collectibleSuccess.php
@@ -44,7 +44,7 @@
         //-->
       </ul>
     </div>
-    <div class="pull-right share">
+    <div id="social-sharing" class="pull-right share">
       <!-- AddThis Button BEGIN -->
       <?php /*
       <a href="javascript:void(0)" class="btn-lightblue btn-mini-social">

apps/frontend/modules/collection/templates/collectibleSuccess.php
@@ -120,7 +120,7 @@
         //-->
       </ul>
     </div>
-    <div class="pull-right share">
+    <div id="social-sharing" class="pull-right share">
       <!-- AddThis Button BEGIN -->
       <a class="btn-lightblue btn-mini-social addthis_button_email">
         <i class="mail-icon-mini"></i> Email

apps/frontend/modules/collection/templates/indexSuccess.php
@@ -41,7 +41,7 @@
         </li>
       </ul>
     </div>
-    <div class="pull-right share">
+    <div id="social-sharing" class="pull-right share">
       <!-- AddThis Button BEGIN -->
       <a class="btn-lightblue btn-mini-social addthis_button_email">
         <i class="mail-icon-mini"></i> Email

apps/frontend/modules/collector/templates/_sidebarIndex.php
@@ -24,7 +24,7 @@
         </li>
       </ul>
     </div>
-    <div class="pull-right share">
+    <div id="social-sharing" class="pull-right share">
       <a class="addthis_button_tweet" tw:twitter:data-count="none"></a>
       <a class="addthis_button_google_plusone" g:plusone:size="medium" g:plusone:annotation="none"></a>
       <a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:width="40"></a>

apps/frontend/modules/misc/templates/guideDownloadSuccess.php
@@ -27,7 +27,7 @@
           </p>
           <br/>
           <!-- AddThis Button BEGIN -->
-          <div class="addthis_toolbox addthis_default_style addthis_32x32_style spacer-left">
+          <div  id="social-sharing" class="addthis_toolbox addthis_default_style addthis_32x32_style spacer-left">
             <a class="addthis_button_preferred_1"></a>
             <a class="addthis_button_preferred_2"></a>
             <a class="addthis_button_preferred_3"></a>

apps/frontend/templates/_head.php
@@ -62,15 +62,13 @@
           postUrl: '<?= url_for('@mycq_aviary_update_image', true); ?>'
         }
       },
-      docready: [],
-      ready: function(fn) { window.cq.docready.push(fn) },
       _ready: null
     };
 
     // http://stackoverflow.com/a/8567229
-    var $ = function() {
-      return { ready: window.cq.ready }
-    };
+    var _docready=[];
+    var _ready = function(fn){_docready.push(fn)};
+    var $ = function(){return{ready:_ready}};
   </script>
   <script src="<?= cq_javascript_src('frontend/head.js'); ?>"></script>
   <?php include_component_slot('jquery_header'); ?>

apps/frontend/templates/_javascripts.php
@@ -23,16 +23,15 @@
     both: '<?= javascript_path('frontend/scripts.common.bundle.' . GIT_REVISION . '.js'); ?>',
     complete: function ()
     {
-      // Restore the original jQuery ready() function
-      jQuery.fn.ready = window.cq._ready;
+      // Restore the original jQuery.fn.ready() function
+      $.ready = window.cq._ready;
 
       // http://stackoverflow.com/a/8567229
       (function ($, window, document)
       {
-        for (func in window.cq.docready) {
-          $(document).ready(window.cq.docready[func]);
+        for (func in window._docready) {
+          $(document).ready(window._docready[func]);
         }
-        window.cq.docready=[];
       }(jQuery, this, this.document));
 
       // Execute the controller in scripts.common.bundle.js
@@ -42,20 +41,23 @@
 </script>
 
 <script>
-(function(d, t)
+if (document.getElementById('social-sharing') !== null)
 {
-  var addthis = d.createElement(t);
-  var s = d.getElementsByTagName(t)[0];
+  (function(d, t)
+  {
+    var addthis = d.createElement(t);
+    var s = d.getElementsByTagName(t)[0];
 
-  addthis.async = true;
-  addthis.src = '//s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fa2c6240b775d05&async=true';
-  s.parentNode.insertBefore(addthis, s);
+    addthis.async = true;
+    addthis.src = '//s7.addthis.com/js/250/addthis_widget.js#pubid=ra-4fa2c6240b775d05&async=true';
+    s.parentNode.insertBefore(addthis, s);
 
-})(document, 'script');
+  })(document, 'script');
 
-$(window).load(function() {
-  addthis.init()
-})
+  $(window).load(function() {
+    addthis.init()
+  });
+}
 </script>
 
 <?php if ($sf_request->isSecure()): ?>

apps/frontend/templates/_jquery.php
@@ -7,8 +7,8 @@
     window.jQuery.ui || document.write('<script src="<?= cq_javascript_src('frontend/jquery.ui.js'); ?>"><\/script>');
 
     // Store a reference to the original remove method.
-    window.cq._ready = jQuery.fn.ready;
+    window.cq._ready = $.ready;
 
     // Define overriding method. (http://stackoverflow.com/a/8567229)
-    jQuery.fn.ready = window.cq.ready;
+    $.ready = window._ready;
   </script>

web/blog/wp-content/themes/collectorsquest/js/load-posts.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function ($)
+$(document).ready(function()
 {
   // The number of the next page to load (/page/x/).
   var pageNum = parseInt(cq_i18n.startPage) + 1;