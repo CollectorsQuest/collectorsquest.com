CollectorsQuest/collectorsquest.com

commit 410d234a6fb0914a9d9ff9929198567a793f5d04

Changes to how we load jQuery.
* WordPress was loading it's own version of jQuery and most of the WordPress code depends
   on jQuery being loaded in the header so changes were needed to support that.
* Overwriting jQuery and jQuery UI scripts with empty.js to not break WordPress code but also
   not to load unneeded javascript resources.

apps/frontend/templates/_head.php
@@ -61,10 +61,16 @@
           apiKey: '<?= cqConfig::getCredentials('aviary', 'api_key'); ?>',
           postUrl: '<?= url_for('@mycq_aviary_update_image', true); ?>'
         }
-      }
+      },
+      docready: [],
+      ready: function(fn) { window.cq.docready.push(fn) },
+      _ready: null
     };
 
     // http://stackoverflow.com/a/8567229
-    var docready=[],$=function(){return{ready:function(fn){docready.push(fn)}}};
+    var $ = function() {
+      return { ready: window.cq.ready }
+    };
   </script>
   <script src="<?= cq_javascript_src('frontend/head.js'); ?>"></script>
+  <?php include_component_slot('jquery_header'); ?>

apps/frontend/templates/_javascripts.php
@@ -1,10 +1,4 @@
-<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
-<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-<script>
-  window.jQuery    || document.write('<script src="<?= cq_javascript_src('frontend/jquery.js'); ?>"><\/script>');
-  window.jQuery.ui || document.write('<script src="<?= cq_javascript_src('frontend/jquery.ui.js'); ?>"><\/script>');
-</script>
+<?php include_component_slot('jquery_footer'); ?>
 
 <?php if ($sf_params->get('gcf')): ?>
 <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
@@ -29,12 +23,16 @@
     both: '<?= javascript_path('frontend/scripts.common.bundle.' . GIT_REVISION . '.js'); ?>',
     complete: function ()
     {
+      // Restore the original jQuery ready() function
+      jQuery.fn.ready = window.cq._ready;
+
       // http://stackoverflow.com/a/8567229
       (function ($, window, document)
       {
-        for (func in window.docready) {
-          $(document).ready(window.docready[func]);
+        for (func in window.cq.docready) {
+          $(document).ready(window.cq.docready[func]);
         }
+        window.cq.docready=[];
       }(jQuery, this, this.document));
 
       // Execute the controller in scripts.common.bundle.js

apps/frontend/templates/_jquery.php
@@ -1,0 +1,14 @@
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+  <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+  <script>
+    window.jQuery    || document.write('<script src="<?= cq_javascript_src('frontend/jquery.js'); ?>"><\/script>');
+    window.jQuery.ui || document.write('<script src="<?= cq_javascript_src('frontend/jquery.ui.js'); ?>"><\/script>');
+
+    // Store a reference to the original remove method.
+    window.cq._ready = jQuery.fn.ready;
+
+    // Define overriding method. (http://stackoverflow.com/a/8567229)
+    jQuery.fn.ready = window.cq.ready;
+  </script>

apps/frontend/modules/global/actions/components.class.php
@@ -39,6 +39,11 @@ class globalComponents extends cqFrontendComponents
     return sfView::SUCCESS;
   }
 
+  public function executeJquery()
+  {
+    return sfView::SUCCESS;
+  }
+
   public function executePagination(sfWebRequest $request)
   {
     /** @var $pager sfPropelPager */

web/blog/wp-content/themes/collectorsquest/index.new.php
@@ -73,10 +73,16 @@
     $data['breadcrumbs'] = $crumbs;
   }
 
+  wp_deregister_script('jquery');
+  wp_register_script('jquery', '/wp-content/themes/collectorsquest/js/empty.js', array(), '1.7.2', 1);
+  wp_enqueue_script('jquery');
+
   ob_start();
   wp_head();
   $head = ob_get_clean();
 
+  // Optimize the WordPress javascripts (avoid duplicates with out own scripts)
+  include __DIR__ .'/javascripts.php';
 
   ob_start();
   get_header();

web/blog/wp-content/themes/collectorsquest/javascripts.php
@@ -1,0 +1,45 @@
+<?php
+
+$scripts = array(
+  'jquery-ui-core' => '1.8.20',
+  'jquery-effects-core' => '1.8.20',
+  'jquery-effects-blind' => '1.8.20',
+  'jquery-effects-bounce' => '1.8.20',
+  'jquery-effects-clip' => '1.8.20',
+  'jquery-effects-drop' => '1.8.20',
+  'jquery-effects-explode' => '1.8.20',
+  'jquery-effects-fade' => '1.8.20',
+  'jquery-effects-fold' => '1.8.20',
+  'jquery-effects-highlight' => '1.8.20',
+  'jquery-effects-pulsate' => '1.8.20',
+  'jquery-effects-scale' => '1.8.20',
+  'jquery-effects-shake' => '1.8.20',
+  'jquery-effects-slide' => '1.8.20',
+  'jquery-effects-transfer' => '1.8.20',
+
+  'jquery-ui-accordion' => '1.8.20',
+  'jquery-ui-autocomplete' => '1.8.20',
+  'jquery-ui-datepicker' => '1.8.20',
+  'jquery-ui-dialog' => '1.8.20',
+  'jquery-ui-draggable' => '1.8.20',
+  'jquery-ui-droppable' => '1.8.20',
+  'jquery-ui-mouse' => '1.8.20',
+  'jquery-ui-position' => '1.8.20',
+  'jquery-ui-progressbar' => '1.8.20',
+  'jquery-ui-resizable' => '1.8.20',
+  'jquery-ui-selectable' => '1.8.20',
+  'jquery-ui-slider' => '1.8.20',
+  'jquery-ui-sortable' => '1.8.20',
+  'jquery-ui-tabs' => '1.8.20',
+  'jquery-ui-widget' => '1.8.20'
+);
+
+foreach ($scripts as $name => $version)
+{
+  if ($wp_scripts->query($name, 'queue'))
+  {
+    wp_deregister_script($name);
+    wp_register_script($name, '/wp-content/themes/collectorsquest/js/empty.js', array(), $version, 1);
+    wp_enqueue_script($name);
+  }
+}

web/blog/wp-content/themes/collectorsquest/js/empty.js
@@ -1,0 +1,1 @@
+// indeed empty ;)

web/js/frontend/application.js
@@ -540,7 +540,7 @@ var COMMON = window.COMMON = (function(){
       });
 
       // if the username cookie is set and has a value that is truthy
-      if ($.cookie(window.cq.cookies.username)) {
+      if (window.cq.cookies && $.cookie(window.cq.cookies.username)) {
         // triger the event to show the login form in the footer
         $('#footer-control-login-button').trigger('click');
         // and set the username to the value of the cookie

commit de8097e4a9237fbef99bea7fa48c48e9d129c48f

Yet again fix for the way we load jQuery (broken in 410d234a6fb0914a9d9ff9929198567a793f5d04)

apps/frontend/config/view.yml
@@ -23,14 +23,14 @@ default:
     header:        [global, header]
     footer:        [global, footer]
     breadcrumbs:   [global, breadcrumbs]
-    slot1:         []
+    slot1:         ~
 
-    sidebar_120:   []
-    sidebar_160:   []
-    sidebar_180:   []
-    sidebar_300:   []
+    sidebar_120:   ~
+    sidebar_160:   ~
+    sidebar_180:   ~
+    sidebar_300:   ~
 
-    jquery_header: []
+    jquery_header: ~
     jquery_footer: [global, jquery]
 
   has_layout:     true

apps/frontend/modules/_ajax/templates/_modalLogin.php
@@ -1,4 +1,4 @@
-<div id="modal-login-holder" class="modal fade">
+<div id="modal-login-holder" class="modal hide">
   <div class="modal-header">
     <button class="close" data-dismiss="modal">×</button>
     <ul class="nav nav-pills">

apps/frontend/modules/_ajax/templates/multimediaSuccess.php
@@ -1,4 +1,4 @@
-<div class="modal fade hide" data-dynamic="true">
+<div class="modal hide" data-dynamic="true">
   <div class="modal-body" style="max-height: none; padding: 0;">
     <?= image_tag_multimedia($multimedia, $which, array('max_width' => 940, 'max_height' => null)); ?>
   <div class="modal-footer">

apps/frontend/modules/_blog/config/view.yml
@@ -4,7 +4,7 @@ all:
     breadcrumbs: [_blog, breadcrumbs]
     sidebar_300: [_blog, sidebar]
     jquery_header: [global, jquery]
-    jquery_footer: []
+    jquery_footer: ~
 
   metas:
     title: ~

apps/frontend/modules/mycq/templates/_uploadPhotos.php
@@ -6,7 +6,7 @@
     <input type="file" name="files[]" multiple="multiple">
   </span>
 
-  <div id="fileupload-modal" class="modal hide fade">
+  <div id="fileupload-modal" class="modal hide">
     <div class="modal-header">
       <h3>Uploading files, please wait...</h3>
     </div>
@@ -34,7 +34,7 @@
       </table>
     </div>
     <div class="modal-footer">
-      <div class="span3 fileupload-progress fade">
+      <div class="span3 fileupload-progress">
         <!-- The global progress bar -->
         <div class="progress progress-info progress-striped active">
           <div class="bar" style="width:0;"></div>
@@ -55,7 +55,7 @@
 <!-- The template to display files available for upload -->
 <script id="template-upload" type="text/x-tmpl">
   {% for (var i=0, file; file=o.files[i]; i++) { %}
-  <tr class="template-upload fade">
+  <tr class="template-upload">
     <td class="preview"><span class="fade"></span></td>
     <td class="name"><span>{%=o.formatFileName(file.name)%}</span></td>
     {% if (file.error) { %}
@@ -87,7 +87,7 @@
 <!-- The template to display files available for download -->
 <script id="template-download" type="text/x-tmpl">
   {% for (var i=0, file; file=o.files[i]; i++) { %}
-  <tr class="template-download fade">
+  <tr class="template-download">
     {% if (file.error) { %}
     <td>-</td>
     <td class="name"><span>{%=o.formatFileName(file.name)%}</span></td>

apps/frontend/modules/mycq/templates/marketplaceCreditHistorySuccess.php
@@ -16,7 +16,7 @@
       <div class="tab-content-inner spacer">
 
       <?php /*
-      <div class="alert alert-block alert-notice fade in">
+      <div class="alert alert-block alert-notice in">
         <h4 class="alert-heading">Oh snap! You are out of credits for listing items for sale!</h4>
         <p class="spacer-top">
           Change this and that and try again. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum.

apps/frontend/templates/_flash_messages.php
@@ -1,25 +1,25 @@
 <?php if (has_slot('flash_error')): ?>
-<div class="alert alert-error alert-block fade in" data-alert="alert">
+<div class="alert alert-error alert-block in" data-alert="alert">
   <a class="close" data-dismiss="alert">×</a>
   <h4 class="alert-heading"><?= __('Oh snap! You got an error!', array(), 'flash'); ?></h4>
   <?= get_slot('flash_error'); ?>
 </div>
 <?php elseif ($sf_user->hasFlash('error')): ?>
-<div class="alert alert-error fade in" data-alert="alert">
+<div class="alert alert-error in" data-alert="alert">
   <a class="close" data-dismiss="alert">×</a>
   <?= implode('<br><br>', array_filter((array) $sf_user->getFlash('error', null, true))); ?>
 </div>
 <?php elseif ($sf_user->hasFlash('success')): ?>
-<div class="alert alert-success fade in" data-alert="alert">
+<div class="alert alert-success in" data-alert="alert">
   <a class="close" data-dismiss="alert">×</a>
   <?= $sf_user->getFlash('success', null, true); ?>
 </div>
 <?php elseif ($sf_user->hasFlash('success_ajax')): ?>
-<div class="alert alert-success fade in" data-alert="alert">
+<div class="alert alert-success in" data-alert="alert">
   <?= $sf_user->getFlash('success_ajax', null, true); ?>
 </div>
 <?php elseif ($sf_user->hasFlash('info')): ?>
-<div class="alert alert-info fade in" data-alert="alert">
+<div class="alert alert-info in" data-alert="alert">
   <a class="close" data-dismiss="alert">×</a>
   <?= $sf_user->getFlash('info', null, true); ?>
 </div>

apps/frontend/templates/_head.php
@@ -61,14 +61,11 @@
           apiKey: '<?= cqConfig::getCredentials('aviary', 'api_key'); ?>',
           postUrl: '<?= url_for('@mycq_aviary_update_image', true); ?>'
         }
-      },
-      _ready: null
+      }
     };
 
     // http://stackoverflow.com/a/8567229
-    var _docready=[];
-    var _ready = function(fn){_docready.push(fn)};
-    var $ = function(){return{ready:_ready}};
+    var _docready=[],$=function(){return{ready:function(fn){_docready.push(fn)}}};
   </script>
   <script src="<?= cq_javascript_src('frontend/head.js'); ?>"></script>
   <?php include_component_slot('jquery_header'); ?>

apps/frontend/templates/_javascripts.php
@@ -23,9 +23,6 @@
     both: '<?= javascript_path('frontend/scripts.common.bundle.' . GIT_REVISION . '.js'); ?>',
     complete: function ()
     {
-      // Restore the original jQuery.fn.ready() function
-      $.ready = window.cq._ready;
-
       // http://stackoverflow.com/a/8567229
       (function ($, window, document)
       {
@@ -35,7 +32,9 @@
       }(jQuery, this, this.document));
 
       // Execute the controller in scripts.common.bundle.js
-      $(document).ready(CONTROLLER.init());
+      $(document).ready(CONTROLLER.init);
+
+      $.holdReady(false);
     }
   }]);
 </script>

apps/frontend/templates/_jquery.php
@@ -6,9 +6,7 @@
     window.jQuery    || document.write('<script src="<?= cq_javascript_src('frontend/jquery.js'); ?>"><\/script>');
     window.jQuery.ui || document.write('<script src="<?= cq_javascript_src('frontend/jquery.ui.js'); ?>"><\/script>');
 
-    // Store a reference to the original remove method.
-    window.cq._ready = $.ready;
-
-    // Define overriding method. (http://stackoverflow.com/a/8567229)
-    $.ready = window._ready;
+    // We want to delay the $(document).ready()
+    // until Modernizr has loaded all dependencies
+    $.holdReady(true);
   </script>