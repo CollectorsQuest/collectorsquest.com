ce11ab227e6a7cfc1d2dba27a1c1b6da926640a4
Anton Ruzhekov <anton@collectorsquest.com>
2012-09-10 19:07:12 +0300

    replaced Now Playing video widget with Items for Sale on history landing pages (ref: https://basecamp.com/1759305/projects/353141-collectorsquest-com/todos/14636076-proposed-change-to)

--- apps/frontend/modules/aent/templates/_sidebarAmericanPickers.php
+++ apps/frontend/modules/aent/templates/_sidebarAmericanPickers.php
@@ -26,8 +26,10 @@
 
 <?php
   include_component(
-    '_sidebar', 'widgetMagnifyVideos',
-    array('playlist' => 'American Pickers')
+    '_sidebar', 'widgetCollectiblesForSale',
+    array(
+      'limit' => 6, 'fallback' => 'random'
+    )
   );
 ?>
 
