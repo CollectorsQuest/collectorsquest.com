5797cd76e522ab756b758690b46d3d710656ad0f
Pavel Goncharov <pashackpr@gmail.com>
2012-09-11 23:23:26 +0300

    backend links to collectors, collections, collectibles, points to frontend now https://basecamp.com/1759305/projects/19165-collectorsquest-com/todos/15161916-can-we-go-over-the

---
+++ apps/backend/modules/collectibles/templates/_list_collection.php
@@ -0,0 +1,5 @@
+<?php
+/** @var $Collectible Collectible */
+
+echo link_to_frontend($Collectible->getCollection(), 'collection_by_slug',
+  $Collectible->getCollection(), array('target' => '_blank'));
