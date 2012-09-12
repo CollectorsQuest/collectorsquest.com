4d74f280e3a71ea22f6544a8f7cd481949b5d7db
Kiril Angov <kiril.angov@gmail.com>
2012-09-12 18:06:52 +0900

    Small code refactoring for the backend external links changes

--- apps/backend/modules/collectibles/templates/_list_collection.php
+++ apps/backend/modules/collectibles/templates/_list_collection.php
@@ -2,4 +2,5 @@
 /** @var $Collectible Collectible */
 
 echo link_to_frontend($Collectible->getCollection(), 'collection_by_slug',
-  $Collectible->getCollection(), array('target' => '_blank'));
+  $Collectible->getCollection(), array('target' => '_blank')
+);
