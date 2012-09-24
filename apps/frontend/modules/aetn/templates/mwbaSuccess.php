c4eb244875f69efde61207aad3a86f5b05665c57
TeodorMarinov <teo.marinov@gmail.com>
2012-09-24 18:37:48 +0300

    Work on to-do https://basecamp.com/1759305/projects/127256-collectorsquest-com/todos/16108777-implement-in

--- apps/frontend/modules/aetn/templates/mwbaSuccess.php
+++ apps/frontend/modules/aetn/templates/mwbaSuccess.php
@@ -1,3 +1,17 @@
-<?php
+<div class="mwba-wrapper">
+  <div class="labels-ribbons">
+      <a href="/history/mwba/john-d-rockefeller" class="rockefeller_button"></a>
+      <a href="/history/mwba/theodore-roosevelt" class="roosevelt_button"></a>
+      <a href="/history/mwba/cornelius-vanderbilt#" class="vanderbilt_button"></a>
+  </div>
+
+    <span class="banner">
+        <a href="#" title="The Men Who Built America">
+            <img src="../images/headlines/history-banner-mwba-landing-page.jpg" alt="The Men Who Built America">
+        </a>
+    </span>
+</div>
+
+
 
 
