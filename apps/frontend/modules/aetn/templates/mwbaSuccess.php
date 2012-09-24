6ac84061cb541dcf5388d0b47d9a841f74ac62e6
Teo <teo.marinov@gmail.com>
2012-09-25 00:16:55 +0300

    Implemented html/css for the MWBA landing page

--- apps/frontend/modules/aetn/templates/mwbaSuccess.php
+++ apps/frontend/modules/aetn/templates/mwbaSuccess.php
@@ -1,17 +1,23 @@
 <div class="mwba-wrapper">
+  <div class="top-banner">
+    <?= image_tag('frontend/misc/mwba-top-banner.png', array('alt' => 'The Men Who Built America'));  ?>
+  </div>
   <div class="labels-ribbons">
-      <a href="/history/mwba/john-d-rockefeller" class="rockefeller_button"></a>
-      <a href="/history/mwba/theodore-roosevelt" class="roosevelt_button"></a>
-      <a href="/history/mwba/cornelius-vanderbilt#" class="vanderbilt_button"></a>
+      <a href="<?= url_for('@aetn_mwba_petroliana', true); ?>" class="rockefeller_button"></a>
+      <a href="<?= url_for('@aetn_mwba_rooseveltiana', true); ?>" class="roosevelt_button"></a>
+      <a href="<?= url_for('@aetn_mwba_railroadiana', true); ?>" class="vanderbilt_button"></a>
   </div>
 
-    <span class="banner">
-        <a href="#" title="The Men Who Built America">
-            <img src="../images/headlines/history-banner-mwba-landing-page.jpg" alt="The Men Who Built America">
-        </a>
-    </span>
+  <div class="center-graphic">
+    <?= image_tag('frontend/misc/mwba-center-graphic.png'); ?>
+  </div>
+
+  <div class="banner">
+    <?php
+      echo link_to(cq_image_tag(
+        'headlines/history-banner-mwba-landing-page.jpg',
+        array('alt_title' => 'The Men Who Built America')
+      ), 'http://www.history.com/shows/men-who-built-america', array('target' => '_blank'));
+    ?>
+  </div>
 </div>
-
-
-
-
