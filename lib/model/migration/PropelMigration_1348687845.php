95a1c6e00503d53a17f2032985736fa70179fd7d
Kiril Angov <kiril.angov@gmail.com>
2012-09-26 22:35:23 +0300

    Tabs -> Spaces in PropelMigration_1348687845.php

--- lib/model/migration/PropelMigration_1348687845.php
+++ lib/model/migration/PropelMigration_1348687845.php
@@ -7,35 +7,35 @@
 class PropelMigration_1348501687
 {
 
-	public function preUp($manager)
-	{
-		// add the pre-migration code here
-	}
+  public function preUp($manager)
+  {
+    // add the pre-migration code here
+  }
 
-	public function postUp($manager)
-	{
-		// add the post-migration code here
-	}
+  public function postUp($manager)
+  {
+    // add the post-migration code here
+  }
 
-	public function preDown($manager)
-	{
-		// add the pre-migration code here
-	}
+  public function preDown($manager)
+  {
+    // add the pre-migration code here
+  }
 
-	public function postDown($manager)
-	{
-		// add the post-migration code here
-	}
+  public function postDown($manager)
+  {
+    // add the post-migration code here
+  }
 
-	/**
-	 * Get the SQL statements for the Up migration
-	 *
-	 * @return array list of the SQL strings to execute for the Up migration
-	 *               the keys being the datasources
-	 */
-	public function getUpSQL()
-	{
-		return array (
+  /**
+   * Get the SQL statements for the Up migration
+   *
+   * @return array list of the SQL strings to execute for the Up migration
+   *               the keys being the datasources
+   */
+  public function getUpSQL()
+  {
+    return array (
   'propel' => '
 # This is a fix for InnoDB in MySQL >= 4.1.x
 # It "suspends judgement" for fkey relationships until are tables are set.
@@ -50,17 +50,17 @@ ALTER TABLE `comment_archive` ADD `is_spam` TINYINT(1) NOT NULL AFTER `is_hidden
 SET FOREIGN_KEY_CHECKS = 1;
 ',
 );
-	}
+  }
 
-	/**
-	 * Get the SQL statements for the Down migration
-	 *
-	 * @return array list of the SQL strings to execute for the Down migration
-	 *               the keys being the datasources
-	 */
-	public function getDownSQL()
-	{
-		return array (
+  /**
+   * Get the SQL statements for the Down migration
+   *
+   * @return array list of the SQL strings to execute for the Down migration
+   *               the keys being the datasources
+   */
+  public function getDownSQL()
+  {
+    return array (
   'propel' => '
 # This is a fix for InnoDB in MySQL >= 4.1.x
 # It "suspends judgement" for fkey relationships until are tables are set.
@@ -75,6 +75,6 @@ ALTER TABLE `comment_archive` DROP `is_spam`;
 SET FOREIGN_KEY_CHECKS = 1;
 ',
 );
-	}
+  }
 
 }
