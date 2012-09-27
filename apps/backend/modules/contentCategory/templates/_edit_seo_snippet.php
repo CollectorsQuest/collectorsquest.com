17ae5adc206ec0ae4c093d502c66ecfa94928794
Kiril Angov <kiril.angov@gmail.com>
2012-09-27 13:10:35 +0300

    Basic SEO management and preview for Content Categories in apps/backend

---
+++ apps/backend/modules/contentCategory/templates/_edit_seo_snippet.php
@@ -0,0 +1,16 @@
+<?php
+  /** @var $ContentCategory ContentCategory */
+  $ContentCategory = $form->getObject();
+
+  $default = 'Browse collectible, vintage and antique items on Collectors Quest, like these
+              '. $ContentCategory->getName() .' items, posted by trusted sellers and passionate collectors!';
+
+  include_partial(
+    'global/seo_snippet',
+    array(
+      'title' => $ContentCategory->getSeoTitle() . ' | Collectors Quest',
+      'href' => url_for_frontend('content_category', array('sf_subject' => $ContentCategory)),
+      'description' => $ContentCategory->getSeoDescription() ?: $default
+    )
+  );
+?>
