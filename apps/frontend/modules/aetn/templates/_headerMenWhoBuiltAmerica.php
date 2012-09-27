005778c0b219ef268ae89d3c5a0443f12532dcab
TeodorMarinov <teo.marinov@gmail.com>
2012-09-27 18:38:22 +0300

    Work on to-do https://basecamp.com/1759305/projects/1181514-collectorsquest-com/todos/16409197-implement-in

--- apps/frontend/modules/aetn/templates/_headerMenWhoBuiltAmerica.php
+++ apps/frontend/modules/aetn/templates/_headerMenWhoBuiltAmerica.php
@@ -6,31 +6,9 @@
   <div class="paper-wrapper">
     <div class="title"></div>
     <div class="text-container">
-      <p>
-          Theodore Roosevelt: naturalist, Rough Rider, Nobel Peace Prize winner, founder of the Bull Moose party,
-          and the youngest man to ever serve as president. As a paragon of masculinity and determination, Roosevelt
-          inspired a nation. Roosevelt&rsquo;s softly-speaking, big-stick-carrying personality has endured, and people
-          continue to collect anything they can find that relates to his legacy.
-      </p>
-      <p>
-        Of course, Rooseveltiana collectors have their share of
-        <a href="http://www.collectorsquest.com/collectible/16503/tr-in-oval-with-flag-button-c1021" target="_blank">
-            political buttons</a>, but one can also find his face and figure on
-        <a href="http://www.collectorsquest.com/collectible/16967/teddy-roosevelt-clock" target="_blank">clocks</a> and
-        <a href="http://www.collectorsquest.com/collectible/16608/progressive-tr-watch-fob-c1021" target="_blank">
-            watches
-        </a>,
-        <a href="http://www.collectorsquest.com/collectible/87813/rough-rider-knife-c1021" target="_blank">knives</a>,
-        <a href="http://www.collectorsquest.com/collectible/87810/tr-and-lion-mechanical-bank-c1021" target="_blank">
-            banks
-        </a>,
-        <a href="http://www.collectorsquest.com/collectible/87808/tr-flow-blue-flower-plate-c1021" target="_blank">
-            plates</a>, and many other types of items which we&rsquo;ve collected here. On October 27th,
-        Roosevelt&rsquo;s 154th birthday, the American Museum of Natural History will re-open their
-        <a href="http://www.amnh.org/explore/theodore-roosevelt-memorial-officially-reopens-october-27-2012"
-           target="_blank">Theodore Roosevelt memorial</a>.For more information on Theodore Roosevelt, check out the
-        <a href="http://www.trassociation.org/">Theodore Roosevelt Association&rsquo;s website</a>.
-      </p>
+      <?php
+      include_partial('mwba_man_pages_header');
+      ?>
     </div>
   </div>
   <a href="#" class="previous-man"></a>
