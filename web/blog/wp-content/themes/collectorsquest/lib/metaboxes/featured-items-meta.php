1196c0a4615b3e3844ba40f39182828542a30d5b
Anton Ruzhekov <anton@collectorsquest.com>
2012-09-28 14:48:05 +0300

    Remove not needed exclude fields
     We don't need to exclude Collectibles for Sale and WP Posts
     Ref: https://basecamp.com/1759305/projects/353141-collectorsquest-com/todos/16555118-extend-the-featured

--- web/blog/wp-content/themes/collectorsquest/lib/metaboxes/featured-items-meta.php
+++ web/blog/wp-content/themes/collectorsquest/lib/metaboxes/featured-items-meta.php
@@ -54,16 +54,6 @@
 
     <textarea name="<?php $mb->the_name(); ?>" cols="5" rows="3"><?php $mb->the_value(); ?></textarea></label>
 
-  <label>Collectibles for Sale IDs:
-    <?php $mb->the_field('cq_collectibles_for_sale_ids_exclude'); ?>
-
-    <textarea name="<?php $mb->the_name(); ?>" cols="5" rows="3"><?php $mb->the_value(); ?></textarea></label>
-
-  <label>WordPress Post IDs:
-    <?php $mb->the_field('cq_wp_post_ids_exclude'); ?>
-
-    <textarea name="<?php $mb->the_name(); ?>" cols="5" rows="3"><?php $mb->the_value(); ?></textarea></label>
-
   <p>Enter comma separated tags. (banner,basketball,NBA)</p>
 
   <label>Tags:
