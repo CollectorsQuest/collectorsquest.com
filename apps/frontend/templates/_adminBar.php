09dabb65af28a0aa04e9496b5d4c6d56d2ce450f
Pavel Goncharov <pashackpr@gmail.com>
2012-10-02 18:52:13 +0300

    Button for backed at adminBar
    Ref: https://basecamp.com/1759305/projects/19165-collectorsquest-com/todos/16479574-add-some-button-in

--- apps/frontend/templates/_adminBar.php
+++ apps/frontend/templates/_adminBar.php
@@ -7,43 +7,50 @@ use_helper('cqBackend');
 ?>
 
 <div id="admin-bar" class="navbar navbar-inverse">
-  <div class="navbar-inner">
-    <div class="container-fluid">
-      <a class="brand" href='<?php echo url_for_backend('homepage') ?>'>
-        Admin Dashboard
-      </a>
-      <?php if (count($items)) : ?>
-        <?php foreach ($items as $name => $sub_item): ?>
-          <?php if (count($sub_item)): ?>
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <a class="brand" href='<?php echo url_for('homepage') ?>'>
+                CollectorsQuest.com
+            </a>
+
+
             <ul class="nav">
-                <li class="dropdown">
-                    <a href="#nogo" data-toggle="dropdown" class="dropdown-toggle">
-                        <?= $name ?> <span class="caret"></span>
+                <li>
+                    <a href='<?php echo url_for_backend('homepage') ?>'>
+                        <i class="icon-wrench icon-white"></i>&nbsp;Admin Dashboard
                     </a>
-                    <ul class="dropdown-menu">
-                      <?php foreach ($sub_item as $key => $item): ?>
-                        <li>
-                            <a target="_blank" href="<?= $key ?>">
-                                <span><?= $item ?></span>
-                            </a>
-                        </li>
-                      <?php endforeach; ?>
-                    </ul>
+                </li>
+              <?php if (count($items)) : ?>
+                <?php foreach ($items as $name => $sub_item): ?>
+                  <?php if (count($sub_item)): ?>
+                    <li class="dropdown">
+                      <a href="#nogo" data-toggle="dropdown" class="dropdown-toggle">
+                        <?= $name ?> <span class="caret"></span>
+                      </a>
+                      <ul class="dropdown-menu">
+                       <?php foreach ($sub_item as $key => $item): ?>
+                         <li>
+                           <a target="_blank" href="<?= $key ?>">
+                             <span><?= $item ?></span>
+                           </a>
+                         </li>
+                       <?php endforeach; ?>
+                       </ul>
+                    </li>
+                  <?php endif ?>
+                <?php endforeach; ?>
+              <?php endif ?>
+            </ul>
+            <ul class="nav pull-right">
+                <li>
+                  <?php
+                  echo link_to_backend(
+                    '<i class="icon-lock icon-white"></i>&nbsp;' . __('Admin Logout'), 'ice_backend_signout', array(),
+                    array('onclick' => 'return confirm("You will be also logged out of your webmail. Are you sure you want to continue?")')
+                  );
+                  ?>
                 </li>
             </ul>
-          <?php endif ?>
-        <?php endforeach; ?>
-      <?php endif ?>
-      <ul class="nav pull-right">
-        <li>
-          <?php
-          echo link_to_backend(
-            '<i class="icon-lock icon-white"></i>&nbsp;'.__('Admin Logout'), 'ice_backend_signout',array(),
-            array('onclick' => 'return confirm("You will be also logged out of your webmail. Are you sure you want to continue?")')
-          );
-          ?>
-        </li>
-      </ul>
+        </div>
     </div>
-  </div>
 </div>
