b3904b37145ce31434cf39aa6075298c2ce17adc
Kiril Angov <kiril.angov@gmail.com>
2012-10-10 01:10:09 -0400

    Fix for PropelMigration_1349810704.php

--- lib/model/migration/PropelMigration_1349844479.php
+++ lib/model/migration/PropelMigration_1349844479.php
@@ -37,14 +37,12 @@ class PropelMigration_1349844479
         # It "suspends judgement" for fkey relationships until are tables are set.
         SET FOREIGN_KEY_CHECKS = 0;
 
-        ALTER TABLE `collectible` ADD
-        (
-          `average_rate` FLOAT,
-          `average_content_rate` FLOAT,
-          `average_images_rate` FLOAT
-        );
+        ALTER TABLE `collectible` ADD `average_rate` FLOAT AFTER `score`;
+        ALTER TABLE `collectible` ADD `average_content_rate` FLOAT AFTER `average_rate`;
+        ALTER TABLE `collectible` ADD `average_images_rate` FLOAT AFTER `average_content_rate`;
 
-        CREATE TABLE `collectible_rates`
+        DROP TABLE IF EXISTS `collectible_rate`;
+        CREATE TABLE `collectible_rate`
         (
           `id` INTEGER NOT NULL AUTO_INCREMENT,
           `dimension` VARCHAR(255) NOT NULL,
@@ -52,16 +50,16 @@ class PropelMigration_1349844479
           `collectible_id` INTEGER NOT NULL,
           `collector_id` INTEGER NOT NULL,
           PRIMARY KEY (`id`),
-          INDEX `collectible_rates_FI_1` (`collectible_id`),
-          INDEX `collectible_rates_FI_2` (`collector_id`),
-          CONSTRAINT `collectible_rates_FK_1`
+          INDEX `collectible_rate_FI_1` (`collectible_id`),
+          INDEX `collectible_rate_FI_2` (`collector_id`),
+          CONSTRAINT `collectible_rate_FK_1`
             FOREIGN KEY (`collectible_id`)
             REFERENCES `collectible` (`id`)
             ON DELETE CASCADE,
-          CONSTRAINT `collectible_rates_FK_2`
+          CONSTRAINT `collectible_rate_FK_2`
             FOREIGN KEY (`collector_id`)
             REFERENCES `collector` (`id`)
-            ON DELETE SET NULL
+            ON DELETE CASCADE
         ) ENGINE=InnoDB;
 
         # This restores the fkey checks, after having unset them earlier
@@ -91,9 +89,7 @@ class PropelMigration_1349844479
         DROP TABLE IF EXISTS `collectible_rates`;
 
         ALTER TABLE `collectible` DROP `average_rate`;
-
         ALTER TABLE `collectible` DROP `average_content_rate`;
-
         ALTER TABLE `collectible` DROP `average_images_rate`;
 
         # This restores the fkey checks, after having unset them earlier
