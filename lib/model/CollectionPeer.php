bb43c0408580afc0410507a45a59dea79ae0a97d
Pavel Goncharov <pashackpr@gmail.com>
2012-10-11 18:14:13 +0300

    Implement "hidden" tags
    Add Internal tags  methods to Collection Model
    
    Ref: https://basecamp.com/1759305/projects/19165-collectorsquest-com/todos/17779277-implement-hidden

--- lib/model/CollectionPeer.php
+++ lib/model/CollectionPeer.php
@@ -4,5 +4,7 @@ require 'lib/model/om/BaseCollectionPeer.php';
 
 class CollectionPeer extends BaseCollectionPeer
 {
+  const TAGS_NAMESPACE_INTERNAL   = 'internal';
+  const TAGS_KEY_TAG              = 'tag';
 
 }
