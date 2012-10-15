e6d84d56ee73526f94ba7063adbbe91bd92bf2e0
Pavel Goncharov <pashackpr@gmail.com>
2012-10-15 14:06:32 +0300

    Backend Messages list
    Replace wrong sorting functionality

--- apps/backend/modules/messages/actions/actions.class.php
+++ apps/backend/modules/messages/actions/actions.class.php
@@ -13,42 +13,4 @@ require_once dirname(__FILE__) . '/../lib/messagesGeneratorHelper.class.php';
  */
 class messagesActions extends autoMessagesActions
 {
-  protected function processSort($query)
-  {
-    $sort = $this->getSort();
-    if (array(null, null) == $sort)
-    {
-      return;
-    }
-
-    if (in_array($sort[0], array('sender_name','reseiver_name')))
-    {
-       $column = sfInflector::camelize($sort[0]);
-
-    }
-    else
-    {
-      try
-      {
-        $column = PrivateMessagePeer::translateFieldName($sort[0], BasePeer::TYPE_FIELDNAME,
-          BasePeer::TYPE_PHPNAME);
-      } catch (PropelException $e)
-      {
-        // probably a fake column, using a custom orderByXXX() query method
-        $column = sfInflector::camelize($sort[0]);
-      }
-    }
-    $method = sprintf('orderBy%s', $column);
-
-    try
-    {
-      $query->$method('asc' == $sort[1] ? 'asc' : 'desc');
-    }
-    catch (PropelException $e)
-    {
-      // non-existent sorting method
-      // ignore the sort parameter
-      $this->setSort(array(null, null));
-    }
-  }
 }
