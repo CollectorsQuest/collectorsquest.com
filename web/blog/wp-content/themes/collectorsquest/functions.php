6ac0a5f909f70b09a7dfa217717889972ab05aba
Anton Ruzhekov <anton@collectorsquest.com>
2012-10-15 10:17:00 +0300

    Add post tags and custom fields to Market Themes
    Ref: https://basecamp.com/1759305/projects/1258511-collectorsquest-com/todos/17856459-create-section-in#comment_27058038

--- web/blog/wp-content/themes/collectorsquest/functions.php
+++ web/blog/wp-content/themes/collectorsquest/functions.php
@@ -319,7 +319,8 @@ function cq_custom_post_type_init()
     'rewrite'         => array( 'slug' => 'cms', 'with_front' => false ),
     'query_var'       => false,
     'menu_position'   => 100,
-    'supports'        => array('title', 'editor', 'excerpt', 'thumbnail', 'revisions')
+    'taxonomies'      => array('post_tag'),
+    'supports'        => array('title', 'editor', 'revisions', 'custom-fields')
   ));
 }
 
