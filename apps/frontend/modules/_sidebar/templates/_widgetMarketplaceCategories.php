f9130a922ea9b630d69184e3d4d79381adc79565
Anton Ruzhekov <anton@collectorsquest.com>
2012-10-16 16:24:40 +0300

    Fix Marketplace Categories sidebar widget
    Remove 'See all' link, commented out the code to display 3rd and 4th
    level categories, fixed problem with widget title

--- apps/frontend/modules/_sidebar/templates/_widgetMarketplaceCategories.php
+++ apps/frontend/modules/_sidebar/templates/_widgetMarketplaceCategories.php
@@ -9,9 +9,11 @@
    * @var $current_sub_subcategory      ContentCategory
    * @var $current_sub_sub_subcategory  ContentCategory
    * @var $height stdClass
+   * @var $category_title string
    */
 
-  if (IceGateKeeper::open('marketplace_categories', 'page'))
+  // remove the 'false' to enable 'See all' link
+  if (false && IceGateKeeper::open('marketplace_categories', 'page'))
   {
     $link = link_to(
       'See all &raquo;', '@marketplace_categories',
@@ -23,7 +25,7 @@
     $link = null;
   }
 
-  cq_sidebar_title($current_category, $link);
+  cq_sidebar_title($category_title, $link);
   $_height = -63;
 ?>
 
@@ -56,65 +58,69 @@
               $_height -= 20;
             }
         ?>
-          <ul>
-            <?php foreach ($sub_subcategories as $j => $sub_subcategory): ?>
-              <li>
-                <?php
-                  if ($sub_subcategory != $current_sub_subcategory)
-                  {
-                    if ($sub_subcategory)
+          <?php if(!empty($sub_subcategories)): ?>
+            <ul>
+              <?php foreach ($sub_subcategories as $j => $sub_subcategory): ?>
+                <li>
+                  <?php
+                    if ($sub_subcategory != $current_sub_subcategory)
                     {
-                      echo link_to(
-                        $sub_subcategory->getName(), 'marketplace_category_by_slug',
-                        $sub_subcategory, array('title' => $sub_subcategory->getName())
-                      );
-                      $_height -= 20;
+                      if ($sub_subcategory)
+                      {
+                        echo link_to(
+                          $sub_subcategory->getName(), 'marketplace_category_by_slug',
+                          $sub_subcategory, array('title' => $sub_subcategory->getName())
+                        );
+                        $_height -= 20;
+                      }
                     }
-                  }
-                  else
-                  {
-                    if (empty($sub_sub_subcategories) || $current_sub_sub_subcategory->isNew())
+                    else
                     {
-                      echo ($sub_subcategory) ? $sub_subcategory : '';
-                      $_height -= 20;
-                    }
-                    else if ($sub_subcategory)
-                    {
-                      echo link_to(
-                        $sub_subcategory->getName(), 'marketplace_category_by_slug',
-                        $sub_subcategory, array('title' => $sub_subcategory->getName())
-                      );
-                      $_height -= 20;
-                    }
-                ?>
-                  <ul>
-                    <?php foreach ($sub_sub_subcategories as $k => $sub_sub_subcategory): ?>
-                      <li>
-                        <?php
-                          if ($sub_sub_subcategory != $current_sub_sub_subcategory)
-                          {
-                            if ($sub_sub_subcategory)
-                            {
-                              echo link_to(
-                                $sub_sub_subcategory->getName(), 'marketplace_category_by_slug',
-                                $sub_sub_subcategory, array('title' => $sub_sub_subcategory->getName())
-                              );
-                              $_height -= 20;
-                            }
-                          }
-                          else if ($sub_sub_subcategory)
-                          {
-                            echo ($sub_sub_subcategory) ? $sub_sub_subcategory : '';
-                            $_height -= 20;
-                          }
-                        ?>
-                      </li>
-                    <?php endforeach; ?>
-                  </ul>
-                <?php } // endif ($sub_subcategory != $current_sub_subcategory) ?>
-              </li>
-            <?php endforeach; ?>
-          </ul>
+                      if (empty($sub_sub_subcategories) || $current_sub_sub_subcategory->isNew())
+                      {
+                        echo ($sub_subcategory) ? $sub_subcategory : '';
+                        $_height -= 20;
+                      }
+                      else if ($sub_subcategory)
+                      {
+                        echo link_to(
+                          $sub_subcategory->getName(), 'marketplace_category_by_slug',
+                          $sub_subcategory, array('title' => $sub_subcategory->getName())
+                        );
+                        $_height -= 20;
+                      }
+                  ?>
+                    <?php if(!empty($sub_sub_subcategories)): ?>
+                      <ul>
+                        <?php foreach ($sub_sub_subcategories as $k => $sub_sub_subcategory): ?>
+                          <li>
+                            <?php
+                              if ($sub_sub_subcategory != $current_sub_sub_subcategory)
+                              {
+                                if ($sub_sub_subcategory)
+                                {
+                                  echo link_to(
+                                    $sub_sub_subcategory->getName(), 'marketplace_category_by_slug',
+                                    $sub_sub_subcategory, array('title' => $sub_sub_subcategory->getName())
+                                  );
+                                  $_height -= 20;
+                                }
+                              }
+                              else if ($sub_sub_subcategory)
+                              {
+                                echo ($sub_sub_subcategory) ? $sub_sub_subcategory : '';
+                                $_height -= 20;
+                              }
+                            ?>
+                          </li>
+                        <?php endforeach; ?>
+                      </ul>
+                    <?php endif; // !empty($sub_sub_subcategories) ?>
+                  <?php } // endif ($sub_subcategory != $current_sub_subcategory) ?>
+                </li>
+              <?php endforeach; ?>
+            </ul>
+          <?php endif; // !empty($sub_subcategories) ?>
         <?php } // endif ($subcategory != $current_subcategory) ?>
       </li>
     <?php endforeach; ?>
