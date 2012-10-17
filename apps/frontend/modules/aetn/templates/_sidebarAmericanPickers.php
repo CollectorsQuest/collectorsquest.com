5d60fa61a72d1f2bccc8df4be1d8fcb7b1a6ad2c
Anton Ruzhekov <anton@collectorsquest.com>
2012-10-17 13:05:06 +0300

    Add Holiday Market banners from mockup
    Ref: https://basecamp.com/1759305/projects/1258511-collectorsquest-com/todos/18436805-place-the-banners

--- apps/frontend/modules/aetn/templates/_sidebarAmericanPickers.php
+++ apps/frontend/modules/aetn/templates/_sidebarAmericanPickers.php
@@ -13,6 +13,8 @@
 </div>
 */ ?>
 
+<?php include_partial('marketplace/holidayMarketPromo_300x90'); ?>
+
 <div class="banner-sidebar-promo-300-90">
   <a href="<?= url_for('@aetn_pawn_stars', true); ?>" title="Check out items seen on Pawn Stars">
     <img src="/images/headlines/pawn-stars-banner.jpg" alt="Check out items seen on Pawn Stars">
--- apps/frontend/modules/general/templates/_sidebarIndex.php
+++ apps/frontend/modules/general/templates/_sidebarIndex.php
@@ -1,5 +1,16 @@
 <p class="text-center">
   <?php
+    if (IceGateKeeper::open('holiday_marketplace', 'page'))
+    {
+      echo link_to(
+        cq_image_tag(
+          'headlines/2012-10-17_CQHolidayMarket_160x346.jpg',
+          array('style' => 'margin-bottom: 15px;')
+        ),
+        'marketplace/holiday', array('ref' => cq_link_ref('sidebar'))
+      );
+    }
+
     if (!$sf_user->isAuthenticated())
     {
       echo link_to(
