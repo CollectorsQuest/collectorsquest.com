CollectorsQuest/collectorsquest.com

commit 45b9880ab41f03bb185981774c593fec18e4fc42

Remove "mycq_seller_pay" from the GateKeeper

apps/frontend/modules/seller/lib/form/SellerPackagesForm.class.php
@@ -308,7 +308,7 @@ class SellerPackagesForm extends BaseForm
 
   public function applyPromoCode($validator, $values)
   {
-    if (IceGateKeeper::locked('mycq_seller_pay') && empty($values['promo_code']))
+    if (empty($values['promo_code']))
     {
       throw new sfValidatorErrorSchema($validator, array(
         'promo_code'=> new sfValidatorError($validator,
@@ -361,16 +361,6 @@ class SellerPackagesForm extends BaseForm
         if ($this->package->getPackagePrice() <= $this->package->getDiscount())
         {
           $this->validatorSchema['payment_type']->setOption('required', false);
-        }
-        else if (IceGateKeeper::locked('mycq_seller_pay'))
-        {
-          throw new sfValidatorErrorSchema(
-            $validator, array(
-              'promo_code' => new sfValidatorError(
-                $validator, 'This promo code cannot be used in beta testing mode!'
-              )
-            )
-          );
         }
       }
 

apps/frontend/config/gatekeeper.yml
@@ -12,7 +12,6 @@ all:
     mycq_homepage: true
     mycq_marketplace: true
     mycq_wanted: false
-    mycq_seller_pay: true
     mycq_profile_addresses: false
     collectible_shipping: true
     collectible_allow_no_shipping: true
@@ -34,7 +33,6 @@ next:
     mycq_homepage: true
     mycq_marketplace: true
     mycq_wanted: false
-    mycq_seller_pay: true
     mycq_profile_addresses: false
     collectible_shipping: true
     collectible_allow_no_shipping: true
@@ -55,7 +53,6 @@ dev:
     mycq_homepage: true
     mycq_marketplace: true
     mycq_wanted: false
-    mycq_seller_pay: true
     mycq_profile_addresses: false
     collectible_shipping: true
     collectible_allow_no_shipping: true

apps/frontend/modules/marketplace/templates/indexSuccess.php
@@ -45,7 +45,6 @@ $sf_user->setFlash('height_main_div', $height_main_div, false, 'internal');
 </div>
 <?php endif; ?>
 
-<?php if (IceGateKeeper::open('mycq_seller_pay')): ?>
 <div class="banners-620 spacer-top-20">
   <?php
   if (!$sf_user->isAuthenticated())
@@ -63,4 +62,3 @@ $sf_user->setFlash('height_main_div', $height_main_div, false, 'internal');
   }
   ?>
 </div>
-<?php endif; ?>

apps/frontend/modules/seller/templates/packagesSuccess.php
@@ -44,12 +44,6 @@
 
 <?php if ($packagesForm->hasGlobalErrors()): ?>
   <?= $packagesForm->renderGlobalErrors(); ?>
-<?php elseif (IceGateKeeper::locked('mycq_seller_pay')): ?>
-  <div class="alert alert-info">
-    The Marketplace is almost ready! If you're interested in becoming a seller,
-    please email <?= mail_to('info@collectorsquest.com', 'info@collectorsquest.com'); ?>
-    and we'll add you to our notification list.
-  </div>
 <?php endif; ?>
 
 <form action="<?= url_for('seller_packages')?>" method="post""
@@ -58,7 +52,6 @@
 
   <?php
     if (
-      IceGateKeeper::open('mycq_seller_pay') &&
       isset($packagesForm['pending_transaction_confirm']) &&
       $packagesForm->isError('pending_transaction_confirm')
     ):
@@ -146,20 +139,18 @@
         <?php endif; ?>
       </div>
     </div>
-    <?php if (IceGateKeeper::open('mycq_seller_pay')): ?>
+
     <?php cq_section_title('How would you like to pay?', null, array('id' => 'payment-header')); ?>
-      <div class="payment-type">
-        <div class="control-group">
-          <div class="controls-inline clearfix">
-            <?= $packagesForm['payment_type']->render() ?>
-          </div>
-          <?= $packagesForm['payment_type']->renderError() ?>
+    <div class="payment-type">
+      <div class="control-group">
+        <div class="controls-inline clearfix">
+          <?= $packagesForm['payment_type']->render() ?>
         </div>
+        <?= $packagesForm['payment_type']->renderError() ?>
       </div>
-    <?php endif; ?>
+    </div>
   </fieldset>
 
-  <?php if (IceGateKeeper::open('mycq_seller_pay')): ?>
   <fieldset id="credit-card-info" class="js-hide cf">
 
     <div class="credit-card-wrapper">
@@ -222,7 +213,6 @@
     <?= $packagesForm['zip']->renderRow() ?>
     <?= $packagesForm['country']->renderRow() ?>
   </fieldset>
-  <?php endif; ?>
 
   <div class="agreement-checks control-group spacer-bottom-reset">
     <label class="control-label control-label">&nbsp;</label>

commit 949b7cc104b02b5454298d9f311c51b4015e1f13

Fix a problem introduced by 45b9880ab41f03bb185981774c593fec18e4fc42

apps/frontend/modules/seller/lib/form/SellerPackagesForm.class.php
@@ -310,16 +310,6 @@ class SellerPackagesForm extends BaseForm
   {
     if (empty($values['promo_code']))
     {
-      throw new sfValidatorErrorSchema($validator, array(
-        'promo_code'=> new sfValidatorError($validator,
-          'A promo code is required while we are in private beta!<br/>
-           Please email info@collectorsquest.com for more information.'
-        ))
-      );
-    }
-
-    if (empty($values['promo_code']))
-    {
       return $values;
     }
 