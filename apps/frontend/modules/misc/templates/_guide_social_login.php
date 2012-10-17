82a10a26d611e7abfd1c87d0aa719bb0c3f697d4
Kiril Angov <kiril.angov@gmail.com>
2012-10-17 14:22:09 -0400

    Improve the social login modal
    Ref: https://basecamp.com/1759305/projects/19290-collectorsquest-com/todos/10299603-signing-up-via

--- apps/frontend/modules/misc/templates/_guide_social_login.php
+++ apps/frontend/modules/misc/templates/_guide_social_login.php
@@ -2,7 +2,7 @@
   <?php
     echo link_to(
       '<i class="hide-text">'. ucfirst($action) .' using Facebook</i>',
-      '@ajax?section=partial&page=socialModalLogin',
+      '@ajax?section=partial&page=socialModalLogin&provider=facebook',
       array(
         'class' => 'open-dialog facebook-big-icon', 'rel' => 'tooltip', 'data-placement' => 'top',
         'onclick' => 'return false', 'title' => ucfirst($action).' using Facebook'
@@ -11,7 +11,7 @@
 
     echo link_to(
       '<i class="hide-text">'. ucfirst($action) .' using Twitter</i>',
-      '@ajax?section=partial&page=socialModalLogin',
+      '@ajax?section=partial&page=socialModalLogin&provider=twitter',
       array(
         'class' => 'open-dialog twitter-big-icon', 'rel' => 'tooltip', 'data-placement' => 'top',
         'onclick' => 'return false', 'title' => ucfirst($action).' using Twitter'
@@ -20,7 +20,7 @@
 
     echo link_to(
       '<i class="hide-text">'. ucfirst($action) .' using Google+</i>',
-      '@ajax?section=partial&page=socialModalLogin',
+      '@ajax?section=partial&page=socialModalLogin&provider=google',
       array(
         'class' => 'open-dialog google-big-icon', 'rel' => 'tooltip', 'data-placement' => 'top',
         'onclick' => 'return false', 'title' => ucfirst($action).' using Google+'
@@ -29,7 +29,7 @@
 
     echo link_to(
       '<i class="hide-text">'. ucfirst($action) .' using Windows Live ID</i>',
-      '@ajax?section=partial&page=socialModalLogin',
+      '@ajax?section=partial&page=socialModalLogin&provider=live_id',
       array(
         'class' => 'open-dialog live-id-big-icon', 'rel' => 'tooltip', 'data-placement' => 'top',
         'onclick' => 'return false', 'title' => ucfirst($action).' using Windows Live ID'
