d4a18326c846d7ad14cb54eb5fc17fcec5ba0e37
Kiril Angov <kiril.angov@gmail.com>
2012-10-21 20:36:06 -0400

    Adjusted the display of orders which are pending payment verification

--- apps/frontend/modules/mycq/actions/components.class.php
+++ apps/frontend/modules/mycq/actions/components.class.php
@@ -174,9 +174,9 @@ class mycqComponents extends cqFrontendComponents
   {
     $collector = $this->getCollector();
 
-    $q = CollectibleForSaleQuery::create()
-      ->filterByCollector($collector)
-      ->filterByIsSold(true)
+    $q = ShoppingOrderQuery::create()
+      ->isPaid()
+      ->filterBySellerId($collector->getId())
       ->orderByCreatedAt(Criteria::DESC);
 
     if ($this->getRequestParameter('q'))
