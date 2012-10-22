9e9dc6b47a535fd96ab517b9d8182d8ab117f74b
pavel <pavel@collectorsquest.com>
2012-10-22 18:11:56 +0300

    Email logger
    - add model files
    
    Ref: https://basecamp.com/1759305/projects/19290-collectorsquest-com/todos/18818535-add-the-ability-to

---
+++ plugins/cqEmailsPlugin/lib/filter/SentEmailFormFilter.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * SentEmail filter form.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage filter
+ * @author     ##AUTHOR_NAME##
+ */
+class SentEmailFormFilter extends BaseSentEmailFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ plugins/cqEmailsPlugin/lib/model/SentEmail.php
@@ -0,0 +1,20 @@
+<?php
+
+
+require 'plugins/cqEmailsPlugin/lib/model/om/BaseSentEmail.php';
+
+
+/**
+ * Skeleton subclass for representing a row from the 'sent_email' table.
+ *
+ * 
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ * @package    propel.generator.plugins.cqEmailsPlugin.lib.model
+ */
+class SentEmail extends BaseSentEmail {
+
+}
