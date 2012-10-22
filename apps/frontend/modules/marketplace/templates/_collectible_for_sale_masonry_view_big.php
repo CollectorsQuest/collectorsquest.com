fa283098a0c4f2dfdd027f59cdb6d9134426d8b5
Teo <teo.marinov@gmail.com>
2012-10-22 22:23:54 +0300

    Work on to-do https://basecamp.com/1759305/projects/1258511-collectorsquest-com/todos/18841918-implement-in
    Implement in html/css the showing up of the extra information for collectibles for sale

--- apps/frontend/modules/marketplace/templates/_collectible_for_sale_masonry_view_big.php
+++ apps/frontend/modules/marketplace/templates/_collectible_for_sale_masonry_view_big.php
@@ -6,12 +6,11 @@
 
 <div id="collectible_for_sale_<?= $collectible_for_sale->getCollectibleId(); ?>_grid_view_masonry_big"
      data-id="<?= $collectible_for_sale->getCollectibleId(); ?>"
-     class="span4 collectible_for_sale_grid_view_masonry_big brick link"
-     style="width: 220px; margin-left: 16px; margin-bottom: 20px;">
+     class="span4 collectible_for_sale_grid_view_masonry_big brick link">
 
   <?php echo ice_image_tag_flickholdr('220x'. ($collectible_for_sale->getCollectibleId() % 500)); ?>
 
-  <div style="position: relative; top: 100%; height: 80px; margin-top: -80px; padding: 10px; background: #fff;">
+  <div class="text-box">
     <?php
       echo !empty($link) ?
         $link : link_to_collectible($collectible_for_sale->getCollectible(), 'text', array('class' => 'target'));
