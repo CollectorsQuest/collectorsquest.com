9b4cec14fec5c26a67443dc212c85aeda2268b90
IvanTanev <vankata.t@gmail.com>
2012-10-31 21:02:27 +0200

    Add the JS controller from frontend to backend and include some js for organization

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -4,14 +4,17 @@
     <meta charset="utf-8">
     <?php include_metas() ?>
     <?php include_title() ?>
-    
+
 	<link rel="shortcut icon" href="/images/favicon.ico">
-	
+
     <?php include_partial('iceBackendModule/head'); ?>
 
     <?php cq_include_stylesheets(); ?>
   </head>
-  <body>
+  <body id="<?= 'body-'. $sf_context->getModuleName() .'-'. $sf_context->getActionName(); ?>"
+        class="<?= 'body-'. $sf_context->getModuleName(); ?>"
+        data-controller="<?= $sf_context->getModuleName(); ?>"
+        data-action="<?= $sf_context->getActionName(); ?>">
     <?php include_component('iceBackendModule', 'body', array('content' => $sf_content)); ?>
 
     <?php
