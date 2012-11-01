51143abb88eb38bba07a6e0d85094bd9507e289d
Kiril Angov <kangov@collectorsquest.com>
2012-11-01 13:20:14 -0400

    Fix the <html> declaration as in layout.php

--- apps/frontend/modules/_video/templates/headerSuccess.php
+++ apps/frontend/modules/_video/templates/headerSuccess.php
@@ -1,21 +1,16 @@
 <?php
-  /**
-   * @var  $sf_user       cqFrontendUser
-   * @var  $sf_params     sfParameterHolder
-   * @var  $sf_cache_key  string
-   */
-
-  $sf_cache_key  = (int) $sf_user->getId() .'_';
-  $sf_cache_key .= $sf_user->isAuthenticated() ? 'authenticated' : 'not_authenticated';
+ /* @var  $sf_user    cqFrontendUser */
+ /* @var  $sf_params  sfParameterHolder */
 ?>
+
 <!doctype html>
 <!--[if IE 8 ]>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js lt-ie10 lt-ie9">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js lt-ie10 lt-ie9 ie8">
 <![endif]-->
 <!--[if IE 9]>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js lt-ie10">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js lt-ie10 ie9">
 <![endif]-->
-<!--[if gt IE 9]>
+<!--[if gt IE 9]><!-->
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="no-js">
 <!--<![endif]-->
 <head>
@@ -30,7 +25,7 @@
 
   <?php
     // Include the global javascripts
-    include_partial('global/javascripts', array('sf_cache_key' => $sf_cache_key));
+    include_partial('global/javascripts');
   ?>
 </body>
 </html>
