e308a11e486a091bfa11208c0f4727eae723b0e3
Kiril Angov <kangov@collectorsquest.com>
2012-11-05 19:12:41 -0500

    Update the migration timestamp to the current time()

--- lib/model/migration/PropelMigration_1352160693.php
+++ lib/model/migration/PropelMigration_1352160693.php
@@ -3,38 +3,38 @@
 /**
  * Update multimedia table with "role" field
  */
-class PropelMigration_1351801241
+class PropelMigration_1352160693
 {
 
-	public function preUp($manager)
-	{
-		// add the pre-migration code here
-	}
+  public function preUp($manager)
+  {
+    // add the pre-migration code here
+  }
 
-	public function postUp($manager)
-	{
-		// add the post-migration code here
-	}
+  public function postUp($manager)
+  {
+    // add the post-migration code here
+  }
 
-	public function preDown($manager)
-	{
-		// add the pre-migration code here
-	}
+  public function preDown($manager)
+  {
+    // add the pre-migration code here
+  }
 
-	public function postDown($manager)
-	{
-		// add the post-migration code here
-	}
+  public function postDown($manager)
+  {
+    // add the post-migration code here
+  }
 
-	/**
-	 * Get the SQL statements for the Up migration
-	 *
-	 * @return array list of the SQL strings to execute for the Up migration
-	 *               the keys being the datasources
-	 */
-	public function getUpSQL()
-	{
-		return array (
+  /**
+   * Get the SQL statements for the Up migration
+   *
+   * @return array list of the SQL strings to execute for the Up migration
+   *               the keys being the datasources
+   */
+  public function getUpSQL()
+  {
+    return array (
       'propel' => '
         # This is a fix for InnoDB in MySQL >= 4.1.x
         # It "suspends judgement" for fkey relationships until are tables are set.
@@ -52,17 +52,17 @@ class PropelMigration_1351801241
         SET FOREIGN_KEY_CHECKS = 1;
       ',
     );
-	}
+  }
 
-	/**
-	 * Get the SQL statements for the Down migration
-	 *
-	 * @return array list of the SQL strings to execute for the Down migration
-	 *               the keys being the datasources
-	 */
-	public function getDownSQL()
-	{
-		return array (
+  /**
+   * Get the SQL statements for the Down migration
+   *
+   * @return array list of the SQL strings to execute for the Down migration
+   *               the keys being the datasources
+   */
+  public function getDownSQL()
+  {
+    return array (
       'propel' => '
         # This is a fix for InnoDB in MySQL >= 4.1.x
         # It "suspends judgement" for fkey relationships until are tables are set.
@@ -80,6 +80,6 @@ class PropelMigration_1351801241
         SET FOREIGN_KEY_CHECKS = 1;
       ',
     );
-	}
+  }
 
 }
