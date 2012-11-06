9a73b1dcbe5fb6b7d5439b61a2e50390d691ef91
Yanko Simeonoff <ysimeonoff@collectorsquest.com>
2012-11-06 18:19:24 +0200

    Added organization membership request action

--- apps/frontend/modules/organization/actions/actions.class.php
+++ apps/frontend/modules/organization/actions/actions.class.php
@@ -10,9 +10,9 @@
 class organizationActions extends cqFrontendActions
 {
 
- /**
-  * Display an organization's main profile
-  */
+  /**
+   * Display an organization's main profile
+   */
   public function executeIndex(sfWebRequest $request)
   {
     /* @var $organization Organization */
@@ -22,6 +22,44 @@ class organizationActions extends cqFrontendActions
     $this->organization = $organization;
 
     return sfView::SUCCESS;
+  }
+
+  /**
+   * Action Join
+   *
+   * @param sfWebRequest $request
+   *
+   * @return string
+   */
+  public function executeJoin(sfWebRequest $request)
+  {
+    /* @var $organization Organization */
+    $organization = $this->getRoute()->getObject();
+
+    try
+    {
+      OrganizationAccess::createMembershipRequest($organization, $this->getCollector());
+
+      $this->getUser()->setFlash('success', 'Membership requested');
+    }
+    catch (OrganizationAccessMembershipRequestAlreadyPendingException $e)
+    {
+      $this->getUser()->setFlash('error', 'You already requested membership for this organization');
+    }
+    catch (OrganizationAccessMembershipRequestAlreadyApprovedException $e)
+    {
+      $this->getUser()->setFlash('error', 'You already requested membership for this organization');
+    }
+    catch (OrganizationAccessMembershipRequestAlreadyDeniedException $e)
+    {
+      $this->getUser()->setFlash('error', 'You already requested membership for this organization');
+    }
+    catch (OrganizationAccessMembershipRequestAlreadyMemberException $e)
+    {
+      $this->getUser()->setFlash('error', 'You are already a member of this organization');
+    }
+
+    $this->redirect('organization_by_slug', $organization);
   }
 
 }
--- apps/frontend/modules/organization/templates/indexSuccess.php
+++ apps/frontend/modules/organization/templates/indexSuccess.php
@@ -1,11 +1,24 @@
 <?php
-/* @var $organizatio Organization */
+/* @var $organization Organization */
+/* @var $sf_user cqFrontendUser */
 ?>
 
 <div class="row-fluid header-bar">
-  <div class="span9">
-    <h1 class="Chivo webfont" style="margin-left: 145px;" itemprop="name">
-      <?= $organization->getName(); ?>
-    </h1>
-  </div>
+    <div class="span9">
+        <h1 class="Chivo webfont" style="margin-left: 145px;" itemprop="name">
+          <?= $organization->getName(); ?>
+        </h1>
+    </div>
+</div>
+
+<div class="row-fluid">
+    <div class="span9">
+        <div class="span9">
+          <?php if (!$organization->isMember($sf_user->getCollector())): ?>
+          <?= form_tag('@organization_join?id='.$organization->getId(), array('class' => 'form-horizontal')) ?>
+            <button type="submit" class="btn">Join</button>
+            </form>
+          <?php endif; ?>
+        </div>
+    </div>
 </div>
