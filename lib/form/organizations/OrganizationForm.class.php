99a7bb43fc4e1dbf3a671e1e57d73b41428997c8
IvanTanev <vankata.t@gmail.com>
2012-11-08 21:28:24 +0200

    Fix OrganizationForm referral_code null uniques

--- lib/form/organizations/OrganizationForm.class.php
+++ lib/form/organizations/OrganizationForm.class.php
@@ -11,6 +11,19 @@ class OrganizationForm extends BaseOrganizationForm
 {
   public function configure()
   {
+    $this->validatorSchema->setPostValidator(
+      new sfValidatorAnd(array(
+        new sfValidatorPropelUnique(array(
+            'model' => 'Organization',
+            'column' => array('referral_code'),
+            'allow_null_uniques' => true,
+        )),
+        new sfValidatorPropelUnique(array(
+            'model' => 'Organization',
+            'column' => array('slug'),
+        )),
+      ))
+    );
   }
 
   public function updateDefaultsFromObject()
