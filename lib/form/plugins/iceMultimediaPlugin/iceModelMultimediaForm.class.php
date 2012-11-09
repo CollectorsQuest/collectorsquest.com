0f3c51a82a4dffe971f32350521e71d41079e5f8
Kiril Angov <kangov@collectorsquest.com>
2012-11-09 11:27:22 -0500

    Add filter and form classes for iceModelMultimedia

---
+++ lib/form/plugins/iceMultimediaPlugin/iceModelMultimediaForm.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * iceModelMultimedia form.
+ *
+ * @package    CollectorsQuest
+ * @subpackage form
+ * @author     Collectors Quest, Inc.
+ */
+class iceModelMultimediaForm extends BaseiceModelMultimediaForm
+{
+  public function configure()
+  {
+  }
+}
