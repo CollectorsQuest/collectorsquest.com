aeb2930d079cbcf45b1de6690629475e793e62a5
pavel <pavel@collectorsquest.com>
2012-11-09 19:41:34 +0200

    Create a badge behavior
    - Add sluggable behavior to schema
    - Add cqBadgesBehavior class with behavior basic methods
    - Add behavior hooks definition to plugin configuration class
    
    Ref: https://basecamp.com/1759305/projects/1480785-collectorsquest-com/todos/20892092-create-a-badge

--- plugins/cqBadgesBehaviorPlugin/config/cqBadgesBehaviorPluginConfiguration.class.php
+++ plugins/cqBadgesBehaviorPlugin/config/cqBadgesBehaviorPluginConfiguration.class.php
@@ -8,5 +8,20 @@
  */
 class cqBadgesBehaviorPluginConfiguration extends sfPluginConfiguration
 {
+  public function initialize()
+  {
+    sfPropelBehavior::registerHooks('cqBadgesBehavior', array (
+      ':save:post' => array ('cqBadgesBehavior', 'postSave'),
+      ':delete:pre' => array ('cqBadgesBehavior', 'preDelete'),
+    ));
 
+    sfPropelBehavior::registerMethods('cqBadgesBehavior', array (
+      array('cqBadgesBehavior', 'addBadge'),
+      array('cqBadgesBehavior', 'getBadges'),
+      array('cqBadgesBehavior', 'removeAllBadges'),
+      array('cqBadgesBehavior', 'setBadges'),
+    ));
+
+    return parent::initialize();
+  }
 }
