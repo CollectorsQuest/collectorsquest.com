1467536dcd78c24005d4bdd64122c54312b15701
Anton Ruzhekov <anton@collectorsquest.com>
2012-11-24 13:34:56 +0200

    Turn off lazy loading - code style optimization
    Ref:https://github.com/CollectorsQuest/collectorsquest.com/commit/79d101a70ebdce0ec5acfe07ce9ac6d7885912bc#commitcomment-2200398

--- apps/frontend/modules/collection/templates/_collection_stack_grid_view.php
+++ apps/frontend/modules/collection/templates/_collection_stack_grid_view.php
@@ -12,11 +12,7 @@
  *  3) request comes from a mobile device
  */
 $lazy_image = !isset($lazy_image) || $lazy_image;
-$lazy_image = $lazy_image && !$sf_request->isXmlHttpRequest() && 'all' !== $sf_params->get('show');
-if ($sf_request->isMobile() === true)
-{
-  $lazy_image = null;
-}
+$lazy_image = $lazy_image && !$sf_request->isMobile() && !$sf_request->isXmlHttpRequest() && 'all' !== $sf_params->get('show');
 
 ?>
 
