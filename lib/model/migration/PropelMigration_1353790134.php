3a82206f189702fe5afdf16c5d44526db6dde09a
pavel <pavel@collectorsquest.com>
2012-11-25 00:40:23 +0200

    Phpass - update archive
    - Update archive schema
    - Update migration

--- lib/model/migration/PropelMigration_1353790134.php
+++ lib/model/migration/PropelMigration_1353790134.php
@@ -39,6 +39,7 @@ class PropelMigration_1353790134
         SET FOREIGN_KEY_CHECKS = 0;
 
         ALTER TABLE `collector` ADD `portable_password` VARCHAR(64) NOT NULL AFTER `sha1_password`;
+        ALTER TABLE `collector_archive` ADD `portable_password` VARCHAR(64) NOT NULL AFTER `sha1_password`;
 
         SET FOREIGN_KEY_CHECKS = 1;
 ',
@@ -49,4 +50,27 @@ class PropelMigration_1353790134
     );
   }
 
+  /**
+   * Get the SQL statements for the Down migration
+   *
+   * @return array list of the SQL strings to execute for the Down migration
+   *               the keys being the datasources
+   */
+  public function getDownSQL()
+  {
+    return array (
+      'propel' => '
+        SET FOREIGN_KEY_CHECKS = 0;
+
+        ALTER TABLE `collector` DROP `portable_password`;
+        ALTER TABLE `collector_archive` DROP `portable_password`;
+
+        SET FOREIGN_KEY_CHECKS = 1;
+      ',
+      'blog' => '
+        SET FOREIGN_KEY_CHECKS = 0;
+        SET FOREIGN_KEY_CHECKS = 1;
+      ',
+    );
+  }
 }
