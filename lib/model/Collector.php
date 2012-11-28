a5981d54beede11e3781c56d1d7f3a0282e52213
Kiril Angov <kangov@collectorsquest.com>
2012-11-28 17:46:44 -0500

    Revert "Add caching of ShippingReferences to Collector/Collectible"
    This reverts commit 41710041c73489b73d450c760dde67f556a40fa4.

--- lib/model/Collector.php
+++ lib/model/Collector.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'lib/model/om/BaseCollector.php';
+
 /**
  * @method     int getSingupNumCompletedSteps() Return the number of completed signup steps
  * @method     Collector setSingupNumCompletedSteps(int $v) Set the number of completed signup steps
@@ -107,9 +109,6 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
 
   /** @var Collector */
   protected $seller;
-
-  /** @var ShippingReference[] */
-  protected $shipping_references = null;
 
   /**
    * Register extra properties to allow magic getters/setters to be used
@@ -1395,14 +1394,9 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
    */
   public function getShippingReferencesByCountryCode(PropelPDO $con = null)
   {
-    if (null === $this->shipping_references)
-    {
-      $this->shipping_references = ShippingReferenceQuery::create()
-        ->filterByCollector($this)
-        ->find($con)->getArrayCopy($keyColumn = 'CountryIso3166');
-    }
-
-    return $this->shipping_references;
+    return ShippingReferenceQuery::create()
+      ->filterByCollector($this)
+      ->find($con)->getArrayCopy($keyColumn = 'CountryIso3166');
   }
 
   /**
@@ -1415,23 +1409,14 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
    */
   public function getShippingReferenceForCountryCode($coutry_code, PropelPDO $con = null)
   {
-    // get all shiping references, indexed by country code
-    $shipping_references = $this->getShippingReferencesByCountryCode($con);
-
-    // if we have a shipping reference for the specified country code, return it
-    if (isset($shipping_references[$coutry_code]))
-    {
-      return $shipping_references[$coutry_code];
-    }
-
-    // otherwize if we have a ZZ code (international), return it instead
-    if (isset($shipping_references['ZZ']))
-    {
-      return $shipping_references['ZZ'];
-    }
-
-    // otherwize return null
-    return null;
+    return ShippingReferenceQuery::create()
+      ->filterByCollector($this)
+      ->filterByCountryIso3166($coutry_code)
+      ->findOne($con)
+    ?: ShippingReferenceQuery::create()
+      ->filterByCollector($this)
+      ->filterByCountryIso3166('ZZ') // international
+      ->findOne($con);
   }
 
   /**
@@ -1443,17 +1428,7 @@ class Collector extends BaseCollector implements ShippingReferencesInterface
   public function getShippingReferenceDomestic(PropelPDO $con = null)
   {
     return $this->getShippingReferenceForCountryCode(
-      $this->getProfile($con)->getCountryIso3166(),
-      $con
-    );
-  }
-
-  /**
-   * @return    void
-   */
-  public function clearShippingReferences()
-  {
-    $this->shipping_references = null;
+      $this->getProfile($con)->getCountryIso3166(), $con);
   }
 
   /**
