a03359d32b38ffe9beb3b40088ea71239885ba04
Kiril Angov <kangov@collectorsquest.com>
2012-11-28 16:20:22 -0500

    Code formatting

--- apps/frontend/modules/aetn/templates/_partials/franksPicksPromo_300x90.php
+++ apps/frontend/modules/aetn/templates/_partials/franksPicksPromo_300x90.php
@@ -1,16 +1,10 @@
 <?php if (cqGateKeeper::open('aetn_franks_picks', 'page')): ?>
-<?php
-  if (!isset($class))
-  {
-    $class = '';
-  }
-?>
 <div class="banner-sidebar-promo-300-90">
   <?php
     cq_ad_slot(
       cq_image_tag('headlines/2012-0883_FP_Promo_300x90_FIN.jpg',
         array(
-          'width' => '300', 'height' => '90', 'class' => $class,
+          'width' => '300', 'height' => '90', 'class' => isset($class) ? $class : null,
           'alt' => 'One-of-a-kind finds picked fresh from Frank the host of American Pickers'
         )
       ),
