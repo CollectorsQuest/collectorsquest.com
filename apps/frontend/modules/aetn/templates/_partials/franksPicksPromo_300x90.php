0edaa1e75e3112c38429e7a549004b51bc8c7c55
Anton Ruzhekov <anton@collectorsquest.com>
2012-11-28 12:37:57 +0200

    Add Frank's Picks 300x90 banner on right places
    Ref: https://basecamp.com/1759305/projects/1258511-collectorsquest-com/todos/18964770-get-a-e-to-design-a#comment_33568979

--- apps/frontend/modules/aetn/templates/_partials/franksPicksPromo_300x90.php
+++ apps/frontend/modules/aetn/templates/_partials/franksPicksPromo_300x90.php
@@ -1,10 +1,16 @@
 <?php if (cqGateKeeper::open('aetn_franks_picks', 'page')): ?>
+<?php
+  if (!isset($class))
+  {
+    $class = '';
+  }
+?>
 <div class="banner-sidebar-promo-300-90">
   <?php
     cq_ad_slot(
       cq_image_tag('headlines/2012-0883_FP_Promo_300x90_FIN.jpg',
         array(
-          'width' => '300', 'height' => '90',
+          'width' => '300', 'height' => '90', 'class' => $class,
           'alt' => 'One-of-a-kind finds picked fresh from Frank the host of American Pickers'
         )
       ),
