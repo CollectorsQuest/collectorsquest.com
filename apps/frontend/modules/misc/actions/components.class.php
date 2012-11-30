4340c80fc74aa3936f93dfe86fa3515cfb883ace
Anton Ruzhekov <anton@collectorsquest.com>
2012-11-30 21:28:27 +0200

    Do not cache Collectible IDs if cache is disabled
    Ref: https://basecamp.com/1759305/projects/353141-collectorsquest-com/todos/21636157-we-need-to-implement#comment_34195220

--- apps/frontend/modules/misc/actions/components.class.php
+++ apps/frontend/modules/misc/actions/components.class.php
@@ -171,7 +171,11 @@ class miscComponents extends cqFrontendComponents
     $status = $wp_post->getPostStatus();
     $_collectible_ids = $this->getUser()->getAttribute('featured_items_collectible_ids_' . $post_id, null, 'cache');
 
-    if (!$_collectible_ids || $status !== 'publish')
+    /*
+     * calculate collectible_ids if collectible_ids are not yet known OR
+     * wp post is NOT published OR cache is disabled
+     */
+    if (!$_collectible_ids || $status !== 'publish' || sfConfig::get('sf_cache', 'false'))
     {
       // add Collectibles based on Category IDs
       if (!empty($category_ids))
