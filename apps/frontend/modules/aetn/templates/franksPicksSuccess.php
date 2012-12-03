ef05edef235f52e2b9f3c3810aeb4596c80e7454
Kiril Angov <kangov@collectorsquest.com>
2012-12-03 13:12:52 -0500

    Display the collection text for Franks Picks

--- apps/frontend/modules/aetn/templates/franksPicksSuccess.php
+++ apps/frontend/modules/aetn/templates/franksPicksSuccess.php
@@ -6,11 +6,14 @@ echo image_tag('headlines/2012-0883_FP_Shop_Home_939x180_FIN.jpg',
     'size' => '939x180'
   )
 );
+
+echo '<br/><br/>';
+echo $collection->getDescription();
 echo '<br/><br/>';
 
 cq_page_title("Frank's Picks");
-
 ?>
+
 <div id="collectibles-holder" class="row thumbnails" style="margin-top: 10px;">
   <?php include_component('aetn', 'franksPicksCollectiblesForSale'); ?>
 </div>
