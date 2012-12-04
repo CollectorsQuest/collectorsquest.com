53df702d0d30e26cf4e74381f70907e28c9efd17
pavel <pavel@collectorsquest.com>
2012-12-04 21:03:04 +0200

    Two you have sold emails
    - add IsNotified flags to shopping order
    - update migration
    
    Ref: https://basecamp.com/1759305/projects/878205-collectorsquest-com/todos/22485668-received-two-you

--- apps/frontend/modules/shopping/actions/actions.class.php
+++ apps/frontend/modules/shopping/actions/actions.class.php
@@ -787,27 +787,40 @@ class shoppingActions extends cqFrontendActions
     /**
      * Send emails to both the seller and the buyer
      */
-    $cqEmail = new cqEmail($this->getMailer());
-    $cqEmail->send('Shopping/buyer_order_confirmation', array(
-      'to' => $shopping_order->getBuyerEmail(),
-      'params' => array(
-        'buyer_name'  => $shopping_order->getShippingFullName(),
-        'oSeller' => $shopping_order->getSeller(),
-        'oCollectible' => $shopping_order->getCollectible(),
-        'oShoppingOrder' => $shopping_order
-      )
-    ));
+    if (!$shopping_order->getIsBuyerNotified())
+    {
+      $cqEmail = new cqEmail($this->getMailer());
+      $shopping_order->setIsBuyerNotified(
+        $cqEmail->send('Shopping/buyer_order_confirmation', array(
+          'to' => $shopping_order->getBuyerEmail(),
+          'params' => array(
+            'buyer_name'  => $shopping_order->getShippingFullName(),
+            'oSeller' => $shopping_order->getSeller(),
+            'oCollectible' => $shopping_order->getCollectible(),
+            'oShoppingOrder' => $shopping_order
+          )
+        )));
+    }
 
-    $cqEmail = new cqEmail($this->getMailer());
-    $cqEmail->send('Shopping/seller_order_notification', array(
-      'to' => $shopping_order->getSeller()->getEmail(),
-      'params' => array(
-        'buyer_name'  => $shopping_order->getShippingFullName(),
-        'oSeller' => $shopping_order->getSeller(),
-        'oCollectible' => $shopping_order->getCollectible(),
-        'oShoppingOrder' => $shopping_order
-      )
-    ));
+    if (!$shopping_order->getIsSellerNotified())
+    {
+      $cqEmail = new cqEmail($this->getMailer());
+      $shopping_order->setIsSellerNotified(
+        $cqEmail->send('Shopping/seller_order_notification', array(
+          'to' => $shopping_order->getSeller()->getEmail(),
+          'params' => array(
+            'buyer_name'  => $shopping_order->getShippingFullName(),
+            'oSeller' => $shopping_order->getSeller(),
+            'oCollectible' => $shopping_order->getCollectible(),
+            'oShoppingOrder' => $shopping_order
+          )
+        )));
+    }
+    if ($shopping_order->isColumnModified(ShoppingOrderPeer::IS_BUYER_NOTIFIED)
+      || $shopping_order->isColumnModified(ShoppingOrderPeer::IS_SELLER_NOTIFIED))
+    {
+      $shopping_order->save();
+    }
   }
 
   /**
