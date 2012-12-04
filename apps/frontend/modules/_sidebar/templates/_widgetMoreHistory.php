b8ebb4ffdd590cb0e9cb8d7698228c61985401e4
Anton Ruzhekov <anton@collectorsquest.com>
2012-12-04 19:32:21 +0200

    Widget "More HISTORY" -> center aligned

--- apps/frontend/modules/_sidebar/templates/_widgetMoreHistory.php
+++ apps/frontend/modules/_sidebar/templates/_widgetMoreHistory.php
@@ -6,24 +6,28 @@
  */
 $_height = 0;
 
-cq_sidebar_title('More HISTORY');
-$_height -= 63;
 ?>
+<div class="optimize-mobile-300 center">
+  <?php
+    cq_sidebar_title('More HISTORY');
+    $_height -= 63;
+  ?>
 
-<a class="block spacer-bottom" href="http://www.history.com/shows/cajun-pawn-stars"
-   title="Cajun Pawn Stars" target="_blank">
-  <?php echo cq_image_tag('headlines/cps_more_history_promo.jpg', array('alt'=>'Cajun Pawn Stars'));?>
-</a>
+  <a class="block spacer-bottom" href="http://www.history.com/shows/cajun-pawn-stars"
+     title="Cajun Pawn Stars" target="_blank">
+    <?php echo cq_image_tag('headlines/cps_more_history_promo.jpg', array('alt'=>'Cajun Pawn Stars'));?>
+  </a>
 
-<a class="block spacer-bottom" href="http://www.history.com/shows/american-restoration"
-   title="American Restoration" target="_blank">
-  <?php echo cq_image_tag('headlines/ar_more_history_promo.jpg', array('alt'=>'American Restoration'));?>
-</a>
+  <a class="block spacer-bottom" href="http://www.history.com/shows/american-restoration"
+     title="American Restoration" target="_blank">
+    <?php echo cq_image_tag('headlines/ar_more_history_promo.jpg', array('alt'=>'American Restoration'));?>
+  </a>
 
-<a class="block spacer-bottom" href="http://www.history.com/shows/counting-cars"
-   title="Counting Cars" target="_blank">
-  <?php echo cq_image_tag('headlines/counting_cars_300x45.jpg', array('alt'=>'Counting Car'));?>
-</a>
+  <a class="block spacer-bottom" href="http://www.history.com/shows/counting-cars"
+     title="Counting Cars" target="_blank">
+    <?php echo cq_image_tag('headlines/counting_cars_300x45.jpg', array('alt'=>'Counting Car'));?>
+  </a>
+</div>
 
 <?php
   $_height -= 165;
