ba0b93eaff393be204e69976262d37f394918c98
Kiril Angov <kangov@collectorsquest.com>
2012-12-05 16:12:47 -0500

    Track the different applications correctly in NewRelic

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -32,14 +32,17 @@ unset($app, $env, $dbg);
  */
 if (extension_loaded('newrelic'))
 {
-  if (SF_APP !== 'frontend' || SF_ENV !== 'prod')
+  if (trim($_SERVER['SCRIPT_NAME'], '/') !== 'index.php')
   {
-    newrelic_set_appname(ucfirst(SF_APP) .'.CollectorsQuest.'. SF_ENV);
+    $script = '/'. trim($_SERVER['SCRIPT_NAME'], '/');
+    $script = preg_replace('/\\.[^.\\s]{3,4}$/', '', $script);
   }
   else
   {
-    newrelic_set_appname('CollectorsQuest.com');
+    $script = null;
   }
+
+  newrelic_set_appname('CollectorsQuest.'. (SF_ENV !== 'prod' ? SF_ENV : 'com') . $script);
 }
 
 if (!defined('GIT_REVISION'))
