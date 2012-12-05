32a0af97f82d5dc2779bfb1f272a6c8e9d22753e
IvanTanev <vankata.t@gmail.com>
2012-12-05 19:41:23 +0200

    Fix stray ">" in _modal_confirm.php

--- apps/frontend/templates/_modal_confirm.php
+++ apps/frontend/templates/_modal_confirm.php
@@ -1,6 +1,6 @@
 <div id="confirmation-modal" class="modal hide">
     <div class="modal-header">
-      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>>
+      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
       <h3></h3>
     </div>
     <div class="modal-body">
