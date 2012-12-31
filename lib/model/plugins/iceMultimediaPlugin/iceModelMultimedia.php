3fedc74f0df26769cf94a4aaa43d78a0e3a9b88e
IvanTanev <vankata.t@gmail.com>
2012-12-31 13:48:29 +0200

    Throw exception on $multimedia->setCreatedAt() for existing objects
    Refs: https://basecamp.com/1759305/projects/1758858-collectorsquest-com/todos/25233747-alternate-photo-does

--- lib/model/plugins/iceMultimediaPlugin/iceModelMultimedia.php
+++ lib/model/plugins/iceMultimediaPlugin/iceModelMultimedia.php
@@ -34,4 +34,27 @@ class iceModelMultimedia extends PluginiceModelMultimedia
     return $colors;
   }
 
+  /**
+   * Disallow changing the created_at field for existing objects
+   *
+   * @param     mixed $v
+   * @return    iceModelMultimedia
+   *
+   * @throws    RuntimeException
+   * @see       BaseiceModelMultimedia::setCreatedAt()
+   */
+  public function setCreatedAt($v)
+  {
+    if ($this->isNew())
+    {
+      return parent::setCreatedAt($v);
+    }
+    else
+    {
+      throw new RuntimeException(
+        '[iceModelMultimedia] you cannot change the created_at field for existing Multimedia records'
+      );
+    }
+  }
+
 }
