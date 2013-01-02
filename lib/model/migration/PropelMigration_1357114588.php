d6e8ccdb322d4e2aa1a3de966078cd11a0e34bc3
IvanTanev <vankata.t@gmail.com>
2013-01-02 10:38:23 +0200

    Add migration to move "Flea Market Finds" col to HanShotFirst acc
    refs: https://basecamp.com/1759305/projects/1758858-collectorsquest-com/todos/25233835-move-the-collection

---
+++ lib/model/migration/PropelMigration_1357114588.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Move collection Flea Market Finds (4642) of the main CQ account to
+ * a separate account - HanShotFirst (18053)
+ */
+class PropelMigration_1357114588
+{
+    const COLLECTION_ID = 4642;
+    const TARGET_COLLECTOR_ID = 18053;
+
+    public function preUp($manager)
+    {
+       // update the collector id for the collection
+       $collection = CollectorCollectionQuery::create()->findPk(self::COLLECTION_ID);
+       $collection->setCollectorId(self::TARGET_COLLECTOR_ID);
+       $collection->save();
+
+       // and then update it for each collectible attached to the collection
+       foreach ($collection->getCollectibles() as $collectible)
+       {
+         /* @var $collectible Collectible */
+         $collectible->setCollectorId(self::TARGET_COLLECTOR_ID);
+       }
+       $collection->getCollectibles()->save();
+    }
+
+    public function postUp($manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown($manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+          'propel' => '
+            SET FOREIGN_KEY_CHECKS = 0;
+            SET FOREIGN_KEY_CHECKS = 1;
+          ',
+          'blog' => '
+            SET FOREIGN_KEY_CHECKS = 0;
+            SET FOREIGN_KEY_CHECKS = 1;
+          ',
+        );
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+          'propel' => '
+            SET FOREIGN_KEY_CHECKS = 0;
+            SET FOREIGN_KEY_CHECKS = 1;
+          ',
+          'blog' => '
+            SET FOREIGN_KEY_CHECKS = 0;
+            SET FOREIGN_KEY_CHECKS = 1;
+          ',
+        );
+    }
+
+}
