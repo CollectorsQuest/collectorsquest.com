44c7fe9cb082449709077fabba4e979b8adfa707
Anton Ruzhekov <anton@collectorsquest.com>
2013-01-09 19:33:35 +0200

    Fix subtitle text indent
    Ref: https://basecamp.com/1759305/projects/1758858-collectorsquest-com/todos/26356228-look-into-putting

--- apps/frontend/modules/marketplace/templates/holidayMobile.php
+++ apps/frontend/modules/marketplace/templates/holidayMobile.php
@@ -1,6 +1,6 @@
 <a name="discover"></a>
 <div class="slot2-inner">
-  <?php cq_section_title('More from the Market', null, array('class' => 'row-fluid section-title blue')); ?>
+  <?php cq_section_title('More from the Market', null, array('class' => 'row-fluid section-title blue', 'style' => 'text-indent: 10px;')); ?>
 
   <br/>
   <div id="items-for-sale">
