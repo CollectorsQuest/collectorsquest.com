c43df6a08fd2146cd9bd3568d0ea4c4caa118205
Anton Ruzhekov <anton@collectorsquest.com>
2013-01-10 19:03:40 +0200

    Optimize widgetCollectorAmericanPickers for mobile
    Ref: https://basecamp.com/1759305/projects/1758858-collectorsquest-com/todos/26359483-done-optimize-the#comment_39548358

--- apps/frontend/modules/_sidebar/templates/_widgetCollectorAmericanPickers.php
+++ apps/frontend/modules/_sidebar/templates/_widgetCollectorAmericanPickers.php
@@ -33,24 +33,22 @@ $_height = 0;
 <?php $_height -= 128; ?>
 
 <?php if (!$sf_user->isOwnerOf($collector) && isset($message) && $message === true): ?>
-  <div class="row-fluid spacer">
-    <div class="send-pm">
-      <form action="<?= url_for2('messages_compose', array('to'=>$collector->getUsername()), true); ?>"
-            method="post" class="spacer-bottom-reset" id="form-private-message">
-        <?= $pm_form->renderHiddenFields(); ?>
-        <textarea class="requires-login" required data-login-title="Please log in to contact American Pickers:"
-                  data-signup-title="Create an account to contact American Pickers:" name="message[body]"
-                  placeholder="Got a question about this item? Send a message here and the folks at HISTORY will be happy to help." style="height: 53px;"></textarea>
-        <div class="buttons-container" id="buttons-private-message">
-          <?php /* <button type="button" class="btn cancel" value="cancel">cancel</button>
-           &nbsp; - or - &nbsp;
-          <input type="submit" class="btn-lightblue-normal" value="Send the Message"> */?>
-          <button type="submit" class="btn-lightblue-normal textright requires-login">
-            <i class="mail-icon-mini"></i> &nbsp;Send message
-          </button>
-        </div>
-      </form>
-    </div>
+  <div class="row-fluid spacer send-pm send-pm-mobile" style="width: 288px;">
+    <form action="<?= url_for2('messages_compose', array('to'=>$collector->getUsername()), true); ?>"
+          method="post" class="spacer-bottom-reset" id="form-private-message">
+      <?= $pm_form->renderHiddenFields(); ?>
+      <textarea class="requires-login" required data-login-title="Please log in to contact American Pickers:"
+                data-signup-title="Create an account to contact American Pickers:" name="message[body]"
+                placeholder="Got a question about this item? Send a message here and the folks at HISTORY will be happy to help." style="height: 53px;"></textarea>
+      <div class="buttons-container" id="buttons-private-message">
+        <?php /* <button type="button" class="btn cancel" value="cancel">cancel</button>
+         &nbsp; - or - &nbsp;
+        <input type="submit" class="btn-lightblue-normal" value="Send the Message"> */?>
+        <button type="submit" class="btn-lightblue-normal textright requires-login">
+          <i class="mail-icon-mini"></i> &nbsp;Send message
+        </button>
+      </div>
+    </form>
   </div>
   <?php $_height -= 62 ?>
 <?php endif; ?>
