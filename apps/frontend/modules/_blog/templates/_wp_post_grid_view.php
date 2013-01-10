c3e011f2217174c4bea00f257f8867805e0b9f32
Anton Ruzhekov <anton@collectorsquest.com>
2013-01-10 12:24:02 +0200

    Search Blog - Featured items and search results
    Have those custom post types appear without author link
    and extended excerpt text
    Ref: https://basecamp.com/1759305/projects/19290-collectorsquest-com/todos/26533820-roosevelt-search#comment_39306339

--- apps/frontend/modules/_blog/templates/_wp_post_grid_view.php
+++ apps/frontend/modules/_blog/templates/_wp_post_grid_view.php
@@ -4,13 +4,17 @@
      data-id="<?= $wp_post->getId(); ?>" class="wp_post_grid_view">
 
   <h3><?= !empty($title) ? $title : link_to_blog_post($wp_post); ?></h3>
-  <blockquote><?= !empty($excerpt) ? $excerpt : $wp_post->getPostExcerpt(128); ?></blockquote>
-  <?php
+  <?php if ($wp_post->getPostType() == 'search_result' || $wp_post->getPostType() == 'featured_items'): ?>
+    <blockquote style="height: 66px;"><?= !empty($excerpt) ? $excerpt : $wp_post->getPostExcerpt(270); ?></blockquote>
+  <?php else: ?>
+    <blockquote><?= !empty($excerpt) ? $excerpt : $wp_post->getPostExcerpt(128); ?></blockquote>
+    <?php
     echo link_to_blog_author($wp_post->getwpUser(), 'image', array(
       'link_to' => array('style' => 'float: left; margin-right: 10px;'),
       'image_tag' => array('width' => 35, 'height' => 35, 'style' => 'float: left; margin-right: 10px;')
     ));
-  ?>
-  posted by <br/>
-  <?= link_to_blog_author($wp_post->getwpUser(), 'text'); ?>
+    ?>
+    posted by <br/>
+    <?= link_to_blog_author($wp_post->getwpUser(), 'text'); ?>
+  <?php endif; ?>
 </div>
