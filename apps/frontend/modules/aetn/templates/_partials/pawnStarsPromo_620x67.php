9b2acbbb82f4dc59fef9493c2e7a035c2b070e73
Anton Ruzhekov <anton@collectorsquest.com>
2013-01-11 15:15:04 +0200

    Replace 300x90 banners with 620x67
    For all A&E shows on mobile
    Ref: https://basecamp.com/1759305/projects/1758858-collectorsquest-com/todos/26700181-go-over-all-the

---
+++ apps/frontend/modules/aetn/templates/_partials/pawnStarsPromo_620x67.php
@@ -0,0 +1,13 @@
+<div class="banner-sidebar-promo-620-67">
+  <?php
+    cq_ad_slot(
+      cq_image_tag('headlines/2012-0420_PS_Promo_Space_620x67_FIN.jpg',
+        array(
+          'width' => '620', 'height' => '67',
+          'alt' => 'Check out items seen on Pawn Stars'
+        )
+      ),
+      '@aetn_pawn_stars'
+    );
+  ?>
+</div>
--- apps/frontend/modules/aetn/templates/_sidebarPawnStars.php
+++ apps/frontend/modules/aetn/templates/_sidebarPawnStars.php
@@ -1,16 +1,23 @@
 <?php
   /* @var $collection Collection */
+  /* @var $sf_request   cqWebRequest */
 ?>
 
 <div class="banner-sidebar-top">
   <?php cq_dart_slot('300x250', 'collections', 'pawnstars', 'sidebar') ?>
 </div>
 
-<?php include_partial('aetn/partials/franksPicksPromo_300x90'); ?>
-
-<?php include_partial('aetn/partials/americanPickersPromo_300x90'); ?>
-
-<?php include_partial('aetn/partials/americanRestorationPromo_300x90'); ?>
+<?php
+  if ($sf_request->isMobileLayout()):
+    include_partial('aetn/partials/franksPicksPromo_620x67');
+    include_partial('aetn/partials/americanPickersPromo_620x67');
+    include_partial('aetn/partials/americanRestorationPromo_620x67');
+  else:
+    include_partial('aetn/partials/franksPicksPromo_300x90');
+    include_partial('aetn/partials/americanPickersPromo_300x90');
+    include_partial('aetn/partials/americanRestorationPromo_300x90');
+  endif;
+?>
 
 <?php // include_partial('marketplace/partials/holidayMarketPromo_300x90'); ?>
 
