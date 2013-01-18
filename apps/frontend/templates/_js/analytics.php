1dd3b7ec298f39351d22d12dc31f38b8c7a3708e
IvanTanev <vankata.t@gmail.com>
2013-01-18 21:11:52 +0200

    Remove Woopra tracking code

--- apps/frontend/templates/_js/analytics.php
+++ apps/frontend/templates/_js/analytics.php
@@ -16,31 +16,3 @@
     s.parentNode.insertBefore(ga, s);
   })(document, 'script');
 </script>
-
-<!-- Start of Woopra Code -->
-<script type="text/javascript">
-  function woopraReady(tracker)
-  {
-    tracker.setDomain('<?= sfConfig::get('app_domain_name'); ?>');
-    tracker.setIdleTimeout(300000);
-    <?php if ($sf_user->isAuthenticated()): ?>
-    tracker.addVisitorProperty('name', '<?= $sf_user->getCollector()->getDisplayName(); ?>');
-    tracker.addVisitorProperty('avatar', '<?= src_tag_collector($sf_user->getCollector(), '100x100'); ?>');
-    tracker.addVisitorProperty('email','<?= $sf_user->getCollector()->getEmail() ?>');
-    <?php endif; ?>
-    tracker.track();
-
-    return false;
-  }
-
-  (function()
-  {
-    var wsc = document.createElement('script');
-    wsc.src = '//static.woopra.com/js/woopra.js';
-    wsc.type = 'text/javascript';
-    wsc.async = true;
-    var ssc = document.getElementsByTagName('script')[0];
-    ssc.parentNode.insertBefore(wsc, ssc);
-  })();
-</script>
-<!-- End of Woopra Code -->
