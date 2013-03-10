8ede8e9bc56baba899c6090dfefda47cf8987f93
Kiril Angov <kangov@collectorsquest.com>
2013-03-09 21:05:07 -0500

    Fix for https://basecamp.com/1759305/projects/19290-collectorsquest-com/messages/9379607-collections-page

--- apps/frontend/modules/_sidebar/templates/_widgetPopularTopics.php
+++ apps/frontend/modules/_sidebar/templates/_widgetPopularTopics.php
@@ -2,18 +2,21 @@
 
 <div class="twocolumn cf">
   <ul>
-  <?php
-    /** @var $featured_items wpPost[] */
-    foreach ($featured_items as $i => $wp_post)
-    {
-      if ($wp_post->getId() == 34711)
+    <li><a href="/history/american-pickers?ref=hp"><strong>American Pickers</strong></a></li>
+    <li><a href="/history/american-restoration?ref=hp"><strong>American Restoration</strong></a></li>
+    <li><a href="/history/pawn-stars?ref=hp"><strong>Pawn Stars</strong></a></li>
+    <?php
+      /** @var $featured_items wpPost[] */
+      foreach ($featured_items as $i => $wp_post)
       {
-        echo '<li>', link_to('Railroadiana', '@aetn_mwba_railroadiana'), '</li>';
-      }
+        if ($wp_post->getId() == 34711)
+        {
+          echo '<li>', link_to('Railroadiana', '@aetn_mwba_railroadiana'), '</li>';
+        }
 
-      // display 'regular' popular categories
-      echo '<li>', link_to($wp_post->getPostTitle(), 'wordpress_featured_items', $wp_post), '</li>';
-    }
-  ?>
+        // display 'regular' popular categories
+        echo '<li>', link_to($wp_post->getPostTitle(), 'wordpress_featured_items', $wp_post), '</li>';
+      }
+    ?>
   </ul>
 </div>
