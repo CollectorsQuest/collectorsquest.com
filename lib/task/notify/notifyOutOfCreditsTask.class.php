2188bdaee08e990cac28de315c2110027bbeabc5
IvanTanev <vankata.t@gmail.com>
2013-07-09 02:50:38 +0300

    Write a "wishlist" notifyOutOfCreditsTask that uses 2 yet not implemented classes
     - FindsOutOfCreditsCollectors
     - CollectorPromotionGenerator

---
+++ lib/task/notify/notifyOutOfCreditsTask.class.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Notify collectors that they ran out of credits 1 week after.
+ * Second reminder 2 weeks after, offering a 20% discount.
+ *
+ * @author      Ivan Plamenov Tanev aka CraftyShadow <vankata.t@gmail.com>
+ */
+class notifyOutOfCreditsTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name', 'frontend'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'prod'),
+    ));
+
+    $this->namespace        = 'notify';
+    $this->name             = 'out-of-credits';
+    $this->briefDescription = '';
+    $this->detailedDescription = <<<EOF
+The [notify:out-of-credits|INFO] task notifies collectors that they are out of credits
+and advices them to buy more. The second notification includes a 20% discount
+code.
+
+Call it with:
+
+  [php symfony notify:out-of-credits|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    $_SERVER['HTTP_HOST'] = sfConfig::get('app_www_domain');
+
+    cqContext::createInstance($this->configuration);
+
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    /* @var $con PropelPDO */
+    $con = Propel::getConnection();
+
+    $this->notifyByTime('-1 week', $with_discount = false, $con);
+    $this->notifyByTime('-2 weeks', $with_discount = true, $con);
+  }
+
+  /**
+   * @param  mixed $time strtotime compatible string or a unix timestamp
+   * @param  boolean $with_discount should the notification include a discount code
+   * @param  PropelPDO $con
+   */
+  public function notifyByTime($time, $with_discount, PropelPDO $con = null)
+  {
+    $this->log('Searching for sellers out of credits for: ' . (string) $time);
+
+    $cqEmail = new cqEmail($this->getMailer());
+    $finder = new FindsOutOfCreditsCollectors();
+    $discounter = new CollectorPromotionGenerator();
+
+    $date_ran_out = new DateTime($time);
+    $ran_outs = $finder->findRanOutOn($date_ran_out, $con);
+
+    $this->log(sprintf('Found %d collectors to notify', count($ran_outs)));
+
+    foreach ($ran_outs as $collector)
+    {
+      /* @var $collector Collector */
+
+      $this->log('Seinding email to: '. $collector->getEmail());
+
+      if ($with_discount)
+      {
+        $discount = $discounter->forCollector($collector);
+      }
+      else
+      {
+        $discount = false;
+      }
+
+      $cqEmail->send('Notify/out_of_credits', array(
+          'to' => $collector->getEmail(),
+          'params' => array(
+              'oSeller' => $collector->getSeller($con),
+              'oRanOutDate' => $date_ran_out,
+              'sDiscountCode' => $discount,
+          ),
+      ));
+    }
+  }
+
+}
