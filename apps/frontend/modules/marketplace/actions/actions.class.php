06faaf0c2ce82f9d997ef46cc4e68fc1a866720d
Kiril Angov <kangov@collectorsquest.com>
2014-07-14 20:48:35 -0400

    Try to not repeat caregories

--- apps/frontend/modules/marketplace/actions/actions.class.php
+++ apps/frontend/modules/marketplace/actions/actions.class.php
@@ -79,7 +79,7 @@ class marketplaceActions extends cqFrontendActions
     $this->categories = ContentCategoryQuery::create()
       ->filterById(array(2, 402, 674, 1767, 1367, 1425, 1677, 1755, 1604, 3043))
       ->hasCollectiblesForSale()
-      ->groupByCollectionCategoryId()
+      ->groupById()
       ->orderByName(Criteria::ASC)
       ->find();
 
