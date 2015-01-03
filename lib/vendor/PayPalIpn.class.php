35c89f1f1713ecc9df0aeee2df603859cb3a5fbb
Kiril Angov <kangov@collectorsquest.com>
2015-01-02 21:02:32 -0500

    Do not force sslv3 for PayPal requests

--- lib/vendor/PayPalIpn.class.php
+++ lib/vendor/PayPalIpn.class.php
@@ -30,7 +30,7 @@ class PayPalIPN
    *
    * @var boolean
    */
-  public $force_ssl_v3 = true;
+  public $force_ssl_v3 = false;
 
   /**
    *  If true, cURL will use the CURLOPT_FOLLOWLOCATION to follow any
